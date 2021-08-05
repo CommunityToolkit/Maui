1747c6281aa940ef736c22bfdf28742b6da19f6f
James Montemagno <james.montemagno@gmail.com>
2021-08-05 14:55:52 -0700

    New essentials project

---
+++ src/CommunityToolkit.Maui.Essentials/AssemblyInfo/AssemblyInfo.android.cs
@@ -0,0 +1,3 @@
+using System.Reflection;
+
+[assembly: AssemblyMetadata("IsTrimmable", "True")]
---
+++ src/CommunityToolkit.Maui.Essentials/Contacts/Contacts.netstandard.tvos.watchos.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CommunityToolkit.Maui.Essentials
+{
+	public static partial class Contacts
+	{
+		static Task<Contact> PlatformPickContactAsync() => throw ExceptionUtils.NotSupportedOrImplementedException;
+
+		static Task<IEnumerable<Contact>> PlatformGetAllAsync(CancellationToken cancellationToken) => throw ExceptionUtils.NotSupportedOrImplementedException;
+	}
+}
