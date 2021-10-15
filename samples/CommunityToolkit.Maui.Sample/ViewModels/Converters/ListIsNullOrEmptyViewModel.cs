3872c7e740e846aa353914ac6a94a678683cfc19
JSturla <jms0210@gmail.com>
2021-10-15 16:31:00 -0300

    ListIsNullOrEmpty Page Sample
    Added CommunityToolkit.MVVM
    Added class person to UnitTest (can be modified in future)
    Added ListIsNullOrEmptyViewmodel

---
+++ samples/CommunityToolkit.Maui.Sample/ViewModels/Converters/ListIsNullOrEmptyViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using CommunityToolkit.Maui.UnitTests.ObjectModel;
+using Microsoft.Maui.Controls;
+
+namespace CommunityToolkit.Maui.Sample.ViewModels.Converters
+{
+	public class ListIsNullOrEmptyViewModel : BaseViewModel
+	{
+		public ObservableCollection<Person> Items { get; } = new ObservableCollection<Person>();
+
+		public ListIsNullOrEmptyViewModel()
+		{
+			AddItemCommand = new Command(() =>
+			{
+				Items.Add(new Person
+				{
+					Id = Items.Count,
+					FirstName = $"Person {Items.Count}"
+				});
+			});
+			RemoveItemCommand = new Command(() => Items.RemoveAt(0));
+
+			// ListIsNullOrEmptyConvertor needs to know that Items are updated
+			Items.CollectionChanged += (sender, e) => OnPropertyChanged(nameof(Items));
+		}
+
+		public Command AddItemCommand { get; }
+
+		public Command RemoveItemCommand { get; }
+	}
+}
---
+++ src/CommunityToolkit.Maui.UnitTests/ObjectModel/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace CommunityToolkit.Maui.UnitTests.ObjectModel
+{
+    public class Person : ObservableObject
+	{
+		string firstName = string.Empty;
+		string lastName = string.Empty;
+		int id = 0;
+
+		public int Id
+		{
+			get => id;
+			set => SetProperty(ref id, value);
+		}
+
+		public string FirstName
+		{
+			get => firstName;
+			set => SetProperty(ref firstName, value);
+		}
+
+		public string LastName
+		{
+			get => lastName;
+			set => SetProperty(ref lastName, value);
+		}
+
+		public string Group => FirstName[0].ToString().ToUpperInvariant();
+	}
+    
+}
+
