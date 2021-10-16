2554955b07b54ab0f415c8be7b702a7bb602c7a6
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2021-10-16 10:35:38 -0700

    Build only Android + MacCatalyst

--- samples/CommunityToolkit.Maui.Sample/Platforms/iOS/AppDelegate.cs
+++ samples/CommunityToolkit.Maui.Sample/Platforms/iOS/AppDelegate.cs
@@ -2,11 +2,10 @@
 using Microsoft.Maui;
 using Microsoft.Maui.Hosting;
 
-namespace CommunityToolkit.Maui.Sample
+namespace CommunityToolkit.Maui.Sample;
+
+[Register(nameof(AppDelegate))]
+public class AppDelegate : MauiUIApplicationDelegate
 {
-    [Register("AppDelegate")]
-    public class AppDelegate : MauiUIApplicationDelegate
-    {
-        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-    }
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
