0b2399feeae3f32a50d806ce07230f583b52d92a
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2021-10-16 12:01:37 -0700

    Update BasePage.cs

--- samples/CommunityToolkit.Maui.Sample/Pages/Base/BasePage.cs
+++ samples/CommunityToolkit.Maui.Sample/Pages/Base/BasePage.cs
@@ -1,10 +1,26 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Input;
+using CommunityToolkit.Maui.Sample.Models;
 using Microsoft.Maui.Controls;
 
 namespace CommunityToolkit.Maui.Sample.Pages;
 
 public abstract class BasePage : ContentPage
 {
+    public BasePage()
+    {
+        NavigateCommand = new Command(async parameter =>
+        {
+            if (parameter != null)
+            {
+                await Navigation.PushAsync(PreparePage(parameter));
+            }
+        });
+    }
+
+    public ICommand NavigateCommand { get; }
+
     protected override void OnAppearing()
     {
         Debug.WriteLine($"OnAppearing: {this}");
@@ -13,5 +29,17 @@ public abstract class BasePage : ContentPage
     protected override void OnDisappearing()
     {
         Debug.WriteLine($"OnDisappearing: {this}");
+    }
+
+    static Page PreparePage(object parameter)
+    {
+        ArgumentNullException.ThrowIfNull(parameter);
+
+        var model = (SectionModel)parameter;
+
+        var page = (Page)(Activator.CreateInstance(model.Type) ?? throw new ArgumentException("Parameter must of Type Page", nameof(parameter)));
+        page.Title = model.Title;
+
+        return page;
     }
 }
