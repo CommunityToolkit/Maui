9a5b2f464f97633806e1b917774aaa85947c4d88
Hanquez Steven <Steven.Hanquez@outlook.com>
2021-10-16 11:41:40 +0200

    Added IsNullOrEmptyConverters Samples

---
+++ samples/CommunityToolkit.Maui.Sample/ViewModels/Converters/IsNullOrEmptyConverterViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.ObjectModel;
+using Microsoft.Maui.Controls;
+using System.Windows.Input;
+
+namespace CommunityToolkit.Maui.Sample.ViewModels.Converters
+{
+    public class IsNullOrEmptyConverterViewModel : BaseViewModel
+	{
+		string? selectedItem;
+
+		public IsNullOrEmptyConverterViewModel() => ClearSelectionCommand = new Command(() => SelectedItem = null);
+
+		public ObservableCollection<string> DummyItemSource { get; } = new ObservableCollection<string>
+		{
+			"Dummy Item 0",
+			"Dummy Item 1",
+			"Dummy Item 2",
+			"Dummy Item 3",
+			"Dummy Item 4",
+			"Dummy Item 5",
+		};
+
+		public ICommand ClearSelectionCommand { get; }
+
+		public string? SelectedItem
+		{
+			get => selectedItem;
+			set => SetProperty(ref selectedItem, value);
+		}
+	}
+}
