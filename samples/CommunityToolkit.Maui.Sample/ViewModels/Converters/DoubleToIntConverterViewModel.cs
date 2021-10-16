6fa6b138d7abf564a7d68141b3937d239f16c0e8
Hanquez Steven <Steven.Hanquez@outlook.com>
2021-10-16 17:54:18 +0200

    Added DoubleToIntConverter Sample

---
+++ samples/CommunityToolkit.Maui.Sample/ViewModels/Converters/DoubleToIntConverterViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace CommunityToolkit.Maui.Sample.ViewModels.Converters
+{
+    public class DoubleToIntConverterViewModel : BaseViewModel
+	{
+		double index;
+
+		public double Input
+		{
+			get => index;
+			set => SetProperty(ref index, value);
+		}
+	}
+}
