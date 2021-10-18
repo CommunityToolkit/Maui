0d8f50184cd1e8e675435d35bf727a13b88cc547
Juan Sturla <jms0210@gmail.com>
2021-10-18 18:12:33 -0300

    Added Color Converters Sample
    Based on MCT Sample, with some modifications:
    - Formatted Strings did not work
    - BoxView at the bottom did not work either
    - There is a weird warnings on the Constructor

--- samples/CommunityToolkit.Maui.Sample/Pages/Converters/ColorsConverterPage.xaml.cs
+++ samples/CommunityToolkit.Maui.Sample/Pages/Converters/ColorsConverterPage.xaml.cs
@@ -1,9 +1,30 @@
-﻿namespace CommunityToolkit.Maui.Sample.Pages.Converters;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Microsoft.Maui.Graphics;
+
+namespace CommunityToolkit.Maui.Sample.Pages.Converters;
 
 public partial class ColorsConverterPage : BasePage
 {
-    public ColorsConverterPage()
-    {
-        InitializeComponent();
-    }
+	readonly Dictionary<string, Color> colors = typeof(Colors)
+		.GetFields(BindingFlags.Static | BindingFlags.Public)
+		.ToDictionary(c => c.Name, c => (c.GetValue(null) as Color)?? Colors.White);
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    public ColorsConverterPage() => InitializeComponent();
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+    protected override void OnAppearing()
+	{
+		var keys = colors.Keys.ToList();
+		picker.ItemsSource = keys;
+	}
+
+	void Picker_SelectedIndexChanged(object sender, EventArgs e)
+	{
+		if (colors.TryGetValue((string)picker.SelectedItem, out var color))
+			boxView.BackgroundColor = color;
+	}
 }
