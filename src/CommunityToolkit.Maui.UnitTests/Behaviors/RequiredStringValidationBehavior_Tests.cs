370ea3bc38f59f1e57a0cdc097a723f79815e519
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2021-10-19 15:23:35 -0700

    Inherit from BaseTest, Use File-Scoped Namespace

--- src/CommunityToolkit.Maui.UnitTests/Behaviors/RequiredStringValidationBehavior_Tests.cs
+++ src/CommunityToolkit.Maui.UnitTests/Behaviors/RequiredStringValidationBehavior_Tests.cs
@@ -3,88 +3,82 @@ using CommunityToolkit.Maui.UnitTests.Mocks;
 using Microsoft.Maui.Controls;
 using Xunit;
 
-namespace CommunityToolkit.Maui.UnitTests.Behaviors
+namespace CommunityToolkit.Maui.UnitTests.Behaviors;
+
+public class RequiredStringValidationBehavior_Tests : BaseTest
 {
-    public class RequiredStringValidationBehavior_Tests
-	{
-        public RequiredStringValidationBehavior_Tests()
-        {
-			Device.PlatformServices = new MockPlatformServices();
-		}
+    [Fact]
+    public void IsValidTrueWhenBothIsNull_Test()
+    {
+        // Arrange
+        var passwordEntry = new Entry();
+        var confirmPasswordEntry = new Entry();
+        var confirmPasswordBehavior = new RequiredStringValidationBehavior();
+        confirmPasswordBehavior.Flags = ValidationFlags.ValidateOnAttaching;
 
-		[Fact]
-		public void IsValidTrueWhenBothIsNull_Test()
-		{
-			// Arrange
-			var passwordEntry = new Entry();
-			var confirmPasswordEntry = new Entry();
-			var confirmPasswordBehavior = new RequiredStringValidationBehavior();
-			confirmPasswordBehavior.Flags = ValidationFlags.ValidateOnAttaching;
+        // Act
+        confirmPasswordBehavior.RequiredString = passwordEntry.Text;
+        confirmPasswordEntry.Behaviors.Add(confirmPasswordBehavior);
 
-			// Act
-			confirmPasswordBehavior.RequiredString = passwordEntry.Text;
-			confirmPasswordEntry.Behaviors.Add(confirmPasswordBehavior);
+        // Assert
+        Assert.True(confirmPasswordBehavior.IsValid);
+    }
 
-			// Assert
-			Assert.True(confirmPasswordBehavior.IsValid);
-		}
+    [Fact]
+    public void IsValidFalseWhenOneIsNull_Test()
+    {
+        // Arrange
+        var passwordEntry = new Entry();
+        var confirmPasswordEntry = new Entry();
+        var confirmPasswordBehavior = new RequiredStringValidationBehavior();
+        confirmPasswordBehavior.Flags = ValidationFlags.ValidateOnAttaching;
 
-		[Fact]
-		public void IsValidFalseWhenOneIsNull_Test()
-		{
-			// Arrange
-			var passwordEntry = new Entry();
-			var confirmPasswordEntry = new Entry();
-			var confirmPasswordBehavior = new RequiredStringValidationBehavior();
-			confirmPasswordBehavior.Flags = ValidationFlags.ValidateOnAttaching;
+        // Act
+        passwordEntry.Text = "123456";
+        confirmPasswordBehavior.RequiredString = passwordEntry.Text;
+        confirmPasswordEntry.Behaviors.Add(confirmPasswordBehavior);
 
-			// Act
-			passwordEntry.Text = "123456";
-			confirmPasswordBehavior.RequiredString = passwordEntry.Text;
-			confirmPasswordEntry.Behaviors.Add(confirmPasswordBehavior);
+        confirmPasswordEntry.Text = null;
 
-			confirmPasswordEntry.Text = null;
+        // Assert
+        Assert.False(confirmPasswordBehavior.IsValid);
+    }
 
-			// Assert
-			Assert.False(confirmPasswordBehavior.IsValid);
-		}
+    [Fact]
+    public void IsValidTrueWhenEnterSameText_Test()
+    {
+        // Arrange
+        var passwordEntry = new Entry();
+        var confirmPasswordEntry = new Entry();
+        var confirmPasswordBehavior = new RequiredStringValidationBehavior();
+        confirmPasswordBehavior.Flags = ValidationFlags.ValidateOnValueChanging;
 
-		[Fact]
-		public void IsValidTrueWhenEnterSameText_Test()
-		{
-			// Arrange
-			var passwordEntry = new Entry();
-			var confirmPasswordEntry = new Entry();
-			var confirmPasswordBehavior = new RequiredStringValidationBehavior();
-			confirmPasswordBehavior.Flags = ValidationFlags.ValidateOnValueChanging;
+        // Act
+        passwordEntry.Text = "123456";
+        confirmPasswordBehavior.RequiredString = passwordEntry.Text;
+        confirmPasswordEntry.Behaviors.Add(confirmPasswordBehavior);
+        confirmPasswordEntry.Text = "123456";
 
-			// Act
-			passwordEntry.Text = "123456";
-			confirmPasswordBehavior.RequiredString = passwordEntry.Text;
-			confirmPasswordEntry.Behaviors.Add(confirmPasswordBehavior);
-			confirmPasswordEntry.Text = "123456";
+        // Assert
+        Assert.True(confirmPasswordBehavior.IsValid);
+    }
 
-			// Assert
-			Assert.True(confirmPasswordBehavior.IsValid);
-		}
+    [Fact]
+    public void IsValidFalseWhenEnterDifferentText_Test()
+    {
+        // Arrange
+        var passwordEntry = new Entry();
+        var confirmPasswordEntry = new Entry();
+        var confirmPasswordBehavior = new RequiredStringValidationBehavior();
+        confirmPasswordBehavior.Flags = ValidationFlags.ValidateOnValueChanging;
 
-		[Fact]
-		public void IsValidFalseWhenEnterDifferentText_Test()
-		{
-			// Arrange
-			var passwordEntry = new Entry();
-			var confirmPasswordEntry = new Entry();
-			var confirmPasswordBehavior = new RequiredStringValidationBehavior();
-			confirmPasswordBehavior.Flags = ValidationFlags.ValidateOnValueChanging;
+        // Act
+        passwordEntry.Text = "123456";
+        confirmPasswordBehavior.RequiredString = passwordEntry.Text;
+        confirmPasswordEntry.Behaviors.Add(confirmPasswordBehavior);
+        confirmPasswordEntry.Text = "1234567";
 
-			// Act
-			passwordEntry.Text = "123456";
-			confirmPasswordBehavior.RequiredString = passwordEntry.Text;
-			confirmPasswordEntry.Behaviors.Add(confirmPasswordBehavior);
-			confirmPasswordEntry.Text = "1234567";
-
-			// Assert
-			Assert.False(confirmPasswordBehavior.IsValid);
-		}
-	}
+        // Assert
+        Assert.False(confirmPasswordBehavior.IsValid);
+    }
 }
--- src/CommunityToolkit.Maui.UnitTests/Behaviors/UriValidationBehavior_Tests.cs
+++ src/CommunityToolkit.Maui.UnitTests/Behaviors/UriValidationBehavior_Tests.cs
@@ -5,45 +5,39 @@ using CommunityToolkit.Maui.UnitTests.Mocks;
 using Microsoft.Maui.Controls;
 using Xunit;
 
-namespace CommunityToolkit.Maui.UnitTests.Behaviors
+namespace CommunityToolkit.Maui.UnitTests.Behaviors;
+
+public class UriValidationBehavior_Tests : BaseTest
 {
-	public class UriValidationBehavior_Tests
-	{
-        public UriValidationBehavior_Tests()
+    [Theory]
+    [InlineData(@"http://microsoft.com", UriKind.Absolute, true)]
+    [InlineData(@"microsoft/xamarin/news", UriKind.Relative, true)]
+    [InlineData(@"http://microsoft.com", UriKind.RelativeOrAbsolute, true)]
+    [InlineData(@"microsoftcom", UriKind.Absolute, false)]
+    [InlineData(@"microsoft\\\\\xamarin/news", UriKind.Relative, false)]
+    [InlineData(@"ht\\\.com", UriKind.RelativeOrAbsolute, false)]
+    public async Task IsValid(string value, UriKind uriKind, bool expectedValue)
+    {
+        // Arrange
+        var behavior = new UriValidationBehavior
         {
-			Device.PlatformServices = new MockPlatformServices();
-		}
+            UriKind = uriKind,
+        };
 
-		[Theory]
-		[InlineData(@"http://microsoft.com", UriKind.Absolute, true)]
-		[InlineData(@"microsoft/xamarin/news", UriKind.Relative, true)]
-		[InlineData(@"http://microsoft.com", UriKind.RelativeOrAbsolute, true)]
-		[InlineData(@"microsoftcom", UriKind.Absolute, false)]
-		[InlineData(@"microsoft\\\\\xamarin/news", UriKind.Relative, false)]
-		[InlineData(@"ht\\\.com", UriKind.RelativeOrAbsolute, false)]
-		public async Task IsValid(string value, UriKind uriKind, bool expectedValue)
-		{
-			// Arrange
-			var behavior = new UriValidationBehavior
-			{
-				UriKind = uriKind,
-			};
+        var entry = new Entry
+        {
+            Text = value,
+            Behaviors =
+                {
+                    behavior
+                }
+        };
+        entry.Behaviors.Add(behavior);
 
-			var entry = new Entry
-			{
-				Text = value,
-				Behaviors =
-				{
-					behavior
-				}
-			};
-			entry.Behaviors.Add(behavior);
+        // Act
+        await behavior.ForceValidate();
 
-			// Act
-			await behavior.ForceValidate();
-
-			// Assert
-			Assert.Equal(expectedValue, behavior.IsValid);
-		}
-	}
+        // Assert
+        Assert.Equal(expectedValue, behavior.IsValid);
+    }
 }
