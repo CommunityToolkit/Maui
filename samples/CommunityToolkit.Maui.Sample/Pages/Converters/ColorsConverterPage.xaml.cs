6ad5771821e8a642746c256b668f6c1871be89eb
Juan Sturla <jms0210@gmail.com>
2021-10-19 07:27:40 -0300

    Small fixes
    Capitalized variables
    Removed #pragma
    Changed Dictionary to IReadOnlyDictionary

--- samples/CommunityToolkit.Maui.Sample/Pages/Converters/ColorsConverterPage.xaml.cs
+++ samples/CommunityToolkit.Maui.Sample/Pages/Converters/ColorsConverterPage.xaml.cs
@@ -8,23 +8,26 @@ namespace CommunityToolkit.Maui.Sample.Pages.Converters;
 
 public partial class ColorsConverterPage : BasePage
 {
-	readonly Dictionary<string, Color> colors = typeof(Colors)
+	readonly IReadOnlyDictionary<string, Color> colors = typeof(Colors)
 		.GetFields(BindingFlags.Static | BindingFlags.Public)
 		.ToDictionary(c => c.Name, c => (c.GetValue(null) as Color)?? Colors.White);
 
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-    public ColorsConverterPage() => InitializeComponent();
-#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+	public ColorsConverterPage()
+	{
+		InitializeComponent();
+		Picker ??= new();
+		BoxView ??= new();
+	}
 
     protected override void OnAppearing()
 	{
 		var keys = colors.Keys.ToList();
-		picker.ItemsSource = keys;
+		Picker.ItemsSource = keys;
 	}
 
 	void Picker_SelectedIndexChanged(object sender, EventArgs e)
 	{
-		if (colors.TryGetValue((string)picker.SelectedItem, out var color))
-			boxView.BackgroundColor = color;
+		if (colors.TryGetValue((string)Picker.SelectedItem, out var color))
+			BoxView.BackgroundColor = color;
 	}
 }
