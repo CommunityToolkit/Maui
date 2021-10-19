81e2619b6d9b5b4915681350d734f5c3ad03588e
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2021-10-19 14:45:37 -0700

    Update Background Colors

--- samples/CommunityToolkit.Maui.Sample/Pages/Converters/ColorsConverterPage.xaml.cs
+++ samples/CommunityToolkit.Maui.Sample/Pages/Converters/ColorsConverterPage.xaml.cs
@@ -2,32 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Security.Cryptography;
 using Microsoft.Maui.Graphics;
 
 namespace CommunityToolkit.Maui.Sample.Pages.Converters;
 
 public partial class ColorsConverterPage : BasePage
 {
-	readonly IReadOnlyDictionary<string, Color> colors = typeof(Colors)
-		.GetFields(BindingFlags.Static | BindingFlags.Public)
-		.ToDictionary(c => c.Name, c => (c.GetValue(null) as Color)?? Colors.White);
+    readonly IReadOnlyDictionary<string, Color> _colors = typeof(Colors)
+        .GetFields(BindingFlags.Static | BindingFlags.Public)
+        .ToDictionary(c => c.Name, c => (c.GetValue(null) as Color) ?? Colors.White);
 
-	public ColorsConverterPage()
-	{
-		InitializeComponent();
-		Picker ??= new();
-		BoxView ??= new();
-	}
+    public ColorsConverterPage()
+    {
+        InitializeComponent();
+
+        Picker ??= new();
+        BoxView ??= new();
+    }
 
     protected override void OnAppearing()
-	{
-		var keys = colors.Keys.ToList();
-		Picker.ItemsSource = keys;
-	}
+    {
+        Picker.ItemsSource = _colors.Keys.ToList();
+        Picker.SelectedIndex = RandomNumberGenerator.GetInt32(Picker.ItemsSource.Count);
+    }
 
-	void Picker_SelectedIndexChanged(object sender, EventArgs e)
-	{
-		if (colors.TryGetValue((string)Picker.SelectedItem, out var color))
-			BoxView.BackgroundColor = color;
-	}
+    void HandleSelectedIndexChanged(object? sender, EventArgs e)
+    {
+        if (_colors.TryGetValue((string)Picker.SelectedItem, out var color))
+            BoxView.BackgroundColor = color;
+    }
 }
