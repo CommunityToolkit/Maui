b3b9f34e58b488a2ae411874b1c80390926ff8a8
Vladislav Antonyuk <33021114+VladislavAntonyuk@users.noreply.github.com>
2021-10-21 00:17:02 +0300

    Add files which were missed from file scoped namespaces (#162)
    * Add files which were missed from file scoped namespaces
    
    * Add summary closed tag

--- src/CommunityToolkit.Maui.UnitTests/BaseTest.cs
+++ src/CommunityToolkit.Maui.UnitTests/BaseTest.cs
@@ -3,40 +3,39 @@ using System.Globalization;
 using CommunityToolkit.Maui.UnitTests.Mocks;
 using Microsoft.Maui.Controls;
 
-namespace CommunityToolkit.Maui.UnitTests
+namespace CommunityToolkit.Maui.UnitTests;
+
+public class BaseTest : IDisposable
 {
-     public class  BaseTest : IDisposable
-    {
-        readonly CultureInfo? defaultCulture, defaultUICulture;
+	readonly CultureInfo? defaultCulture, defaultUICulture;
 
-        bool _isDisposed;
+	bool _isDisposed;
 
-        protected BaseTest()
-        {
-            defaultCulture = System.Threading.Thread.CurrentThread.CurrentCulture;
-            defaultUICulture = System.Threading.Thread.CurrentThread.CurrentUICulture;
-            Device.PlatformServices = new MockPlatformServices();
-        }
+	protected BaseTest()
+	{
+		defaultCulture = System.Threading.Thread.CurrentThread.CurrentCulture;
+		defaultUICulture = System.Threading.Thread.CurrentThread.CurrentUICulture;
+		Device.PlatformServices = new MockPlatformServices();
+	}
 
-        ~BaseTest() => Dispose(false);
+	~BaseTest() => Dispose(false);
 
-        protected virtual void Dispose(bool isDisposing)
-        {
-            if (_isDisposed)
-                return;
+	protected virtual void Dispose(bool isDisposing)
+	{
+		if (_isDisposed)
+			return;
 
-            Device.PlatformServices = null;
+		Device.PlatformServices = null;
 
-            System.Threading.Thread.CurrentThread.CurrentCulture = defaultCulture ?? throw new NullReferenceException();
-            System.Threading.Thread.CurrentThread.CurrentUICulture = defaultUICulture ?? throw new NullReferenceException();
+		System.Threading.Thread.CurrentThread.CurrentCulture = defaultCulture ?? throw new NullReferenceException();
+		System.Threading.Thread.CurrentThread.CurrentUICulture = defaultUICulture ?? throw new NullReferenceException();
 
-            _isDisposed = true;
-        }
+		_isDisposed = true;
+	}
 
-        void IDisposable.Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
-    }
+	void IDisposable.Dispose()
+	{
+		Dispose(true);
+		GC.SuppressFinalize(this);
+	}
 }
