b53a0711782cdd83431a0af7aacc862c07a72be8
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2021-10-19 17:23:45 -0700

    Update StringToListConverter_Tests.cs

--- src/CommunityToolkit.Maui.UnitTests/Converters/StringToListConverter_Tests.cs
+++ src/CommunityToolkit.Maui.UnitTests/Converters/StringToListConverter_Tests.cs
@@ -9,15 +9,15 @@ public class StringToListConverter_Tests : BaseTest
 {
     readonly static IReadOnlyList<object?[]> listData = new[]
     {
-            new object?[] { "A,B.C;D", new string[] { ",", ".", ";" }, new string[] { "A", "B", "C", "D" } },
-            new object?[] { "A+_+B+_+C", "+_+", new string[] { "A", "B", "C" } },
-            new object?[] { "A,,C", ",", new string[] { "A", string.Empty, "C" }, },
-            new object?[] { "A,C", ",", new string?[] { "A", "C" } },
-            new object?[] { "A", ":-:", new string[] { "A" } },
-            new object?[] { string.Empty, ",", new string[] { string.Empty } },
-            new object?[] { null, ",", Array.Empty<string>() },
-            new object?[] { "ABC", null, new string[] { "ABC" } },
-        };
+        new object?[] { "A,B.C;D", new string[] { ",", ".", ";" }, new string[] { "A", "B", "C", "D" } },
+        new object?[] { "A+_+B+_+C", "+_+", new string[] { "A", "B", "C" } },
+        new object?[] { "A,,C", ",", new string[] { "A", string.Empty, "C" }, },
+        new object?[] { "A,C", ",", new string?[] { "A", "C" } },
+        new object?[] { "A", ":-:", new string[] { "A" } },
+        new object?[] { string.Empty, ",", new string[] { string.Empty } },
+        new object?[] { null, ",", Array.Empty<string>() },
+        new object?[] { "ABC", null, new string[] { "ABC" } },
+    };
 
     [Theory]
     [MemberData(nameof(listData))]
