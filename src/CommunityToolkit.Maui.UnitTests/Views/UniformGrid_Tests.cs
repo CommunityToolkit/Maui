a74a84471b6b704e054340f8e4f204ad351eee01
Vladislav Antonyuk <vlad.antonyuk@gmail.com>
2021-10-20 23:54:08 +0300

    Add comments, add tests

---
+++ src/CommunityToolkit.Maui.UnitTests/Views/UniformGrid_Tests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Maui.Views;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
+using Xunit;
+
+namespace CommunityToolkit.Maui.UnitTests.Views;
+
+public class UniformGridTests : BaseTest
+{
+	UniformGrid uniformGrid;
+	public UniformGridTests()
+	{
+		uniformGrid = new UniformGrid()
+		{
+			Children =
+			  {
+				 new BoxView { BackgroundColor = Colors.Yellow, WidthRequest = 25, HeightRequest = 25 },
+				 new BoxView { BackgroundColor = Colors.Orange, WidthRequest = 25, HeightRequest = 25 },
+				 new BoxView { BackgroundColor = Colors.Purple, WidthRequest = 25, HeightRequest = 25 },
+			  },
+			WidthRequest = 75,
+			HeightRequest = 75
+		};
+	}
+
+	[Fact]
+	public void MeasureUniformGrid()
+	{
+		var expectedSize = new Size(75, 75);
+		var actualSize = uniformGrid.Measure(75, 75);
+		Assert.Equal(expectedSize, actualSize);
+	}
+
+	[Fact]
+	public void ArrangeChildrenUniformGrid()
+	{
+		var expectedSize = new Size(25, 25);
+		var actualSize = uniformGrid.ArrangeChildren(new Rectangle(0, 0, 75, 75));
+		Assert.Equal(expectedSize, actualSize);
+	}
+}
--- src/CommunityToolkit.Maui/Views/UniformGrid.shared.cs
+++ src/CommunityToolkit.Maui/Views/UniformGrid.shared.cs
@@ -4,72 +4,86 @@ using Microsoft.Maui.Controls;
 using Microsoft.Maui.Graphics;
 using Microsoft.Maui.Layouts;
 
-namespace CommunityToolkit.Maui.Views
+namespace CommunityToolkit.Maui.Views;
+
+/// <summary>
+/// The UniformGrid is just like the Grid, with the possibility of multiple rows and columns, but with one important difference:
+/// All rows and columns will have the same size.
+/// Use this when you need the Grid behavior without the need to specify different sizes for the rows and columns.
+/// </summary>
+public class UniformGrid : Grid, ILayoutManager
 {
+	double childWidth;
+
+	double childHeight;
+
 	/// <summary>
-	/// The UniformGrid is just like the Grid, with the possibility of multiple rows and columns, but with one important difference:
-	/// All rows and columns will have the same size.
-	/// Use this when you need the Grid behavior without the need to specify different sizes for the rows and columns.
+	/// Assign this as a LayoutManager
 	/// </summary>
-	public class UniformGrid : Grid, ILayoutManager
+	/// <returns><see cref="ILayoutManager"/></returns>
+	protected override ILayoutManager CreateLayoutManager()
 	{
-		double childWidth;
-
-		double childHeight;
-
-        protected override ILayoutManager CreateLayoutManager()
-        {
-            return this;
-        }
-
-        public Size ArrangeChildren(Rectangle rectangle)
-        {
-			Measure(rectangle.Width, rectangle.Height, 0);
-			var columns = GetColumnsCount(Children.Count, rectangle.Width, childWidth);
-			var rows = GetRowsCount(Children.Count, columns);
-			var boundsWidth = rectangle.Width / columns;
-			var boundsHeight = childHeight;
-			var bounds = new Rectangle(0, 0, boundsWidth, boundsHeight);
-			var count = 0;
-
-			for (var i = 0; i < rows; i++)
-			{
-				for (var j = 0; j < columns && count < Children.Count; j++)
-				{
-					var item = Children[count];
-					bounds.X = j * boundsWidth;
-					bounds.Y = i * boundsHeight;
-					item.Arrange(bounds);
-					count++;
-				}
-			}
-
-			return new Size(boundsWidth, boundsHeight);
-		}
-
-        public Size Measure(double widthConstraint, double heightConstraint)
-        {
-			foreach (var child in Children)
-			{
-				if (child.Visibility != Visibility.Visible)
-					continue;
-
-				var sizeRequest = child.Measure(double.PositiveInfinity, double.PositiveInfinity);
-				childHeight = sizeRequest.Height;
-				childWidth = sizeRequest.Width;
-			}
-
-			var columns = GetColumnsCount(Children.Count, widthConstraint, childWidth);
-			var rows = GetRowsCount(Children.Count, columns);
-			return new Size(columns * childWidth, rows * childHeight);
-		}
-
-		int GetColumnsCount(int visibleChildrenCount, double widthConstraint, double maxChildWidth)
-			=> double.IsPositiveInfinity(widthConstraint)
-				? visibleChildrenCount
-				: Math.Min((int)(widthConstraint / maxChildWidth), visibleChildrenCount);
-
-		int GetRowsCount(int visibleChildrenCount, int columnsCount)
-			=> (int)Math.Ceiling((double)visibleChildrenCount / columnsCount);
+		return this;
 	}
+
+	/// <summary>
+	/// Arrange children
+	/// </summary>
+	/// <param name="rectangle">Grid rectangle</param>
+	/// <returns>Child size</returns>
+	public Size ArrangeChildren(Rectangle rectangle)
+	{
+		Measure(rectangle.Width, rectangle.Height, MeasureFlags.None);
+		var columns = GetColumnsCount(Children.Count, rectangle.Width, childWidth);
+		var rows = GetRowsCount(Children.Count, columns);
+		var boundsWidth = rectangle.Width / columns;
+		var boundsHeight = childHeight;
+		var bounds = new Rectangle(0, 0, boundsWidth, boundsHeight);
+		var count = 0;
+
+		for (var i = 0; i < rows; i++)
+		{
+			for (var j = 0; j < columns && count < Children.Count; j++)
+			{
+				var item = Children[count];
+				bounds.X = j * boundsWidth;
+				bounds.Y = i * boundsHeight;
+				item.Arrange(bounds);
+				count++;
+			}
+		}
+
+		return new Size(boundsWidth, boundsHeight);
+	}
+
+	/// <summary>
+	/// Measure grid size
+	/// </summary>
+	/// <param name="widthConstraint">Width constraint</param>
+	/// <param name="heightConstraint">Height constraint</param>
+	/// <returns>Grid size</returns>
+	public Size Measure(double widthConstraint, double heightConstraint)
+	{
+		foreach (var child in Children)
+		{
+			if (child.Visibility != Visibility.Visible)
+				continue;
+
+			var sizeRequest = child.Measure(double.PositiveInfinity, double.PositiveInfinity);
+			childHeight = sizeRequest.Height;
+			childWidth = sizeRequest.Width;
+		}
+
+		var columns = GetColumnsCount(Children.Count, widthConstraint, childWidth);
+		var rows = GetRowsCount(Children.Count, columns);
+		return new Size(columns * childWidth, rows * childHeight);
+	}
+
+	int GetColumnsCount(int visibleChildrenCount, double widthConstraint, double maxChildWidth)
+		=> double.IsPositiveInfinity(widthConstraint)
+			? visibleChildrenCount
+			: Math.Min((int)(widthConstraint / maxChildWidth), visibleChildrenCount);
+
+	int GetRowsCount(int visibleChildrenCount, int columnsCount)
+		=> (int)Math.Ceiling((double)visibleChildrenCount / columnsCount);
 }
