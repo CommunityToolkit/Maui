e09fa4fc7be9837cf7b5191f18a78c7027763018
Juan Sturla <jms0210@gmail.com>
2021-10-27 09:54:48 -0300

    Added sample for ItemTappedEventArgs

---
+++ samples/CommunityToolkit.Maui.Sample/ViewModels/Converters/ItemTappedEventArgsViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Windows.Input;
+using Microsoft.Maui.Controls;
+
+namespace CommunityToolkit.Maui.Sample.ViewModels.Converters;
+
+public class ItemTappedEventArgsViewModel : BaseViewModel
+{
+	public IEnumerable<Person> Items { get; } = new List<Person>()
+		{
+			new Person() { Id = 1, Name = "Person 1" },
+			new Person() { Id = 2, Name = "Person 2" },
+			new Person() { Id = 3, Name = "Person 3" }
+		};
+
+	public ICommand ItemTappedCommand { get; } =
+		new Command<Person>(person => Application.Current?.MainPage?.DisplayAlert("Item Tapped: ", person?.Name, "Cancel"));
+}
+
+public class Person
+{
+	public int Id { get; set; }
+
+	public string Name { get; set; } = string.Empty;
+}
