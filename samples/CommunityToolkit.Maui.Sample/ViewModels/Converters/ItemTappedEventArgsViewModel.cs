da6c15549b185e2cabf3e90fd8bff43a49f1c579
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2021-10-28 11:14:53 -0400

    Add SelectedItem Binding, Use AsyncRelayCommand

--- samples/CommunityToolkit.Maui.Sample/ViewModels/Converters/ItemTappedEventArgsViewModel.cs
+++ samples/CommunityToolkit.Maui.Sample/ViewModels/Converters/ItemTappedEventArgsViewModel.cs
@@ -1,25 +1,45 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using System.Windows.Input;
+using CommunityToolkit.Mvvm.Input;
 using Microsoft.Maui.Controls;
 
 namespace CommunityToolkit.Maui.Sample.ViewModels.Converters;
 
 public class ItemTappedEventArgsViewModel : BaseViewModel
 {
-	public IEnumerable<Person> Items { get; } = new List<Person>()
-		{
-			new Person() { Id = 1, Name = "Person 1" },
-			new Person() { Id = 2, Name = "Person 2" },
-			new Person() { Id = 3, Name = "Person 3" }
-		};
+	Person? _selectedItem = null;
 
-	public ICommand ItemTappedCommand { get; } =
-		new Command<Person>(person => Application.Current?.MainPage?.DisplayAlert("Item Tapped: ", person?.Name, "Cancel"));
+	public ItemTappedEventArgsViewModel()
+	{
+		ItemTappedCommand = new AsyncRelayCommand<Person>(ExecuteItemSelectedCommand);
+	}
+
+	public ICommand ItemTappedCommand { get; }
+
+	public IReadOnlyList<Person> Items { get; } = new[]
+	{
+		new Person(1, "John Doe"),
+		new Person(2, "Jane Doe"),
+		new Person(3, "Joe Doe"),
+	};
+
+	public Person? ItemSelected
+	{
+		get => _selectedItem;
+		set => SetProperty(ref _selectedItem, value);
+	}
+
+	Task ExecuteItemSelectedCommand(Person? person)
+	{
+		ArgumentNullException.ThrowIfNull(person);
+
+		ItemSelected = null;
+
+		return Application.Current?.MainPage?.DisplayAlert("Item Tapped", person.Name, "Ok") ?? Task.CompletedTask;
+	}
+
+	public record Person(int Id, string Name);
 }
 
-public class Person
-{
-	public int Id { get; set; }
-
-	public string Name { get; set; } = string.Empty;
-}
