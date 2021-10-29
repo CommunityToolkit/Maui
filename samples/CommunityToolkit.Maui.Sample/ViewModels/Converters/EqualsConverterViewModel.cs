29f311084f6a6235da0c1a1cff3b0e8de6bfa3a6
Stefan Bogaard <stefanbogaard86@gmail.com>
2021-10-29 20:20:39 +0200

    Create a sample for the EqualConverter

---
+++ samples/CommunityToolkit.Maui.Sample/ViewModels/Converters/EqualsConverterViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace CommunityToolkit.Maui.Sample.ViewModels.Converters;
+
+public class EqualsConverterViewModel : BaseViewModel
+{
+	private string? _inputValue;
+	
+	public string? InputValue
+	{
+		get => _inputValue;
+		set => SetProperty(ref _inputValue, value);
+	}
+}
