7d75e47eef66537ff03af0b51b8a6af22493eed0
Stefan Bogaard <stefanbogaard86@gmail.com>
2021-10-31 19:14:59 +0100

    Created a sample for the BoolToObjectConverter

--- samples/CommunityToolkit.Maui.Sample/Pages/Converters/BoolToObjectConverterPage.xaml.cs
+++ samples/CommunityToolkit.Maui.Sample/Pages/Converters/BoolToObjectConverterPage.xaml.cs
@@ -1,9 +1,21 @@
-﻿namespace CommunityToolkit.Maui.Sample.Pages.Converters;
+﻿using System;
+using Microsoft.Maui.Controls;
+
+namespace CommunityToolkit.Maui.Sample.Pages.Converters;
 
 public partial class BoolToObjectConverterPage : BasePage
 {
     public BoolToObjectConverterPage()
     {
         InitializeComponent();
-    }
+		CheckBox ??= new();
+		Ellipse ??= new();
+	}
+
+	void OnButtonClicked(object sender, EventArgs args)
+	{
+		if (sender is not Button button)
+			return;
+		Ellipse.Fill = new SolidColorBrush(button.BackgroundColor);
+	}
 }
