91cde92975ba400c09bbaf69145ab60f5fc458be
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2021-11-09 19:15:13 -0800

    Update MultiValidationBehavior sample

--- samples/CommunityToolkit.Maui.Sample/Pages/Behaviors/MultiValidationBehaviorPage.xaml.cs
+++ samples/CommunityToolkit.Maui.Sample/Pages/Behaviors/MultiValidationBehaviorPage.xaml.cs
@@ -1,16 +1,15 @@
-﻿using CommunityToolkit.Maui.Behaviors;
-
-namespace CommunityToolkit.Maui.Sample.Pages.Behaviors;
+﻿namespace CommunityToolkit.Maui.Sample.Pages.Behaviors;
 
 public partial class MultiValidationBehaviorPage
 {
 	public MultiValidationBehaviorPage()
 	{
 		InitializeComponent();
-		MultiValidation ??= new MultiValidationBehavior();
-		digit ??= new CharactersValidationBehavior();
-		upper ??= new CharactersValidationBehavior();
-		symbol ??= new CharactersValidationBehavior();
-		any ??= new CharactersValidationBehavior();
+
+		MultiValidation ??= new();
+		DigitValidation ??= new();
+		UpperValidation ??= new();
+		SymbolValidation ??= new();
+		AnyValidation ??= new();
 	}
 }
