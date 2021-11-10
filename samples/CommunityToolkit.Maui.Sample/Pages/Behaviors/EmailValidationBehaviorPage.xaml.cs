333f9fe04c96c389e71c4b9a5a1e3da82f994fc6
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2021-11-09 17:57:22 -0800

    Remove Nested Layouts in EmailValidationBehaviorPage

--- samples/CommunityToolkit.Maui.Sample/Pages/Behaviors/EmailValidationBehaviorPage.xaml.cs
+++ samples/CommunityToolkit.Maui.Sample/Pages/Behaviors/EmailValidationBehaviorPage.xaml.cs
@@ -1,12 +1,10 @@
-﻿using CommunityToolkit.Maui.Behaviors;
-
-namespace CommunityToolkit.Maui.Sample.Pages.Behaviors;
+﻿namespace CommunityToolkit.Maui.Sample.Pages.Behaviors;
 
 public partial class EmailValidationBehaviorPage : BasePage
 {
 	public EmailValidationBehaviorPage()
 	{
 		InitializeComponent();
-		EmailValidator ??= new EmailValidationBehavior();
+		EmailValidator ??= new();
 	}
 }
