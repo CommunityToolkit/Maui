6767dd092da74344eec63c33dc706b2e11f05859
Pieter Nijs <pieternijs@live.be>
2021-11-22 17:36:23 +0100

    Color to animation extension #88 (#194)
    * Initial implementation of ColorAnmiationExtensions ColorTo method
    
    * Fix typo
    
    * Added basic test page for ColorAnimationExtensions
    
    * Implemented ColorAnimationExtensions page for ToColor extension
    
    * Added additional controls for testing
    
    * Fix macOS Build
    
    * Simplify Sample, Remove Default Accessibility Modifiers
    
    * Increase Spacing, Remove Frame Shadow, Improve Label Text Readability, Use DarkGreen for Animate Button BackgroundColor
    
    Co-authored-by: Brandon Minnick <13558917+brminnick@users.noreply.github.com>

---
+++ src/CommunityToolkit.Maui/Extensions/ColorAnimationExtensions.shared.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
+
+namespace CommunityToolkit.Maui.Extensions;
+
+/// <summary>
+/// Extension methods for Microsoft.Maui.Graphics.Color animations
+/// </summary>
+public static class ColorAnimationExtensions
+{
+	/// <summary>
+	/// Animates the BackgroundColor of a VisualElement to the given color
+	/// </summary>
+	/// <param name="element"></param>
+	/// <param name="color">The target color to animate the VisualElement's BackgroundColor to</param>
+	/// <param name="rate">The time, in milliseconds, between the frames of the animation</param>
+	/// <param name="length">The duration, in milliseconds, of the animation</param>
+	/// <param name="easing">The easing function to be used in the animation</param>
+	/// <returns>Value indicating if the animation completed successfully or not</returns>
+	public static Task<bool> BackgroundColorTo(this VisualElement element, Color color, uint rate = 16u, uint length = 250u, Easing? easing = null)
+	{
+		ArgumentNullException.ThrowIfNull(element);
+		ArgumentNullException.ThrowIfNull(color);
+
+		//Although BackgroundColor is defined as not-nullable, it CAN be null
+		//If null => set it to Transparent as Animation will crash on null BackgroundColor
+		element.BackgroundColor ??= Colors.Transparent;
+
+		var animationCompletionSource = new TaskCompletionSource<bool>();
+
+		try
+		{
+			new Animation
+			{
+				{ 0, 1, GetRedTransformAnimation(element, color.Red) },
+				{ 0, 1, GetGreenTransformAnimation(element, color.Green) },
+				{ 0, 1, GetBlueTransformAnimation(element, color.Blue) },
+				{ 0, 1, GetAlphaTransformAnimation(element, color.Alpha) },
+			}
+			.Commit(element, nameof(BackgroundColorTo), rate, length, easing, (d, b) => animationCompletionSource.SetResult(true));
+		}
+		catch (ArgumentException aex)
+		{
+			//When creating an Animation too early in the lifecycle of the Page, i.e. in the OnAppearing method,
+			//the Page might not have an 'IAnimationManager' yet, resulting in an ArgumentException.
+			System.Diagnostics.Debug.WriteLine($"{aex.GetType().Name} thrown in {typeof(ColorAnimationExtensions).FullName}: {aex.Message}");
+			animationCompletionSource.SetResult(false);
+		}
+
+		return animationCompletionSource.Task;
+	}
+
+	static Animation GetRedTransformAnimation(VisualElement element, float targetRed) =>
+		new(v => element.BackgroundColor = element.BackgroundColor.WithRed(v), element.BackgroundColor.Red, targetRed);
+
+	static Animation GetGreenTransformAnimation(VisualElement element, float targetGreen) =>
+		new(v => element.BackgroundColor = element.BackgroundColor.WithGreen(v), element.BackgroundColor.Green, targetGreen);
+
+	static Animation GetBlueTransformAnimation(VisualElement element, float targetBlue) =>
+		new(v => element.BackgroundColor = element.BackgroundColor.WithBlue(v), element.BackgroundColor.Blue, targetBlue);
+
+	static Animation GetAlphaTransformAnimation(VisualElement element, float targetAlpha) =>
+		new(v => element.BackgroundColor = element.BackgroundColor.WithAlpha(v), element.BackgroundColor.Alpha, targetAlpha);
+}
