38ace05f3abe6fae7fa7dff1d008ffe012699474
Vladislav Antonyuk <vlad.antonyuk@gmail.com>
2021-11-25 15:59:07 +0200

    base tests

---
+++ src/CommunityToolkit.Maui.UnitTests/Mocks/MockSnackbar.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using CommunityToolkit.Maui.Views.Popup;
+using CommunityToolkit.Maui.Views.Popup.SnackBar;
+using Microsoft.Maui;
+
+namespace CommunityToolkit.Maui.UnitTests.Mocks;
+
+public class MockSnackbar : ISnackbar
+{
+	public Action Action { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+	public string ActionButtonText { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+	public IView? Anchor { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+	public TimeSpan Duration { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+	public bool IsShown { get; private set; }
+
+	public string Text { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+	public SnackbarOptions VisualOptions { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+	public event EventHandler? Dismissed;
+	public event EventHandler<ShownEventArgs>? Shown;
+
+	public Task Dismiss()
+	{
+		IsShown = false;
+		return Task.CompletedTask;
+	}
+
+	public Task Show()
+	{
+		IsShown = true;
+		return Task.CompletedTask;
+	}
+}
--- src/CommunityToolkit.Maui.UnitTests/Views/SnackBar_Tests.cs
+++ src/CommunityToolkit.Maui.UnitTests/Views/SnackBar_Tests.cs
@@ -1,17 +1,39 @@
 ﻿using System;
 using System.Threading.Tasks;
 using CommunityToolkit.Maui.Extensions;
+using CommunityToolkit.Maui.UnitTests.Mocks;
+using CommunityToolkit.Maui.Views.Popup.SnackBar;
 using Microsoft.Maui.Controls;
 using Xunit;
 
 namespace CommunityToolkit.Maui.UnitTests.Views;
 
-public class SnackBar_Tests
+public class SnackBar_Tests : BaseTest
 {
+	ISnackbar snackbar;
+	public SnackBar_Tests()
+	{
+		snackbar = new MockSnackbar();
+	}
+
 	[Fact]
-	public void PageExtension_DisplaySnackBarAsync_PlatformNotSupportedException()
+	public async Task VisualElementExtension_PageDisplaySnackBar_PlatformNotSupportedException()
 	{
 		var page = new ContentPage();
-		Assert.ThrowsAsync<PlatformNotSupportedException>(() => page.DisplaySnackBarAsync(string.Empty, string.Empty, () => { }));
+		await Assert.ThrowsAsync<PlatformNotSupportedException>(() => page.DisplaySnackBar(string.Empty, string.Empty, () => { }));
+	}
+
+	[Fact]
+	public async Task VisualElementExtension_AnchorDisplaySnackBar_PlatformNotSupportedException()
+	{
+		var button = new Button();
+		await Assert.ThrowsAsync<PlatformNotSupportedException>(() => button.DisplaySnackBar(string.Empty, string.Empty, () => { }));
+	}
+
+	[Fact]
+	public async Task SnackbarShow_AnchorDisplaySnackBar_PlatformNotSupportedException()
+	{
+		await snackbar.Show();
+		Assert.True(snackbar.IsShown);
 	}
 }
