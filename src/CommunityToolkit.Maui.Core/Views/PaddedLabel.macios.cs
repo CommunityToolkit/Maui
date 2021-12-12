e7fb53d3a194bedbedd6339c956f0df721502974
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2021-12-12 14:46:52 -0800

    Remove Duplicate PaddedLabel, Make `PaddedLabel` Public

---
+++ src/CommunityToolkit.Maui.Core/Views/PaddedLabel.macios.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoreGraphics;
+using UIKit;
+
+namespace CommunityToolkit.Maui.Core.Views;
+
+/// <summary>
+/// <see cref="UILabel"/> with Left, Top, Right and Bottom Padding
+/// </summary>
+public class PaddedLabel : UILabel
+{
+	/// <summary>
+	/// Initialize <see cref="PaddedLabel"/>
+	/// </summary>
+	/// <param name="leftPadding"></param>
+	/// <param name="topPadding"></param>
+	/// <param name="rightPadding"></param>
+	/// <param name="bottomPadding"></param>
+	public PaddedLabel(double leftPadding, double topPadding, double rightPadding, double bottomPadding)
+	{
+		LeftPadding = (nfloat)leftPadding;
+		TopPadding = (nfloat)topPadding;
+		RightPadding = (nfloat)rightPadding;
+		BottomPadding = (nfloat)bottomPadding;
+	}
+
+	/// <summary>
+	/// Left Padding
+	/// </summary>
+	public nfloat LeftPadding { get; }
+
+	/// <summary>
+	/// Top Padding
+	/// </summary>
+	public nfloat TopPadding { get; }
+
+	/// <summary>
+	/// Right Padding
+	/// </summary>
+	public nfloat RightPadding { get; }
+
+	/// <summary>
+	/// Bottom Padding
+	/// </summary>
+	public nfloat BottomPadding { get; }
+
+	/// <inheritdoc/>
+	public override CGSize IntrinsicContentSize => new(
+		base.IntrinsicContentSize.Width + LeftPadding + RightPadding,
+		base.IntrinsicContentSize.Height + TopPadding + BottomPadding);
+
+	/// <inheritdoc/>
+	public override void DrawText(CGRect rect)
+	{
+		var insets = new UIEdgeInsets(TopPadding, LeftPadding, BottomPadding, RightPadding);
+		base.DrawText(insets.InsetRect(rect));
+	}
+}
--- src/CommunityToolkit.Maui.Core/Views/Toast/ToastView.macios.cs
+++ src/CommunityToolkit.Maui.Core/Views/Toast/ToastView.macios.cs
@@ -95,33 +95,4 @@ public class ToastView : Popup
 	}
 
 	static nfloat GetEmFromPx(nfloat defaultFontSize, double currentValue) => 100 * (nfloat)currentValue / defaultFontSize;
-
-	class PaddedLabel : UILabel
-	{
-		public PaddedLabel(double left, double top, double right, double bottom)
-		{
-			Left = (nfloat)left;
-			Top = (nfloat)top;
-			Right = (nfloat)right;
-			Bottom = (nfloat)bottom;
-		}
-
-		public nfloat Left { get; }
-
-		public nfloat Top { get; }
-
-		public nfloat Right { get; }
-
-		public nfloat Bottom { get; }
-
-		public override CGSize IntrinsicContentSize => new(
-			base.IntrinsicContentSize.Width + Left + Right,
-			base.IntrinsicContentSize.Height + Top + Bottom);
-
-		public override void DrawText(CGRect rect)
-		{
-			var insets = new UIEdgeInsets(Top, Left, Bottom, Right);
-			base.DrawText(insets.InsetRect(rect));
-		}
-	}
 }
