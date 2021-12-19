af6a9ca3c68d3a8c9ac6b02e78cf07ca0ea8b1f9
Vladislav Antonyuk <vlad.antonyuk@gmail.com>
2021-12-20 01:58:58 +0200

    WinUI fix

--- samples/CommunityToolkit.Maui.Sample/Platforms/Windows/App.xaml.cs
+++ samples/CommunityToolkit.Maui.Sample/Platforms/Windows/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Maui.Hosting;
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+using Microsoft.UI.Xaml;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
