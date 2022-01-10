709409577e1fcca60285a2326017fe8f901531f6
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2022-01-10 14:52:11 -0800

    Update MainGalleryViewModel.cs

--- samples/CommunityToolkit.Maui.Sample/ViewModels/MainGalleryViewModel.cs
+++ samples/CommunityToolkit.Maui.Sample/ViewModels/MainGalleryViewModel.cs
@@ -8,18 +8,24 @@ namespace CommunityToolkit.Maui.Sample.ViewModels;
 
 public class MainGalleryViewModel : BaseGalleryViewModel
 {
-	protected override IEnumerable<SectionModel> CreateItems() => new[]
-	{
-		new SectionModel(typeof(BehaviorsGalleryPage), "Behaviors", Color.FromArgb("#8E8CD8"),
-			"Behaviors lets you add functionality to user interface controls without having to subclass them. Behaviors are written in code and added to controls in XAML or code"),
+    public MainGalleryViewModel(AlertsGalleryPage alertsGalleryPage,
+                                BehaviorsGalleryPage behaviorsGalleryPage,
+                                ConvertersGalleryPage convertersGalleryPage,
+                                ExtensionsGalleryPage extensionsGalleryPage)
+        : base(new[]
+        {
+             new SectionModel(alertsGalleryPage, "Alerts", Color.FromArgb("#EF6950"),
+                 "Alerts allow you display alerts to your user"),
 
-		new SectionModel(typeof(ConvertersGalleryPage), "Converters", Color.FromArgb("#EA005E"),
-			"Converters let you convert bindings of a certain type to a different value, based on custom logic"),
+             new SectionModel(behaviorsGalleryPage, "Behaviors", Color.FromArgb("#8E8CD8"),
+                 "Behaviors lets you add functionality to user interface controls without having to subclass them. Behaviors are written in code and added to controls in XAML or code"),
 
-		new SectionModel(typeof(ExtensionsGalleryPage), "Extensions", Color.FromArgb("#00EA56"),
-			"Extenions lets you add methods to existing types without creating a new derived type, recompiling, or otherwise modifying the original type."),
+             new SectionModel(convertersGalleryPage, "Converters", Color.FromArgb("#EA005E"),
+                 "Converters let you convert bindings of a certain type to a different value, based on custom logic"),
 
-		new SectionModel(typeof(AlertsGalleryPage), "Alerts", Color.FromArgb("#EF6950"),
-			"Alerts allow you display alerts to your user"),
-	};
+             new SectionModel(extensionsGalleryPage, "Extensions", Color.FromArgb("#00EA56"),
+                 "Extenions lets you add methods to existing types without creating a new derived type, recompiling, or otherwise modifying the original type."),
+        })
+    {
+    }
 }
