ca95800f6270405f6b66a4ec464b9b57f2491f19
Vladislav Antonyuk <vlad.antonyuk@gmail.com>
2022-01-11 14:42:35 +0200

    Fix build

--- samples/CommunityToolkit.Maui.Sample/ViewModels/Alerts/AlertsGalleryViewModel.cs
+++ samples/CommunityToolkit.Maui.Sample/ViewModels/Alerts/AlertsGalleryViewModel.cs
@@ -2,33 +2,63 @@
 
 
 
+
+
+
+
 namespace CommunityToolkit.Maui.Sample.ViewModels.Alerts;
+
+
+
+
 
 
 
 public class AlertsGalleryViewModel : BaseGalleryViewModel
 
+
+
 {
 	public AlertsGalleryViewModel()
 
+
+
 		: base(new[]
+
+
 
 		{
 
+
+
 			SectionModel.Create<SnackbarViewModel>("Snackbar", "Show Snackbar")
+
+
 
 		})
 
+
+
 	{
 
+
+
 	}
+
+
 
 }
 
 
+
+
 public class SnackbarViewModel : BaseViewModel
 
+
+
 {
+
+
 
 
 }
