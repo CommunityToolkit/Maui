79b7b1c30c3e937d80a2ded011d23efd65a445c5
pictos <pedrojesus.cefet@gmail.com>
2022-01-13 20:23:24 -0300

    bring renderer

---
+++ src/CommunityToolkit.Maui/PlatformConfiguration/WindowsSpecific/PopUp.shared.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.PlatformConfiguration;
+using MCTElement = CommunityToolkit.Maui.Views.BasePopup;
+using CommunityToolkit.Maui.Views;
+
+namespace CommunityToolkit.Maui.PlatformConfiguration.WindowsSpecific;
+
+public static class PopUp
+{
+	public static readonly BindableProperty BorderColorProperty = BindableProperty.Create(
+		"BorderColor", typeof(Color), typeof(BasePopup), default(Color));
+
+	public static void SetBorderColor(BindableObject element, Color color) =>
+		element.SetValue(BorderColorProperty, color);
+
+	public static Color GetBorderColor(BindableObject element) =>
+		(Color)element.GetValue(BorderColorProperty);
+
+	public static IPlatformElementConfiguration<Windows, MCTElement> SetBorderColor(this IPlatformElementConfiguration<Windows, MCTElement> config, Color value)
+	{
+		SetBorderColor(config.Element, value);
+		return config;
+	}
+}
