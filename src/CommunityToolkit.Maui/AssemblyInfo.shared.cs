ced6c5201bb8ab8b28be60a644820dab6f577b54
Shaun Lawrence <shaunrlawrence@gmail.com>
2022-01-27 20:29:39 +0000

    Add mct prefix for xmlns and update samples to use it

--- src/CommunityToolkit.Maui/AssemblyInfo.shared.cs
+++ src/CommunityToolkit.Maui/AssemblyInfo.shared.cs
@@ -2,6 +2,8 @@
 [assembly: XmlnsDefinition(Constants.XamlNamespace, Constants.CommunityToolkitNamespacePrefix + nameof(CommunityToolkit.Maui.Behaviors))]
 [assembly: XmlnsDefinition(Constants.XamlNamespace, Constants.CommunityToolkitNamespacePrefix + nameof(CommunityToolkit.Maui.Converters))]
 
+[assembly: Microsoft.Maui.Controls.XmlnsPrefix(Constants.XamlNamespace, "mct")]
+
 class Constants
 {
 	public const string XamlNamespace = "http://schemas.microsoft.com/dotnet/2022/maui/toolkit";
