b35267795c1950c0d2f4802ec30aa8113bac2280
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2022-02-06 15:56:09 -0800

    Remove `<InternalsVisibleTo Include="CommunityToolkit.Maui" />`

--- src/CommunityToolkit.Maui.Core/Primitives/Defaults.shared.cs
+++ src/CommunityToolkit.Maui.Core/Primitives/Defaults.shared.cs
@@ -1,12 +1,33 @@
-﻿namespace CommunityToolkit.Maui.Core;
+﻿using System.ComponentModel;
 
-static class Defaults
+namespace CommunityToolkit.Maui.Core;
+
+/// <summary>
+/// Default Values for CommunityToolkit.Maui
+/// </summary>
+public static class Defaults
 {
+	/// <summary>
+	/// Default Font Size
+	/// </summary>
+	[EditorBrowsable(EditorBrowsableState.Never)]
 	public const double FontSize = 14;
 
+	/// <summary>
+	/// Default Character Spacing
+	/// </summary>
+	[EditorBrowsable(EditorBrowsableState.Never)]
+	public const double CharacterSpacing = 0.0d;
+
+	/// <summary>
+	/// Default Text Color
+	/// </summary>
+	[EditorBrowsable(EditorBrowsableState.Never)]
 	public static readonly Color TextColor = Colors.Black;
 
+	/// <summary>
+	/// Default Background Color
+	/// </summary>
+	[EditorBrowsable(EditorBrowsableState.Never)]
 	public static readonly Color BackgroundColor = Colors.LightGray;
-
-	public const double CharacterSpacing = 0.0d;
 }
