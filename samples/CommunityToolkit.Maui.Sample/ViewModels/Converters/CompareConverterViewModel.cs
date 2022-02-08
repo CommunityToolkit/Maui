282ef55948fdeeace5c05be0e0c3809539eeee7b
Mark Scott <43500665+marsscotia@users.noreply.github.com>
2022-02-08 22:00:42 +0000

    Implement CompareConverter sample
    Add CompareConverterViewModel
    Add CompareConverterPage
    
    Register view model in MauiProgram.cs
    Add sample to routes in AppShell.xaml.cs
    Add viewmodel to ConvertsGalleryViewModel

---
+++ samples/CommunityToolkit.Maui.Sample/ViewModels/Converters/CompareConverterViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunityToolkit.Maui.Sample.ViewModels.Converters;
+public class CompareConverterViewModel : BaseViewModel
+{
+	double sliderValue;
+
+	public double SliderValue
+	{
+		get => sliderValue;
+		set => SetProperty(ref sliderValue, value);
+	}
+
+
+	public CompareConverterViewModel()
+	{
+		SliderValue = 2;
+	}
+}
