170ffc5ba9b93ef478a2ee933c6970ea475bb63a
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2022-02-10 10:23:45 -0800

    Rename `ListIsNullOrEmptyConverter` and `ListIsNotNullOrEmptyConverter` (#288)
    * Rename `ListIsNullOrEmptyConverter` and `ListIsNotNullOrEmptyConverter`
    
    * Rename Unit Tests
    
    * Use correct expected result paramter  for `Assert.Equals`
    
    * Update src/CommunityToolkit.Maui.UnitTests/Converters/IsListNotNullOrEmptyConverter_Tests.cs
    
    Co-authored-by: Shaun Lawrence <shaunrlawrence@gmail.com>
    
    * Update src/CommunityToolkit.Maui.UnitTests/Converters/IsListNullOrEmptyConverter_Tests.cs
    
    Co-authored-by: Shaun Lawrence <shaunrlawrence@gmail.com>
    
    * Update src/CommunityToolkit.Maui.UnitTests/Converters/IsListNotNullOrEmptyConverter_Tests.cs
    
    Co-authored-by: Shaun Lawrence <shaunrlawrence@gmail.com>
    
    * Update src/CommunityToolkit.Maui.UnitTests/Converters/IsListNullOrEmptyConverter_Tests.cs
    
    Co-authored-by: Shaun Lawrence <shaunrlawrence@gmail.com>
    
    Co-authored-by: Shaun Lawrence <shaunrlawrence@gmail.com>

---
+++ samples/CommunityToolkit.Maui.Sample/Pages/Converters/IsListNullOrEmptyConverterPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using CommunityToolkit.Maui.Sample.ViewModels.Converters;
+
+namespace CommunityToolkit.Maui.Sample.Pages.Converters;
+
+public partial class IsListNullOrEmptyConverterPage : BasePage<IsListNullOrEmptyConverterViewModel>
+{
+	public IsListNullOrEmptyConverterPage(IsListNullOrEmptyConverterViewModel isListNullOrEmptyConverterViewModel)
+		: base(isListNullOrEmptyConverterViewModel)
+	{
+		InitializeComponent();
+	}
+}
--- src/CommunityToolkit.Maui/Converters/IsListNullOrEmptyConverter.cs
+++ src/CommunityToolkit.Maui/Converters/IsListNullOrEmptyConverter.cs
@@ -1,25 +1,25 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System.Collections;
+using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 using CommunityToolkit.Maui.Extensions;
 
 namespace CommunityToolkit.Maui.Converters;
 
 /// <summary>
-/// Converts the incoming value to a <see cref="bool"/> indicating whether or not the value is not null and not empty.
+/// Converts the incoming value to a <see cref="bool"/> indicating whether or not the value is null or empty.
 /// </summary>
-public class ListIsNotNullOrEmptyConverter : ValueConverterExtension, ICommunityToolkitValueConverter
+public class IsListNullOrEmptyConverter : ValueConverterExtension, ICommunityToolkitValueConverter
 {
 	/// <summary>
-	/// Converts the incoming value to a <see cref="bool"/> indicating whether or not the value is not null and not empty.
+	/// Converts the incoming value to a <see cref="bool"/> indicating whether or not the value is null or empty.
 	/// </summary>
 	/// <param name="value">The value to convert.</param>
 	/// <param name="targetType">The type of the binding target property. This is not implemented.</param>
 	/// <param name="parameter">Additional parameter for the converter to handle. This is not implemented.</param>
 	/// <param name="culture">The culture to use in the converter. This is not implemented.</param>
-	/// <returns>A <see cref="bool"/> indicating if the incoming value is not null and not empty.</returns>
+	/// <returns>A <see cref="bool"/> indicating if the incoming value is null or empty.</returns>
 	[return: NotNull]
-	public object? Convert(object? value, Type? targetType, object? parameter, CultureInfo? culture) =>
-		!ListIsNullOrEmptyConverter.ConvertInternal(value);
+	public object? Convert(object? value, Type? targetType, object? parameter, CultureInfo? culture) => IsListNullOrEmpty(value);
 
 	/// <summary>
 	/// This method is not implemented and will throw a <see cref="NotImplementedException"/>.
@@ -31,4 +31,19 @@ public class ListIsNotNullOrEmptyConverter : ValueConverterExtension, ICommunity
 	/// <returns>N/A</returns>
 	public object? ConvertBack(object? value, Type? targetType, object? parameter, CultureInfo? culture)
 		=> throw new NotImplementedException();
+
+	internal static bool IsListNullOrEmpty([NotNullWhen(false)] object? value)
+	{
+		if (value is null)
+		{
+			return true;
+		}
+
+		if (value is IEnumerable list)
+		{
+			return !list.GetEnumerator().MoveNext();
+		}
+
+		throw new ArgumentException("Value is not a valid IEnumerable or null", nameof(value));
+	}
 }
