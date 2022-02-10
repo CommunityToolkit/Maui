ca413b1929773555f2a954f49714ab69518df936
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2022-02-09 19:16:42 -0800

    Small UI Tweaks
    - Change `StackLayout` to `VerticalStackLayout`
    - Update Slider range to 0-1
    - Set default Slider value to `0.5`
    - Use `LightGreen` and `PaleVioletRed` (there is no `Colors.LightRed`) to make text more easy to read
    - Replace Bottom Margin with `StackLayout.Spacing`

--- samples/CommunityToolkit.Maui.Sample/ViewModels/Converters/CompareConverterViewModel.cs
+++ samples/CommunityToolkit.Maui.Sample/ViewModels/Converters/CompareConverterViewModel.cs
@@ -5,19 +5,14 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace CommunityToolkit.Maui.Sample.ViewModels.Converters;
+
 public class CompareConverterViewModel : BaseViewModel
 {
-	double sliderValue;
+	double sliderValue = 0.5;
 
 	public double SliderValue
 	{
 		get => sliderValue;
 		set => SetProperty(ref sliderValue, value);
-	}
-
-
-	public CompareConverterViewModel()
-	{
-		SliderValue = 2;
 	}
 }
