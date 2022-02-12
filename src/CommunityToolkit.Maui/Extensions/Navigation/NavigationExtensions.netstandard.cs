889edef70f31e5cdc93d3b8ccfad62c1e8d4c901
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2022-02-11 18:38:07 -0800

    Update src/CommunityToolkit.Maui/Extensions/Navigation/NavigationExtensions.netstandard.cs
    Co-authored-by: Vladislav Antonyuk <33021114+VladislavAntonyuk@users.noreply.github.com>

--- src/CommunityToolkit.Maui/Extensions/Navigation/NavigationExtensions.netstandard.cs
+++ src/CommunityToolkit.Maui/Extensions/Navigation/NavigationExtensions.netstandard.cs
@@ -9,7 +9,7 @@ namespace CommunityToolkit.Maui.Extensions;
 public static partial class NavigationExtensions
 {
 	static void PlatformShowPopup(BasePopup popup, IMauiContext mauiContext) =>
-		throw new NotSupportedException($"The current platform '{Device.RuntimePlatform}' does not support Xamarin.CommunityToolkit.BasePopup");
+		throw new NotSupportedException($"The current platform '{Device.RuntimePlatform}' does not support CommunityToolkit.Maui.Core.BasePopup");
 
 	static Task<object?> PlatformShowPopupAsync(Popup popup, IMauiContext mauiContext) =>
 		throw new NotSupportedException($"The current platform '{Device.RuntimePlatform}' does not support Xamarin.CommunityToolkit.Popup.");
