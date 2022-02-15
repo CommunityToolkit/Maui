7637353bdb915755871ecc10a363115857a7a237
pictos <pedrojesus.cefet@gmail.com>
2022-02-15 19:54:53 -0300

    Replace all Xamarin references to MAUI RIP Xamarin (◡︵◡)

--- samples/CommunityToolkit.Maui.Sample/ViewModels/Views/Popups/CsharpBindingPopupViewModel.cs
+++ samples/CommunityToolkit.Maui.Sample/ViewModels/Views/Popups/CsharpBindingPopupViewModel.cs
@@ -4,5 +4,5 @@ public sealed class CsharpBindingPopupViewModel
 {
 	public string Title { get; } = "Xaml Binding Popup";
 
-	public string Message { get; } = "This is a native popup with a Xamarin.Forms View being rendered. The behaviors of the popup will confirm to 100% native look and feel, but still allows you to use your Xamarin.Forms controls.";
+	public string Message { get; } = "This is a native popup with a .NET MAUI View being rendered. The behaviors of the popup will confirm to 100% native look and feel, but still allows you to use your .NET MAUI Controls.";
 }
