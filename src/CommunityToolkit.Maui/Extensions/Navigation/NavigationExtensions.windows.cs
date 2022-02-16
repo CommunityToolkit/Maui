8045fdc1fb43e816d23ee4c7f8a139cbc8b26a76
pictos <pedrojesus.cefet@gmail.com>
2022-02-16 20:45:32 -0300

    make the navigation on Windows better

--- src/CommunityToolkit.Maui/Extensions/Navigation/NavigationExtensions.windows.cs
+++ src/CommunityToolkit.Maui/Extensions/Navigation/NavigationExtensions.windows.cs
@@ -1,5 +1,6 @@
 ﻿using CommunityToolkit.Maui.Core;
 using CommunityToolkit.Maui.Views;
+using Microsoft.Maui.Controls.Platform;
 using Microsoft.Maui.Platform;
 
 namespace CommunityToolkit.Maui.Extensions;
@@ -8,34 +9,10 @@ public static partial class NavigationExtensions
 {
 	static void PlatformShowPopup(BasePopup popup, IMauiContext mauiContext)
 	{
-		var window = mauiContext.GetNativeWindow().GetWindow();
-		popup.Parent = GetCurrentPage((Page)window!.Content);
-		// https://github.com/xamarin/Xamarin.Forms/blob/0c95d0976cc089fe72476fb037851a64987de83c/Xamarin.Forms.Platform.iOS/PageExtensions.cs#L44
+		var window = mauiContext.GetNativeWindow().GetWindow() ?? throw new NullReferenceException("Window is null.");
+		popup.Parent = PageExtensions.GetCurrentPage((Page)window.Content);
 		var native = popup.ToHandler(mauiContext);
 		native?.Invoke(nameof(IPopup.OnOpened));
-		Page GetCurrentPage(Page currentPage)
-		{
-			if (currentPage.NavigationProxy.ModalStack.LastOrDefault() is Page modal)
-			{
-				return modal;
-			}
-			else if (currentPage is FlyoutPage fp)
-			{
-				return GetCurrentPage(fp.Detail);
-			}
-			else if (currentPage is Shell shell && shell.CurrentItem?.CurrentItem is IShellSectionController ssc)
-			{
-				return ssc.PresentedPage;
-			}
-			else if (currentPage is IPageContainer<Page> pc)
-			{
-				return GetCurrentPage(pc.CurrentPage);
-			}
-			else
-			{
-				return currentPage;
-			}
-		}
 	}
 
 	static Task<object?> PlatformShowPopupAsync(Popup popup, IMauiContext mauiContext)
