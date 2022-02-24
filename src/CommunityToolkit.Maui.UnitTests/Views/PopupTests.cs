102459c67aa2abf02f7644c72ac385e4b26ff51d
pictos <pedrojesus.cefet@gmail.com>
2022-02-24 20:13:10 -0300

    Added a test as sample

--- src/CommunityToolkit.Maui.UnitTests/Views/PopupTests.cs
+++ src/CommunityToolkit.Maui.UnitTests/Views/PopupTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using CommunityToolkit.Maui.Core;
+using CommunityToolkit.Maui.Extensions;
 using CommunityToolkit.Maui.Views;
 using Xunit;
 
@@ -7,6 +8,7 @@ namespace CommunityToolkit.Maui.UnitTests;
 
 public class PopupTests : BaseTest
 {
+	readonly IPopup popup = new MockPopup();
 	public PopupTests()
 	{
 		Assert.IsAssignableFrom<IPopup>(new MockPopup());
@@ -16,9 +18,20 @@ public class PopupTests : BaseTest
 	[Fact]
 	public void PlaceholderTest()
 	{
+		var page = new ContentPage
+		{
+			Content = new Label
+			{
+				Text = "Hello there"
+			}
+		};
+		var np = new NavigationPage(page);
+		_ = page.Navigation;
+		page.ShowPopup((MockPopup)popup);
+		Assert.True(true);
 		// This is a place holder to remind us to implement Unit Tests for Popup
 		// This test should be deleted once Popup Unit Tests are added
-		throw new NotImplementedException("Popup Unit Tests Not completed");
+		//throw new NotImplementedException("Popup Unit Tests Not completed");
 	}
 
 	class MockPopup : Popup
