948d9b898fcd9f253d21ea7798f0ecfe73719b16
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2022-02-24 10:30:06 -0800

    Change `bool?` to `bool`

--- samples/CommunityToolkit.Maui.Sample/ViewModels/Converters/VariableMultiValueConverterViewModel.cs
+++ samples/CommunityToolkit.Maui.Sample/ViewModels/Converters/VariableMultiValueConverterViewModel.cs
@@ -2,60 +2,60 @@
 
 public class VariableMultiValueConverterViewModel : BaseViewModel
 {
-	bool? isAllGroupSwitch1On = false;
-	bool? isAllGroupSwitch2On = false;
+	bool isAllGroupSwitch1On = false;
+	bool isAllGroupSwitch2On = false;
 
-	bool? isAnyGroupSwitch1On = false;
-	bool? isAnyGroupSwitch2On = false;
+	bool isAnyGroupSwitch1On = false;
+	bool isAnyGroupSwitch2On = false;
 
-	bool? isGreaterThanGroupSwitch1On = false;
-	bool? isGreaterThanGroupSwitch2On = false;
-	bool? isGreaterThanGroupSwitch3On = false;
-	bool? isGreaterThanGroupSwitch4On = false;
+	bool isGreaterThanGroupSwitch1On = false;
+	bool isGreaterThanGroupSwitch2On = false;
+	bool isGreaterThanGroupSwitch3On = false;
+	bool isGreaterThanGroupSwitch4On = false;
 
-	public bool? IsAllGroupSwitch1On
+	public bool IsAllGroupSwitch1On
 	{
 		get => isAllGroupSwitch1On;
 		set => SetProperty(ref isAllGroupSwitch1On, value);
 	}
 
-	public bool? IsAllGroupSwitch2On
+	public bool IsAllGroupSwitch2On
 	{
 		get => isAllGroupSwitch2On;
 		set => SetProperty(ref isAllGroupSwitch2On, value);
 	}
 
-	public bool? IsAnyGroupSwitch1On
+	public bool IsAnyGroupSwitch1On
 	{
 		get => isAnyGroupSwitch1On;
 		set => SetProperty(ref isAnyGroupSwitch1On, value);
 	}
 
-	public bool? IsAnyGroupSwitch2On
+	public bool IsAnyGroupSwitch2On
 	{
 		get => isAnyGroupSwitch2On;
 		set => SetProperty(ref isAnyGroupSwitch2On, value);
 	}
 
-	public bool? IsGreaterThanGroupSwitch1On
+	public bool IsGreaterThanGroupSwitch1On
 	{
 		get => isGreaterThanGroupSwitch1On;
 		set => SetProperty(ref isGreaterThanGroupSwitch1On, value);
 	}
 
-	public bool? IsGreaterThanGroupSwitch2On
+	public bool IsGreaterThanGroupSwitch2On
 	{
 		get => isGreaterThanGroupSwitch2On;
 		set => SetProperty(ref isGreaterThanGroupSwitch2On, value);
 	}
 
-	public bool? IsGreaterThanGroupSwitch3On
+	public bool IsGreaterThanGroupSwitch3On
 	{
 		get => isGreaterThanGroupSwitch3On;
 		set => SetProperty(ref isGreaterThanGroupSwitch3On, value);
 	}
 
-	public bool? IsGreaterThanGroupSwitch4On
+	public bool IsGreaterThanGroupSwitch4On
 	{
 		get => isGreaterThanGroupSwitch4On;
 		set => SetProperty(ref isGreaterThanGroupSwitch4On, value);
