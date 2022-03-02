aa8a16231fa5ebcce48ee44b90c9ca1f7b21b657
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2022-03-01 17:28:51 -0800

    Update IsStringNotNullOrEmptyConverter

--- src/CommunityToolkit.Maui/Converters/IsStringNotNullOrEmptyConverter.shared.cs
+++ src/CommunityToolkit.Maui/Converters/IsStringNotNullOrEmptyConverter.shared.cs
@@ -6,20 +6,15 @@ namespace CommunityToolkit.Maui.Converters;
 /// <summary>
 /// Converts the incoming value to a <see cref="bool"/> indicating whether or not the value is not null and not empty.
 /// </summary>
-public class IsStringNotNullOrEmptyConverter : BaseConverterOneWay
+public class IsStringNotNullOrEmptyConverter : BaseConverterOneWay<string?, bool>
 {
 	/// <summary>
 	/// Converts the incoming string to a <see cref="bool"/> indicating whether or not the value is not null and not empty using string.IsNullOrEmpty.
 	/// </summary>
 	/// <param name="value">The value to convert.</param>
-	/// <param name="targetType">The type of the binding target property. This is not implemented.</param>
-	/// <param name="parameter">Additional parameter for the converter to handle. This is not implemented.</param>
-	/// <param name="culture">The culture to use in the converter. This is not implemented.</param>
-	/// <returns>A <see cref="bool"/> indicating if the incoming value is not null and not empty.</returns>
-	[return: NotNull]
-	public override object? Convert(object? value, Type? targetType, object? parameter, CultureInfo? culture)
+	public override bool ConvertFrom(string? value)
 	{
-		var stringValue = (string?)value;
+		var stringValue = value;
 
 		return !string.IsNullOrEmpty(stringValue);
 	}
