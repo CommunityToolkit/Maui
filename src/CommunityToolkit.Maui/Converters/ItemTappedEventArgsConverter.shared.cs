8cdbdd61a1cec66931630dffa9b71a8220270e01
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2022-03-01 23:19:09 -0800

    Update Defunct XML Comment

--- src/CommunityToolkit.Maui/Converters/ItemTappedEventArgsConverter.shared.cs
+++ src/CommunityToolkit.Maui/Converters/ItemTappedEventArgsConverter.shared.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿
+using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 
 namespace CommunityToolkit.Maui.Converters;
@@ -12,9 +13,6 @@ public class ItemTappedEventArgsConverter : BaseConverterOneWay<ItemTappedEventA
 	/// Converts/Extracts the incoming value from <see cref="ItemTappedEventArgs"/> object and returns the value of <see cref="ItemTappedEventArgs.Item"/> property from it.
 	/// </summary>
 	/// <param name="value">The value to convert.</param>
-	/// <param name="targetType">The type of the binding target property. This is not implemented.</param>
-	/// <param name="parameter">Additional parameter for the converter to handle. This is not implemented.</param>
-	/// <param name="culture">The culture to use in the converter. This is not implemented.</param>
 	/// <returns>A <see cref="ItemTappedEventArgs.Item"/> object from object of type <see cref="ItemTappedEventArgs"/>.</returns>
 	[return: NotNullIfNotNull("value")]
 	public override object? ConvertFrom(ItemTappedEventArgs? value) => value switch
