cc959b6223a5230aac86d2553f0b776ac6cccc4b
pictos <pedrojesus.cefet@gmail.com>
2022-03-04 23:57:54 -0300

    Added unit test

---
+++ src/CommunityToolkit.Maui.Analyzers/CommunityToolkit.Maui.Analyzers.Tests/DumbCodeFix.cs
@@ -0,0 +1,22 @@
+﻿using CommunityToolkit.Maui.Analyzer;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Threading.Tasks;
+
+namespace CommunityToolkit.Maui.Analyzers.Tests
+{
+	//This code fix is used to make tests happy ／人◕ ‿‿ ◕人＼
+	[ExportCodeFixProvider(LanguageNames.CSharp, Name = "Dumb"), Shared]
+
+	public class DumbCodeFix : CodeFixProvider
+	{
+		public override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create(CheckForNotImplementedExceptionAnalyzer.DiagnosticId);
+
+		public override Task RegisterCodeFixesAsync(CodeFixContext context)
+		{
+			return Task.CompletedTask;
+		}
+	}
+}
---
+++ src/CommunityToolkit.Maui.Analyzers/CommunityToolkit.Maui.Analyzers.Tests/NotImplementedAnalyzerTests.cs
@@ -0,0 +1,53 @@
+﻿using CommunityToolkit.Maui.Analyzer;
+using Microsoft.CodeAnalysis.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading.Tasks;
+using VerifyCS = CommunityToolkit.Maui.Analyzer.Test.CSharpCodeFixVerifier<CommunityToolkit.Maui.Analyzer.CheckForNotImplementedExceptionAnalyzer,
+	CommunityToolkit.Maui.Analyzers.Tests.DumbCodeFix>;
+
+namespace Analyzers.Tests
+{
+    [TestClass]
+    public class NotImplementedAnalyzerTests
+    {
+        [TestMethod]
+        public async Task TestMethod1()
+        {
+            var test = @"";
+
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+
+        [TestMethod]
+        public async Task TestMethod2()
+        {
+            var test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class TYPENAME
+        {
+            public int Bla => throw new NotImplementedException();
+
+            public void Foo(string name)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+";
+            var diagnosticResult1 = VerifyCS.Diagnostic().WithSpan(13, 31, 13, 66);
+            var diagnosticResult2 = VerifyCS.Diagnostic().WithSpan(17, 17, 17, 53);
+
+
+            await VerifyCS.VerifyAnalyzerAsync(test, diagnosticResult1, diagnosticResult2);
+        }
+    }
+}
