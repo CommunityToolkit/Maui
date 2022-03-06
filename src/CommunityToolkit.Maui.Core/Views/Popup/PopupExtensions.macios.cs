07eb5c888c30e240a50259966c32756bb15f4a1b
pictos <pedrojesus.cefet@gmail.com>
2022-03-06 19:21:05 -0300

    code style

--- src/CommunityToolkit.Maui.Core/Views/Popup/PopupExtensions.macios.cs
+++ src/CommunityToolkit.Maui.Core/Views/Popup/PopupExtensions.macios.cs
@@ -35,68 +35,69 @@ public static class PopupExtensions
 		}
 	}
 
-		/// <summary>
-		/// Method to update the <see cref="IPopup.Color"/> of the Popup.
-		/// </summary>
-		/// <param name="popup">An instance of <see cref="MauiPopup"/>.</param>
-		/// <param name="basePopup">An istance of <see cref="IPopup"/>.</param>
-		public static void SetBackgroundColor(this MauiPopup popup, in IPopup basePopup)
+	/// <summary>
+	/// Method to update the <see cref="IPopup.Color"/> of the Popup.
+	/// </summary>
+	/// <param name="popup">An instance of <see cref="MauiPopup"/>.</param>
+	/// <param name="basePopup">An istance of <see cref="IPopup"/>.</param>
+	public static void SetBackgroundColor(this MauiPopup popup, in IPopup basePopup)
+	{
+		if (popup.Control is null)
 		{
-			if (popup.Control is null)
-			{
-				return;
-			}
-
-			var color = basePopup.Color?.ToNative();
-			popup.Control.NativeView.BackgroundColor = color;
+			return;
 		}
 
-		/// <summary>
-		/// Method to update the <see cref="IPopup.IsLightDismissEnabled"/> property of the Popup.
-		/// </summary>
-		/// <param name="popup">An instance of <see cref="MauiPopup"/>.</param>
-		/// <param name="basePopup">An istance of <see cref="IPopup"/>.</param>
-		public static void SetLightDismiss(this MauiPopup popup, in IPopup basePopup)
-		{
-			popup.ModalInPresentation = !basePopup.IsLightDismissEnabled;
-		}
-
-		/// <summary>
-		/// Method to update the layout of the Popup and <see cref="IPopup.Content"/>.
-		/// </summary>
-		/// <param name="popup">An instance of <see cref="MauiPopup"/>.</param>
-		/// <param name="basepopup">An istance of <see cref="IPopup"/>.</param>
-		public static void SetLayout(this MauiPopup popup, in IPopup basepopup)
-		{
-			var presentationController = popup.PresentationController;
-			var preferredContentSize = popup.PreferredContentSize;
-
-			((UIPopoverPresentationController)presentationController).SourceRect = new CGRect(0, 0, preferredContentSize.Width, preferredContentSize.Height);
-
-			if (basepopup.Anchor is null)
-			{
-				var originY = basepopup.VerticalOptions switch
-				{
-					Microsoft.Maui.Primitives.LayoutAlignment.End => UIScreen.MainScreen.Bounds.Height,
-					Microsoft.Maui.Primitives.LayoutAlignment.Center => UIScreen.MainScreen.Bounds.Height / 2,
-					_ => 0f
-				};
-
-				var originX = basepopup.HorizontalOptions switch
-				{
-					Microsoft.Maui.Primitives.LayoutAlignment.End => UIScreen.MainScreen.Bounds.Width,
-					Microsoft.Maui.Primitives.LayoutAlignment.Center => UIScreen.MainScreen.Bounds.Width / 2,
-					_ => 0f
-				};
-
-				popup.PopoverPresentationController.SourceRect = new CGRect(originX, originY, 0, 0);
-				popup.PopoverPresentationController.PermittedArrowDirections = 0;
-			}
-			else
-			{
-				var view = basepopup.Anchor.ToNative(basepopup.Handler?.MauiContext ?? throw new NullReferenceException());
-				popup.PopoverPresentationController.SourceView = view;
-				popup.PopoverPresentationController.SourceRect = view.Bounds;
-			}
-		}
+		var color = basePopup.Color?.ToNative();
+		popup.Control.NativeView.BackgroundColor = color;
 	}
+
+	/// <summary>
+	/// Method to update the <see cref="IPopup.IsLightDismissEnabled"/> property of the Popup.
+	/// </summary>
+	/// <param name="popup">An instance of <see cref="MauiPopup"/>.</param>
+	/// <param name="basePopup">An istance of <see cref="IPopup"/>.</param>
+	public static void SetLightDismiss(this MauiPopup popup, in IPopup basePopup)
+	{
+		popup.ModalInPresentation = !basePopup.IsLightDismissEnabled;
+	}
+
+	/// <summary>
+	/// Method to update the layout of the Popup and <see cref="IPopup.Content"/>.
+	/// </summary>
+	/// <param name="popup">An instance of <see cref="MauiPopup"/>.</param>
+	/// <param name="basepopup">An istance of <see cref="IPopup"/>.</param>
+	public static void SetLayout(this MauiPopup popup, in IPopup basepopup)
+	{
+		var presentationController = popup.PresentationController;
+		var preferredContentSize = popup.PreferredContentSize;
+
+		((UIPopoverPresentationController)presentationController).SourceRect = new CGRect(0, 0, preferredContentSize.Width, preferredContentSize.Height);
+
+		if (basepopup.Anchor is null)
+		{
+			var originY = basepopup.VerticalOptions switch
+			{
+				Microsoft.Maui.Primitives.LayoutAlignment.End => UIScreen.MainScreen.Bounds.Height,
+				Microsoft.Maui.Primitives.LayoutAlignment.Center => UIScreen.MainScreen.Bounds.Height / 2,
+				_ => 0f
+			};
+
+			var originX = basepopup.HorizontalOptions switch
+			{
+				Microsoft.Maui.Primitives.LayoutAlignment.End => UIScreen.MainScreen.Bounds.Width,
+				Microsoft.Maui.Primitives.LayoutAlignment.Center => UIScreen.MainScreen.Bounds.Width / 2,
+				_ => 0f
+			};
+
+			popup.PopoverPresentationController.SourceRect = new CGRect(originX, originY, 0, 0);
+			popup.PopoverPresentationController.PermittedArrowDirections = 0;
+		}
+		else
+		{
+			var view = basepopup.Anchor.ToNative(basepopup.Handler?.MauiContext ?? throw new NullReferenceException());
+			popup.PopoverPresentationController.SourceView = view;
+			popup.PopoverPresentationController.SourceRect = view.Bounds;
+		}
+
+	}
+}
