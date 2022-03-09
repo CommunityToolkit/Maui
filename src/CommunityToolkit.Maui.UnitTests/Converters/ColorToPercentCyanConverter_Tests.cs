ae5fa98690120efa9bda3e691509c3e49c714ac7
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2022-03-09 14:28:03 -0800

    Update ColorToPercentCyanConverter_Tests.cs

--- src/CommunityToolkit.Maui.UnitTests/Converters/ColorToPercentCyanConverter_Tests.cs
+++ src/CommunityToolkit.Maui.UnitTests/Converters/ColorToPercentCyanConverter_Tests.cs
@@ -31,6 +31,14 @@ public class ColorToPercentCyanConverter_Tests : BaseTest
 		new object[] { 0.5, 0.5, 0.5, 0, 0 },
 		new object[] { 0.25, 0.25, 0.25, 1, 0 },
 		new object[] { 0.25, 0.25, 0.25, 0, 0 },
+		new object[] { 0.25, 0.25, 1, 1, 0.75 },
+		new object[] { 0.25, 0.25, 1, 0, 0.75 },
+		new object[] { 0.25, 1, 0.25, 1, 0.75 },
+		new object[] { 0.25, 1, 0.25, 0, 0.75 },
+		new object[] { 0.75, 1, 0.25, 1, 0.25 },
+		new object[] { 0.75, 1, 0.25, 0, 0.25 },
+		new object[] { 0.75, 0, 1, 1, 0.25 },
+		new object[] { 0.75, 0, 1, 0, 0.25 },
 	};
 
 	[Theory]
