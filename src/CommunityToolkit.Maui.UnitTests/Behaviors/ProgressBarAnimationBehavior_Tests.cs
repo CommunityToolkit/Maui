a3593915284e9179d81570e084d01f8a84e18b0e
Shaun Lawrence <shaunrlawrence@gmail.com>
2022-03-21 09:22:36 +0000

    Update ProgressBarAnimationBehavior_Tests.cs

--- src/CommunityToolkit.Maui.UnitTests/Behaviors/ProgressBarAnimationBehavior_Tests.cs
+++ src/CommunityToolkit.Maui.UnitTests/Behaviors/ProgressBarAnimationBehavior_Tests.cs
@@ -49,7 +49,7 @@ public class ProgressBarAnimationBehavior_Tests : BaseTest
 		{
 			ArgumentNullException.ThrowIfNull(sender);
 
-			progressBarAnimationBehavior.AnimationCompleted -= HandleAnimationComplted;
+			progressBarAnimationBehavior.AnimationCompleted -= HandleAnimationCompleted;
 			progressBarAnimationCompletedTCS.SetResult();
 		}
 	}
