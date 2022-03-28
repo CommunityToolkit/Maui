296f29d6a4121ffbb26c018c48432b8af88f3665
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2022-03-27 17:53:05 -0700

    Update ColorToRgbStringConverter_Tests.cs

--- src/CommunityToolkit.Maui.UnitTests/Converters/ColorToRgbStringConverter_Tests.cs
+++ src/CommunityToolkit.Maui.UnitTests/Converters/ColorToRgbStringConverter_Tests.cs
@@ -23,24 +23,24 @@ public class ColorToRgbStringConverter_Tests : BaseTest
 		new object[] { 1, 1, 0, 1, "RGB(255,255,0)" },
 		new object[] { 1, 1, 1, 0, "RGB(255,255,255)" },
 		new object[] { 1, 1, 1, 1, "RGB(255,255,255)" },
-		new object[] { 0.5, 0, 0, 1, "RGB(127,0,0)" },
-		//new object[] { 0.5, 0, 0, 0, 1 },
-		//new object[] { 0, 0.5, 0, 1, 1 },
-		//new object[] { 0, 0.5, 0, 0, 1 },
-		//new object[] { 0, 0, 0.5, 1, 0 },
-		//new object[] { 0, 0, 0.5, 0, 0 },
-		//new object[] { 0.5, 0.5, 0.5, 1, 0 },
-		//new object[] { 0.5, 0.5, 0.5, 0, 0 },
-		//new object[] { 0.25, 0.25, 0.25, 1, 0 },
-		//new object[] { 0.25, 0.25, 0.25, 0, 0 },
-		//new object[] { 0.25, 0.25, 1, 1, 0 },
-		//new object[] { 0.25, 0.25, 1, 0, 0 },
-		//new object[] { 0.25, 1, 0.25, 1, 0.75 },
-		//new object[] { 0.25, 1, 0.25, 0, 0.75 },
-		//new object[] { 0.75, 1, 0.25, 1, 0.75 },
-		//new object[] { 0.75, 1, 0.25, 0, 0.75 },
-		//new object[] { 0.75, 0, 1, 1, 0 },
-		//new object[] { 0.75, 0, 1, 0, 0 },
+		new object[] { 0.5, 0, 0, 1, "RGB(128,0,0)" },
+		new object[] { 0.5, 0, 0, 0, "RGB(128,0,0)" },
+		new object[] { 0, 0.5, 0, 1, "RGB(0,128,0)" },
+		new object[] { 0, 0.5, 0, 0, "RGB(0,128,0)" },
+		new object[] { 0, 0, 0.5, 1, "RGB(0,0,128)" },
+		new object[] { 0, 0, 0.5, 0, "RGB(0,0,128)" },
+		new object[] { 0.5, 0.5, 0.5, 1, "RGB(128,128,128)" },
+		new object[] { 0.5, 0.5, 0.5, 0, "RGB(128,128,128)" },
+		new object[] { 0.25, 0.25, 0.25, 1, "RGB(64,64,64)" },
+		new object[] { 0.25, 0.25, 0.25, 0, "RGB(64,64,64)" },
+		new object[] { 0.25, 0.25, 1, 1, "RGB(64,64,255)" },
+		new object[] { 0.25, 0.25, 1, 0, "RGB(64,64,255)" },
+		new object[] { 0.25, 1, 0.25, 1, "RGB(64,255,64)" },
+		new object[] { 0.25, 1, 0.25, 0, "RGB(64,255,64)" },
+		new object[] { 0.75, 1, 0.25, 1, "RGB(191,255,64)" },
+		new object[] { 0.75, 1, 0.25, 0, "RGB(191,255,64)" },
+		new object[] { 0.75, 0, 1, 1, "RGB(191,0,255)" },
+		new object[] { 0.75, 0, 1, 0, "RGB(191,0,255)" },
 	};
 
 	[Theory]
@@ -50,19 +50,19 @@ public class ColorToRgbStringConverter_Tests : BaseTest
 		var converter = new ColorToRgbStringConverter();
 		var color = new Color(red, green, blue, alpha);
 
-		var resultConvertFrom = converter.ConvertFrom(color);
-		var resultConvert = converter.Convert(color, typeof(string), null, null);
+		var resultConvert = ((ICommunityToolkitValueConverter)converter).Convert(color, typeof(string), null, null);
+		var resultConvertFrom = converter.ConvertFrom(color, typeof(string), null, null);
 
-		Assert.Equal(expectedResult, resultConvertFrom);
 		Assert.Equal(expectedResult, resultConvert);
+		Assert.Equal(expectedResult, resultConvertFrom);
 	}
 
 	[Fact]
 	public void ColorToRgbStringConverterNullInputTest()
 	{
 #pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
-		Assert.Throws<ArgumentNullException>(() => new ColorToPercentYellowConverter().ConvertFrom(null));
-		Assert.Throws<ArgumentNullException>(() => new ColorToPercentYellowConverter().Convert(null, typeof(string), null, null));
+		Assert.Throws<ArgumentNullException>(() => new ColorToPercentYellowConverter().ConvertFrom(null, typeof(string), null, null));
+		Assert.Throws<ArgumentNullException>(() => ((ICommunityToolkitValueConverter)new ColorToPercentYellowConverter()).Convert(null, typeof(string), null, null));
 #pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
 	}
 }
