3fefa4d0cdf11227f7f9bf93c3b698e846776f21
pictos <pedrojesus.cefet@gmail.com>
2022-04-16 00:21:40 -0300

    Added sample for StateToBooleanConverter

---
+++ samples/CommunityToolkit.Maui.Sample/ViewModels/Converters/StateToBooleanConverterViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using CommunityToolkit.Maui.Converters;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace CommunityToolkit.Maui.Sample.ViewModels.Converters;
+public partial class StateToBooleanConverterViewModel : BaseViewModel
+{
+	[ObservableProperty]
+	LayoutState layoutstate = LayoutState.None;
+
+	public ICommand ChangeLayoutCommand { get; }
+
+	public StateToBooleanConverterViewModel()
+	{
+		ChangeLayoutCommand = new Command(ChangeLayout);
+	}
+	
+	public void ChangeLayout()
+	{
+		Layoutstate = Layoutstate == LayoutState.None ? LayoutState.Success : LayoutState.None;
+	}
+}
