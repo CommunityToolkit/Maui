82a148c935cc9f3fdc8787bf4638ecd71e7a0ef7
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2022-05-18 20:37:20 -0700

    Make MultiMathExpressionConverterPage Interactive

--- samples/CommunityToolkit.Maui.Sample/ViewModels/Converters/MultiMathExpressionConverterViewModel.cs
+++ samples/CommunityToolkit.Maui.Sample/ViewModels/Converters/MultiMathExpressionConverterViewModel.cs
@@ -1,8 +1,15 @@
-﻿namespace CommunityToolkit.Maui.Sample.ViewModels.Converters;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace CommunityToolkit.Maui.Sample.ViewModels.Converters;
 
 public partial class MultiMathExpressionConverterViewModel : BaseViewModel
 {
-	public double X0 => 10;
-	public double X1 => 20;
-	public double X2 => 30;
+	[ObservableProperty]
+	double x0 = 10;
+
+	[ObservableProperty]
+	double x1 = 20;
+
+	[ObservableProperty]
+	double x2 = 30;
 }
