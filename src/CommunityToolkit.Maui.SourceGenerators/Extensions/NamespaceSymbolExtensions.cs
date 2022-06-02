654173196afd02c55ae0f9541c2913565afbaa15
Pavel Anpin <pavel@apes.ge>
2022-06-02 14:48:41 +0400

    feat: extensions for serializing  generic type arguments

--- src/CommunityToolkit.Maui.SourceGenerators/Extensions/NamespaceSymbolExtensions.cs
+++ src/CommunityToolkit.Maui.SourceGenerators/Extensions/NamespaceSymbolExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.CodeAnalysis;
-
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
 namespace CommunityToolkit.Maui.SourceGenerators.Extensions;
 
 static class NamespaceSymbolExtensions
@@ -25,5 +27,125 @@ static class NamespaceSymbolExtensions
 				}
 			}
 		}
+	}
+	
+	public static string GetFullTypeString(this INamedTypeSymbol type) => 
+		type.Name + 
+		(type.NullableAnnotation == NullableAnnotation.Annotated ? "?" : string.Empty) +
+		type.TypeArguments.GetGenericTypeArgumentsString();
+
+	public static string GetGenericTypeArgumentsString(this IEnumerable<ITypeSymbol> typeArguments)
+	{
+		string result = string.Empty;
+
+		if (!typeArguments.Any())
+		{
+			return result;
+		}
+
+		result += "<";
+
+		bool isFirstArgument = true;
+		foreach (ITypeSymbol typeArg in typeArguments)
+		{
+			if (isFirstArgument)
+			{
+				isFirstArgument = false;
+			}
+			else
+			{
+				result += ", ";
+			}
+
+			switch (typeArg)
+			{
+				case ITypeParameterSymbol typeParameterSymbol:
+					result += typeParameterSymbol.Name;
+					break;
+				case INamedTypeSymbol namedTypeSymbol:
+					result += namedTypeSymbol.GetFullTypeString();
+					break;
+				default:
+					//Maybe should handle this somehow, maybe inject SourceProductionContext and call ReportDiagnostic on it?
+					break;
+			}
+
+		}
+
+		result += ">";
+
+		return result;
+	}
+
+	public static string GetWhereStatement(this INamedTypeSymbol type)
+	{
+		string result = string.Empty;
+		if (!type.TypeParameters.Any())
+		{
+			return result;
+		}
+
+		foreach (var typeParameterSymbol in type.TypeParameters)
+		{
+			string constraints = "";
+
+			bool isFirstConstraint = true;
+
+			if (typeParameterSymbol.HasNotNullConstraint)
+			{
+				constraints += "notnull";
+
+				isFirstConstraint = false;
+			}
+			else if (typeParameterSymbol.HasUnmanagedTypeConstraint)
+			{
+				constraints += "unmanaged";
+
+				isFirstConstraint = false;
+			}
+			else if (typeParameterSymbol.HasReferenceTypeConstraint)
+			{
+				constraints += "class";
+
+				isFirstConstraint = false;
+			}
+			else if (typeParameterSymbol.HasValueTypeConstraint)
+			{
+				constraints += "struct";
+
+				isFirstConstraint = false;
+			}
+
+			foreach (INamedTypeSymbol contstraintType in typeParameterSymbol.ConstraintTypes)
+			{
+				if (!isFirstConstraint)
+				{
+					constraints += ", ";
+				}
+				else
+				{
+					isFirstConstraint = false;
+				}
+
+				constraints += contstraintType.GetFullTypeString();
+			}
+
+			if (typeParameterSymbol.HasConstructorConstraint)
+			{
+				if (!isFirstConstraint)
+				{
+					constraints += ", ";
+				}
+				constraints += "new()";
+			}
+
+			if (!string.IsNullOrEmpty(constraints))
+			{
+				result += "where " + typeParameterSymbol.Name + " : " + constraints + @"
+";
+			}
+		}
+
+		return result;
 	}
 }
