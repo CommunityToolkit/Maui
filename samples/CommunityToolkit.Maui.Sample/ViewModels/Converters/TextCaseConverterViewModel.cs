611a3ef41c389979749d6f5f56352add5aecc278
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2022-06-10 19:41:26 -0700

    Revert "Update TextCaseConverterViewModel.cs"
    This reverts commit 51f23015df973b414b8dff2267849ac8d2bfcb1b.

--- samples/CommunityToolkit.Maui.Sample/ViewModels/Converters/TextCaseConverterViewModel.cs
+++ samples/CommunityToolkit.Maui.Sample/ViewModels/Converters/TextCaseConverterViewModel.cs
@@ -2,7 +2,7 @@
 
 namespace CommunityToolkit.Maui.Sample.ViewModels.Converters;
 
-public partial class TextCaseConverterViewModel : BaseViewModel
+public class TextCaseConverterViewModel : BaseViewModel
 {
 	[ObservableProperty]
 	string input = string.Empty;
