80b3c8c6941dc3c56cf6eef244ea2df8ff8aaca3
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2022-06-29 18:21:23 -0700

    Add Analyzer

---
+++ src/CommunityToolkit.Maui.Analyzers/ToolkitInitializationAnalyzer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace CommunityToolkit.Maui.Analyzers;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public class ToolkitInitializationAnalyzer : DiagnosticAnalyzer
+{
+	public const string DiagnosticId = nameof(ToolkitInitializationAnalyzer);
+
+	const string category = "Naming";
+	static readonly LocalizableString title = new LocalizableResourceString(nameof(Resources.InitializationErrorTitle), Resources.ResourceManager, typeof(Resources));
+	static readonly LocalizableString messageFormat = new LocalizableResourceString(nameof(Resources.IniitalizationMessageFormat), Resources.ResourceManager, typeof(Resources));
+	static readonly LocalizableString description = new LocalizableResourceString(nameof(Resources.InitializationErrorMessage), Resources.ResourceManager, typeof(Resources));
+
+	static readonly DiagnosticDescriptor rule = new(DiagnosticId, title, messageFormat, category, DiagnosticSeverity.Error, isEnabledByDefault: true, description: description);
+
+	public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create(rule);
+
+	public override void Initialize(AnalysisContext context)
+	{
+		context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+		context.EnableConcurrentExecution();
+		context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.LocalDeclarationStatement);
+	}
+
+	static void AnalyzeNode(SyntaxNodeAnalysisContext context)
+	{
+		var localDeclaration = (LocalDeclarationStatementSyntax)context.Node;
+
+		if (localDeclaration.Declaration.Variables.ToString().Contains("UseMauiApp<")
+			&& !localDeclaration.Declaration.ToString().Contains(".UseMauiCommunityToolkit("))
+		{
+			var diagnostic = Diagnostic.Create(rule, localDeclaration.GetLocation());
+
+			context.ReportDiagnostic(diagnostic);
+		}
+	}
+}
