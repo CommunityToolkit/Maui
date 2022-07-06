ff297f3dc33ad6f514821d4b7eb8a9d590732fce
George Leithead <github@internetwideworld.com>
2022-07-06 09:07:36 +0100

    Update samples/CommunityToolkit.Maui.Sample/ViewModels/ViewControls/AvatarControlViewModel.cs
    Co-authored-by: Brandon Minnick <13558917+brminnick@users.noreply.github.com>

--- samples/CommunityToolkit.Maui.Sample/ViewModels/ViewControls/AvatarControlViewModel.cs
+++ samples/CommunityToolkit.Maui.Sample/ViewModels/ViewControls/AvatarControlViewModel.cs
@@ -5,10 +5,5 @@ namespace CommunityToolkit.Maui.Sample.ViewModels.ViewControls;
 /// <summary>Sample Avatar control view model.</summary>
 public sealed partial class AvatarControlViewModel : BaseViewModel
 {
-	public ObservableCollection<AvatarModel> AvatarList { get; }
-
-	public AvatarControlViewModel()
-	{
-		AvatarList = new ObservableCollection<AvatarModel>(AvatarData.Avatars);
-	}
+	public ObservableCollection<AvatarModel> AvatarList { get; } = new();
 }
