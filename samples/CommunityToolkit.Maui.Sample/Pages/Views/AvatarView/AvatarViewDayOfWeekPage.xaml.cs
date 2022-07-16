28f5035330be94d3910a91424f13f7b78b1ec081
github@internetwideworld.com <github@internetwideworld.com>
2022-07-16 14:50:03 +0100

    Clean up and consistency for AvatarView pages.

--- samples/CommunityToolkit.Maui.Sample/Pages/Views/AvatarView/AvatarViewDayOfWeekPage.xaml.cs
+++ samples/CommunityToolkit.Maui.Sample/Pages/Views/AvatarView/AvatarViewDayOfWeekPage.xaml.cs
@@ -1,13 +1,8 @@
-using CommunityToolkit.Maui.Sample.Models;
 using CommunityToolkit.Maui.Sample.ViewModels.Views.AvatarView;
-using CommunityToolkit.Maui.Views;
 
 namespace CommunityToolkit.Maui.Sample.Pages.Views;
 
 public partial class AvatarViewDayOfWeekPage : BasePage<AvatarViewDayOfWeekViewModel>
 {
-	public AvatarViewDayOfWeekPage(AvatarViewDayOfWeekViewModel avatarViewDayOfWeekViewModel) : base(avatarViewDayOfWeekViewModel)
-	{
-		InitializeComponent();
-	}
+	public AvatarViewDayOfWeekPage(AvatarViewDayOfWeekViewModel avatarViewDayOfWeekViewModel) : base(avatarViewDayOfWeekViewModel) => InitializeComponent();
 }
--- samples/CommunityToolkit.Maui.Sample/Pages/Views/AvatarView/AvatarViewSizesPage.xaml.cs
+++ samples/CommunityToolkit.Maui.Sample/Pages/Views/AvatarView/AvatarViewSizesPage.xaml.cs
@@ -1,13 +1,8 @@
-using CommunityToolkit.Maui.Sample.Models;
 using CommunityToolkit.Maui.Sample.ViewModels.Views.AvatarView;
-using CommunityToolkit.Maui.Views;
 
 namespace CommunityToolkit.Maui.Sample.Pages.Views;
 
 public partial class AvatarViewSizesPage : BasePage<AvatarViewSizesViewModel>
 {
-	public AvatarViewSizesPage(AvatarViewSizesViewModel avatarViewSizesViewModel) : base(avatarViewSizesViewModel)
-	{
-		InitializeComponent();
-	}
+	public AvatarViewSizesPage(AvatarViewSizesViewModel avatarViewSizesViewModel) : base(avatarViewSizesViewModel) => InitializeComponent();
 }
