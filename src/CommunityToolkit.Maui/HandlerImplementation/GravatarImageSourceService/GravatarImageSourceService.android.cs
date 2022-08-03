0bca6f1cb38f2cceabe1cd7140d8b059d039a0ab
github@internetwideworld.com <github@internetwideworld.com>
2022-08-03 13:05:32 +0100

    removed unnecessary create and await task.

--- src/CommunityToolkit.Maui/HandlerImplementation/GravatarImageSourceService/GravatarImageSourceService.android.cs
+++ src/CommunityToolkit.Maui/HandlerImplementation/GravatarImageSourceService/GravatarImageSourceService.android.cs
@@ -10,42 +10,42 @@ public partial class GravatarImageSourceService
 	public override async Task<IImageSourceServiceResult?> LoadDrawableAsync(IImageSource imageSource, Android.Widget.ImageView imageView, CancellationToken cancellationToken = default)
 	{
 		IGravatarImageSource gravatarImageSource = (IGravatarImageSource)imageSource;
-		if (!gravatarImageSource.IsEmpty && gravatarImageSource.Uri is not null)
+		if (gravatarImageSource.IsEmpty || gravatarImageSource.Uri is null)
 		{
-			try
-			{
-				ImageLoaderCallback callback = new();
-				PlatformInterop.LoadImageFromUri(imageView, gravatarImageSource.Uri.OriginalString, new Java.Lang.Boolean(gravatarImageSource.CachingEnabled), callback);
-				return await callback.Result;
-			}
-			catch (Exception ex)
-			{
-				Logger?.LogWarning(ex, "Unable to load image URI '{Uri}'.", gravatarImageSource.Uri.OriginalString);
-				throw;
-			}
+			return null;
 		}
 
-		return await Task.FromResult<IImageSourceServiceResult?>(null);
+		try
+		{
+			ImageLoaderCallback callback = new();
+			PlatformInterop.LoadImageFromUri(imageView, gravatarImageSource.Uri.OriginalString, new Java.Lang.Boolean(gravatarImageSource.CachingEnabled), callback);
+			return await callback.Result;
+		}
+		catch (Exception ex)
+		{
+			Logger?.LogWarning(ex, "Unable to load image URI '{Uri}'.", gravatarImageSource.Uri.OriginalString);
+			throw;
+		}
 	}
 
 	public override async Task<IImageSourceServiceResult<Drawable>?> GetDrawableAsync(IImageSource imageSource, Context context, CancellationToken cancellationToken = default)
 	{
 		IGravatarImageSource gravatarImageSource = (IGravatarImageSource)imageSource;
-		if (!gravatarImageSource.IsEmpty && gravatarImageSource.Uri is not null)
+		if (gravatarImageSource.IsEmpty || gravatarImageSource.Uri is null)
 		{
-			try
-			{
-				ImageLoaderResultCallback drawableCallback = new();
-				PlatformInterop.LoadImageFromUri(context, gravatarImageSource.Uri.OriginalString, new Java.Lang.Boolean(gravatarImageSource.CachingEnabled), drawableCallback);
-				return await drawableCallback.Result;
-			}
-			catch (Exception ex)
-			{
-				Logger?.LogWarning(ex, "Unable to load image URI '{Uri}'.", gravatarImageSource.Uri.OriginalString);
-				throw;
-			}
+			return null;
 		}
 
-		return await Task.FromResult<IImageSourceServiceResult<Drawable>?>(null);
+		try
+		{
+			ImageLoaderResultCallback drawableCallback = new();
+			PlatformInterop.LoadImageFromUri(context, gravatarImageSource.Uri.OriginalString, new Java.Lang.Boolean(gravatarImageSource.CachingEnabled), drawableCallback);
+			return await drawableCallback.Result;
+		}
+		catch (Exception ex)
+		{
+			Logger?.LogWarning(ex, "Unable to load image URI '{Uri}'.", gravatarImageSource.Uri.OriginalString);
+			throw;
+		}
 	}
 }
