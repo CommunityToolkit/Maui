dded822d57b91f3a44b16f2e384be1f37551f6b8
github@internetwideworld.com <github@internetwideworld.com>
2022-08-03 12:53:58 +0100

    Moved default image enum to Primatives folder.

--- src/CommunityToolkit.Maui.Core/Primitives/Defaults/GravatarImageSourceDefaults.shared.cs
+++ src/CommunityToolkit.Maui.Core/Primitives/Defaults/GravatarImageSourceDefaults.shared.cs
@@ -18,3 +18,31 @@ public static class GravatarImageSourceDefaults
 	[EditorBrowsable(EditorBrowsableState.Never)]
 	public const int DefaultSize = 80;
 }
+
+/// <summary>Default gravatar image enumerator.</summary>
+public enum DefaultImage
+{
+	/// <summary>(mystery-person) A simple, cartoon-style silhouetted outline of a person (does not vary by email hash)</summary>
+	MysteryPerson = 0,
+
+	/// <summary>404: Do not load any image if none is associated with the email hash, instead return an HTTP 404 (File Not Found) response.</summary>
+	FileNotFound,
+
+	/// <summary>A geometric pattern based on an email hash.</summary>
+	Identicon,
+
+	/// <summary>A generated 'monster' with different colours, faces, etc.</summary>
+	MonsterId,
+
+	/// <summary>Generated faces with differing features and backgrounds.</summary>
+	Wavatar,
+
+	/// <summary>Awesome generated, 8-bit arcade-style pixilated faces.</summary>
+	Retro,
+
+	/// <summary>A generated robot with different colours, faces, etc.</summary>
+	Robohash,
+
+	/// <summary>A transparent PNG image.</summary>
+	Blank,
+}
