528428abacb66f23bb6fcddb6d737705a288c10a
github@internetwideworld.com <github@internetwideworld.com>
2022-08-04 16:17:07 +0100

    Added unit tests

---
+++ src/CommunityToolkit.Maui.UnitTests/Views/GravatarImageSource/GravatarImageSourceServiceTests.cs
@@ -0,0 +1,40 @@
+﻿using CommunityToolkit.Maui.Core;
+using CommunityToolkit.Maui.Views;
+using Xunit;
+
+namespace CommunityToolkit.Maui.UnitTests.Views.GravatarImageSource;
+
+public class GravatarImageSourceServiceTests : BaseTest
+{
+	[Fact]
+	public void ResolvesToConcreteTypeOverInterface()
+	{
+		var provider = CreateImageSourceServiceProvider(services => _ = services.AddService<IGravatarImageSource, GravatarImageSourceService>());
+		var service = provider.GetRequiredImageSourceService(new GravatarImageImageSourceStub());
+		Assert.IsType<GravatarImageSourceService>(service);
+	}
+
+	[Fact]
+	public void CanResolveCorrectServiceWhenMultiple()
+	{
+		var provider = CreateImageSourceServiceProvider(services =>
+		{
+			services.AddService<IGravatarImageSource, GravatarImageSourceService>();
+			services.AddService<IUriImageSource, UriImageSourceService>();
+		});
+
+		var service = provider.GetRequiredImageSourceService(new GravatarImageImageSourceStub());
+		Assert.IsType<GravatarImageSourceService>(service);
+
+		service = provider.GetRequiredImageSourceService(new UriImageSourceStub());
+		Assert.IsType<UriImageSourceService>(service);
+	}
+
+	static IImageSourceServiceProvider CreateImageSourceServiceProvider(Action<IImageSourceServiceCollection> configure)
+	{
+		var mauiApp = MauiApp.CreateBuilder()
+			.ConfigureImageSources(configure)
+			.Build();
+		return mauiApp.Services.GetRequiredService<IImageSourceServiceProvider>();
+	}
+}
---
+++ src/CommunityToolkit.Maui.UnitTests/Views/GravatarImageSource/GravatarImageSourceTests.cs
@@ -0,0 +1,119 @@
+﻿namespace CommunityToolkit.Maui.UnitTests.Views.GravatarImageSource;
+using CommunityToolkit.Maui.Core;
+using Xunit;
+
+public class GravatarImageSourceTests : BaseHandlerTest
+{
+	readonly string email = "dsiegel@avantipoint.com";
+	readonly TimeSpan cacheValidity = new(1, 0, 0);
+	readonly bool cachingEnabled = false;
+	readonly DefaultImage image = DefaultImage.MonsterId;
+
+	public GravatarImageSourceTests()
+	{
+		Assert.IsAssignableFrom<IGravatarImageSource>(new Maui.Views.GravatarImageSource());
+	}
+
+	[Fact]
+	public void ConstructorTest()
+	{
+		var gravatarImageSource = new Maui.Views.GravatarImageSource()
+		{
+			CacheValidity = cacheValidity,
+			CachingEnabled = cachingEnabled,
+			Email = email,
+			Image = image
+		};
+
+		Assert.Equal(cacheValidity, gravatarImageSource.CacheValidity);
+		Assert.True(cachingEnabled == gravatarImageSource.CachingEnabled);
+		Assert.Equal(email, gravatarImageSource.Email);
+		Assert.Equal(image, gravatarImageSource.Image);
+	}
+
+	[Fact]
+	public void DefaultDefaultImage()
+	{
+		var gravatarImageSource = new Maui.Views.GravatarImageSource();
+		Assert.Equal(GravatarImageSourceDefaults.Defaultimage, gravatarImageSource.Image);
+	}
+
+	[Fact]
+	public void DefaultEmail()
+	{
+		var gravatarImageSource = new Maui.Views.GravatarImageSource();
+		Assert.Equal(GravatarImageSourceDefaults.DefaultEmail, gravatarImageSource.Email);
+	}
+
+	[Fact]
+	public void DefaultCacheValidity()
+	{
+		var gravatarImageSource = new Maui.Views.GravatarImageSource();
+		Assert.Equal(new TimeSpan(1, 0, 0, 0), gravatarImageSource.CacheValidity);
+	}
+
+	[Fact]
+	public void DefaultCachingEnabled()
+	{
+		var gravatarImageSource = new Maui.Views.GravatarImageSource();
+		Assert.True(gravatarImageSource.CachingEnabled);
+	}
+
+	[Fact]
+	public void DefaultUri()
+	{
+		var gravatarImageSource = new Maui.Views.GravatarImageSource();
+		Assert.Null(gravatarImageSource.Uri);
+	}
+
+	[Fact]
+	public void TestChangeImage()
+	{
+		var gravatarImageSource = new Maui.Views.GravatarImageSource
+		{
+			Image = image
+		};
+		Assert.Equal(image, gravatarImageSource.Image);
+		Assert.Equal(new Uri("https://www.gravatar.com/avatar/dbe9d2cc85a3f41cc430bc817e55233b?s=0&d=monsterid"), gravatarImageSource.Uri);
+	}
+
+	[Fact]
+	public void TestChangeEmail()
+	{
+		var gravatarImageSource = new Maui.Views.GravatarImageSource
+		{
+			Email = email
+		};
+		Assert.Equal(new Uri("https://www.gravatar.com/avatar/b65a519785f69fbe7236dd0fd6396094?s=0&d=mp"), gravatarImageSource.Uri);
+	}
+
+	[Fact]
+	public void TestImageControl()
+	{
+		Image testImage = new()
+		{
+			Source = new Maui.Views.GravatarImageSource()
+		};
+		Assert.True(testImage.Source is Maui.Views.GravatarImageSource);
+		Assert.Equal(new Uri("https://www.gravatar.com/avatar/dbe9d2cc85a3f41cc430bc817e55233b?s=80&d=mp"), ((Maui.Views.GravatarImageSource)testImage.Source).Uri);
+	}
+
+	[Fact]
+	public void NullEmailDoesNotCrash()
+	{
+		var loader = new Maui.Views.GravatarImageSource();
+		var exception = Record.Exception(() => loader.Email = null);
+		Assert.Null(exception);
+	}
+
+	[Fact]
+	public void LoadImageFromStream()
+	{
+		IGravatarImageSource loader = new Maui.Views.GravatarImageSource
+		{
+			Email = email
+		};
+		Stream s0 = loader.GetStreamAsync().Result;
+		Assert.Equal(3429, s0.Length);
+	}
+}
