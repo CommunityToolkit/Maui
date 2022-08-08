56201c287a3ee25275d989a421a94499a8eacb7d
github@internetwideworld.com <github@internetwideworld.com>
2022-08-08 11:24:26 +0100

    ReadOnlySpan<char> instead of string?.

--- src/CommunityToolkit.Maui/Views/GravatarImageSource.shared.cs
+++ src/CommunityToolkit.Maui/Views/GravatarImageSource.shared.cs
@@ -121,15 +121,15 @@ public class GravatarImageSource : StreamImageSource
 				_ => $"{defaultGravatar}".ToLower(),
 			};
 
-	static string GetMd5Hash(string? str)
+	static string GetMd5Hash(ReadOnlySpan<char> str)
 	{
-		if (string.IsNullOrWhiteSpace(str))
+		if (str.Length is 0)
 		{
 			return string.Empty;
 		}
 
 		using var md5 = MD5.Create();
-		Span<byte> hash = md5.ComputeHash(Encoding.UTF8.GetBytes(str));
+		Span<byte> hash = md5.ComputeHash(Encoding.UTF8.GetBytes(str.ToArray()));
 		return BitConverter.ToString(hash.ToArray(), 0, hash.Length).Replace("-", string.Empty, StringComparison.OrdinalIgnoreCase).ToLowerInvariant();
 	}
 
