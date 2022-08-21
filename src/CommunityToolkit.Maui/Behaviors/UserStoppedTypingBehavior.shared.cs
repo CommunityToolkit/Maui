6574e5dd84d0f9de0b6b80c810571c32f86d0921
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2022-08-21 16:40:57 -0700

    Update UserStoppedTypingBehavior.shared.cs

--- src/CommunityToolkit.Maui/Behaviors/UserStoppedTypingBehavior.shared.cs
+++ src/CommunityToolkit.Maui/Behaviors/UserStoppedTypingBehavior.shared.cs
@@ -92,7 +92,6 @@ public class UserStoppedTypingBehavior : BaseBehavior<InputView>, IDisposable
 	/// <inheritdoc />
 	public void Dispose()
 	{
-		// Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
 		Dispose(disposing: true);
 		GC.SuppressFinalize(this);
 	}
