c5aa864f7a94bd86ffac5d97f7166c57d070564c
Botirali <105649270+Rashidovicc@users.noreply.github.com>
2022-09-05 15:04:45 +0300

    Update PopupExtensions.shared.cs
    I set it to be the same as the exception below

--- src/CommunityToolkit.Maui/Views/Popup/PopupExtensions.shared.cs
+++ src/CommunityToolkit.Maui/Views/Popup/PopupExtensions.shared.cs
@@ -74,7 +74,7 @@ public static partial class PopupExtensions
 public static partial class PopupExtensions
 {
 	static void PlatformShowPopup(Popup popup, IMauiContext mauiContext) =>
-		throw new NotSupportedException($"The current platform '{DeviceInfo.Platform}' does not support CommunityToolkit.Maui.Core.Popup");
+		throw new NotSupportedException($"The current platform '{DeviceInfo.Platform}' does not support CommunityToolkit.Maui.Core.Popup.");
 
 	static Task<object?> PlatformShowPopupAsync(Popup popup, IMauiContext mauiContext) =>
 		throw new NotSupportedException($"The current platform '{DeviceInfo.Platform}' does not support CommunityToolkit.Maui.Core.Popup.");
