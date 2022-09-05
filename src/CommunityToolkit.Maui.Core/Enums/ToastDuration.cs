d8c6e08554c4280a6befa91ff8949cdb42559527
Muhammad <102380592+MuhammadKasimov@users.noreply.github.com>
2022-09-05 16:52:10 +0500

    Create ToastDuration.cs
    I added new folder for enums because enum was opened in interface folder

---
+++ src/CommunityToolkit.Maui.Core/Enums/ToastDuration.cs
@@ -0,0 +1,16 @@
+namespace CommunityToolkit.Maui.Core;
+/// <summary>
+/// Toast duration
+/// </summary>
+public enum ToastDuration
+{
+	/// <summary>
+	/// Displays Toast for a short time
+	/// </summary>
+	Short,
+
+	/// <summary>
+	/// Displays Toast for a long time
+	/// </summary>
+	Long
+}
