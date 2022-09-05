0bca5cb724ac095c2ba5aa77fe7c7f4e7f0180ca
Firdavs <103358030+FirdavsIbra@users.noreply.github.com>
2022-09-05 15:00:36 +0500

    Update ImpliedOrderGridBehavior.shared.cs
     In addition, letter case should be default case, old version is pascal case.

--- src/CommunityToolkit.Maui/Behaviors/ImpliedOrderGridBehavior.shared.cs
+++ src/CommunityToolkit.Maui/Behaviors/ImpliedOrderGridBehavior.shared.cs
@@ -115,7 +115,7 @@ public class ImpliedOrderGridBehavior : BaseBehavior<Grid>
 		{
 			for (var c = column; c < columnEnd; c++)
 			{
-				if (usedMatrix?[r][c] ?? throw new InvalidOperationException("Grid Child Not Added"))
+				if (usedMatrix?[r][c] ?? throw new InvalidOperationException("Grid child not added"))
 				{
 					LogWarning($"Cell at row {r} column {c} has already been used.", ThrowOnLayoutWarning);
 				}
