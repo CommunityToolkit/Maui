71e5a696673db0b299df6ecceb39eeaae697f929
Pedro Jesus <pedrojesus.cefet@gmail.com>
2022-09-07 22:42:50 -0300

    Implemented Statusbar color as a Behavior

---
+++ src/CommunityToolkit.Maui/Behaviors/PlatformBehaviors/StatusBar/StatusBarBehavior.shared.cs
@@ -0,0 +1,37 @@
+
+namespace CommunityToolkit.Maui.Behaviors;
+
+/// <summary>
+/// <see cref="PlatformBehavior{TView,TPlatformView}"/> that controls the Status bar color
+/// </summary>
+public class StatusBarBehavior : PlatformBehavior<VisualElement>
+{
+	/// <summary>
+	/// <see cref="BindableProperty"/> that manages the StatusBarColor property.
+	/// </summary>
+	public static readonly BindableProperty StatusBarColorProperty =
+		BindableProperty.Create(nameof(StatusBarColor), typeof(Color), typeof(StatusBarBehavior));
+
+	/// <summary>
+	/// Property that holds the value of the Status bar color. 
+	/// </summary>
+	public Color StatusBarColor
+	{
+		get => (Color)GetValue(StatusBarColorProperty);
+		set => SetValue(StatusBarColorProperty, value);
+	}
+
+	/// <inheritdoc /> 
+#if IOS || MACCATALYST
+	protected override void OnAttachedTo(VisualElement bindable, UIKit.UIView platformView)
+#elif ANDROID
+	protected override void OnAttachedTo(VisualElement bindable, Android.Views.View platformView)
+#else
+	protected override void OnAttachedTo(VisualElement bindable, object platformView)
+#endif
+	{
+		Core.Capabilities.StatusBar.SetColor(StatusBarColor);
+	}
+
+
+}
