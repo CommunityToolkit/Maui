c9347c71b9392463e63addda775d97cdce8e55d6
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2022-09-15 13:46:30 -0700

    Create StatusBarTests.cs

---
+++ src/CommunityToolkit.Maui.UnitTests/Behaviors/StatusBarTests.cs
@@ -0,0 +1,18 @@
+﻿using CommunityToolkit.Maui.Behaviors;
+using CommunityToolkit.Maui.Core;
+using Xunit;
+
+namespace CommunityToolkit.Maui.UnitTests;
+
+public class StatusBarTests : BaseTest
+{
+	[Fact]
+	public void VerifyDefaults()
+	{
+		var statusBarBehavior = new StatusBarBehavior();
+
+		Assert.Equal(Colors.Transparent, statusBarBehavior.StatusBarColor);
+		Assert.Equal(Core.StatusBarStyle.Default, statusBarBehavior.StatusBarStyle);
+	}
+}
+
