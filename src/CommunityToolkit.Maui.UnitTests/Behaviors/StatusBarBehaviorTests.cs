04c3bd1d72808f3d8fff2b8b8214510a59921687
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2022-09-15 13:55:59 -0700

    Update StatusBarBehaviorTests.cs

--- src/CommunityToolkit.Maui.UnitTests/Behaviors/StatusBarBehaviorTests.cs
+++ src/CommunityToolkit.Maui.UnitTests/Behaviors/StatusBarBehaviorTests.cs
@@ -2,9 +2,9 @@
 using CommunityToolkit.Maui.Core;
 using Xunit;
 
-namespace CommunityToolkit.Maui.UnitTests;
+namespace CommunityToolkit.Maui.UnitTests.Behaviors;
 
-public class StatusBarTests : BaseTest
+public class StatusBarBehaviorTests : BaseTest
 {
 	[Fact]
 	public void VerifyDefaults()
@@ -13,6 +13,29 @@ public class StatusBarTests : BaseTest
 
 		Assert.Equal(Colors.Transparent, statusBarBehavior.StatusBarColor);
 		Assert.Equal(Core.StatusBarStyle.Default, statusBarBehavior.StatusBarStyle);
+	}
+
+	[Fact]
+	public void VerifyAttachToPageSuceedes()
+	{
+		var statusBarBehavior = new StatusBarBehavior();
+
+		var contentPage = new Page
+		{
+			Behaviors = { statusBarBehavior }
+		};
+
+		Assert.Single(contentPage.Behaviors.OfType<StatusBarBehavior>());
+	}
+
+	[Fact]
+	public void VerifyAttachToViewLabelFails()
+	{
+		var statusBarBehavior = new StatusBarBehavior();
+
+		var view = new View();
+
+		Assert.Throws<InvalidOperationException>(() => view.Behaviors.Add(statusBarBehavior));
 	}
 }
 
