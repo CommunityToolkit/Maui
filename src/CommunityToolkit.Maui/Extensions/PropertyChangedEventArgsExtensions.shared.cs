0936058804ca9edd99135b2865011a483ad3c053
Pedro Jesus <pedrojesus.cefet@gmail.com>
2022-09-17 18:22:42 -0300

    dotnet format

--- src/CommunityToolkit.Maui/Extensions/PropertyChangedEventArgsExtensions.shared.cs
+++ src/CommunityToolkit.Maui/Extensions/PropertyChangedEventArgsExtensions.shared.cs
@@ -4,45 +4,43 @@ using System.Runtime.CompilerServices;
 
 namespace CommunityToolkit.Maui.Extensions;
 
-    static class PropertyChangedEventArgsExtensions
-    {
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static bool Is(this string propertyName, BindableProperty property)
-		{
-			return propertyName == property.PropertyName;
-		}
+static class PropertyChangedEventArgsExtensions
+{
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static bool Is(this string propertyName, BindableProperty property) =>
+		propertyName == property.PropertyName;
 
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static bool IsOneOf(this string propertyName, BindableProperty p0, BindableProperty p1)
-		{
-			return propertyName == p0.PropertyName ||
-				propertyName == p1.PropertyName;
-		}
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static bool IsOneOf(this string propertyName, BindableProperty p0, BindableProperty p1)
+	{
+		return propertyName == p0.PropertyName ||
+			propertyName == p1.PropertyName;
+	}
 
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static bool IsOneOf(this string propertyName, BindableProperty p0, BindableProperty p1, BindableProperty p2)
-		{
-			return propertyName == p0.PropertyName ||
-				propertyName == p1.PropertyName ||
-				propertyName == p2.PropertyName;
-		}
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static bool IsOneOf(this string propertyName, BindableProperty p0, BindableProperty p1, BindableProperty p2)
+	{
+		return propertyName == p0.PropertyName ||
+			propertyName == p1.PropertyName ||
+			propertyName == p2.PropertyName;
+	}
 
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static bool IsOneOf(this string propertyName, BindableProperty p0, BindableProperty p1, BindableProperty p2, BindableProperty p3)
-		{
-			return propertyName == p0.PropertyName ||
-				propertyName == p1.PropertyName ||
-				propertyName == p2.PropertyName ||
-				propertyName == p3.PropertyName;
-		}
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static bool IsOneOf(this string propertyName, BindableProperty p0, BindableProperty p1, BindableProperty p2, BindableProperty p3)
+	{
+		return propertyName == p0.PropertyName ||
+			propertyName == p1.PropertyName ||
+			propertyName == p2.PropertyName ||
+			propertyName == p3.PropertyName;
+	}
 
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static bool IsOneOf(this string propertyName, BindableProperty p0, BindableProperty p1, BindableProperty p2, BindableProperty p3, BindableProperty p4)
-		{
-			return propertyName == p0.PropertyName ||
-				propertyName == p1.PropertyName ||
-				propertyName == p2.PropertyName ||
-				propertyName == p3.PropertyName ||
-				propertyName == p4.PropertyName;
-		}
-    }
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static bool IsOneOf(this string propertyName, BindableProperty p0, BindableProperty p1, BindableProperty p2, BindableProperty p3, BindableProperty p4)
+	{
+		return propertyName == p0.PropertyName ||
+			propertyName == p1.PropertyName ||
+			propertyName == p2.PropertyName ||
+			propertyName == p3.PropertyName ||
+			propertyName == p4.PropertyName;
+	}
+}
