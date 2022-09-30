871aed1fc0a49bd70eb9b2f725568af9a5e9208d
Shaun Lawrence <shaunrlawrence@gmail.com>
2022-09-30 10:11:37 +0100

    Swap to using IValueConverter for the EventToCommandBehavior

---
+++ src/CommunityToolkit.Maui.UnitTests/Mocks/MockValueConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+namespace CommunityToolkit.Maui.UnitTests.Mocks;
+
+class MockValueConverter : IValueConverter
+{
+	readonly Func<object, object> returnValue;
+
+	public MockValueConverter(Func<object, object> returnValue)
+	{
+		this.returnValue = returnValue;
+	}
+
+	public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		return returnValue.Invoke(value);
+	}
+
+	public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		return returnValue.Invoke(value);
+	}
+}
