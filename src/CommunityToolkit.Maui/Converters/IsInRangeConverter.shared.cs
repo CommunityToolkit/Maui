a515f4e820b741da72784333c5b40b6e9b8461ec
github@internetwideworld.com <github@internetwideworld.com>
2022-11-04 09:05:30 +0000

    Updated Min to MinValue Updated Max to MaxValue

--- src/CommunityToolkit.Maui/Converters/IsInRangeConverter.shared.cs
+++ src/CommunityToolkit.Maui/Converters/IsInRangeConverter.shared.cs
@@ -14,10 +14,10 @@ public abstract class IsInRangeConverter<TObject> : BaseConverterOneWay<ICompara
 	public override object DefaultConvertReturnValue { get; set; } = new();
 
 	/// <summary>Minimum value.</summary>
-	public IComparable? Min { get; set; }
+	public IComparable? MinValue { get; set; }
 
 	/// <summary>Maximum value.</summary>
-	public IComparable? Max { get; set; }
+	public IComparable? MaxValue { get; set; }
 
 	/// <summary>The object that corresponds to True value.</summary>
 	public TObject? TrueObject { get; set; }
@@ -25,42 +25,42 @@ public abstract class IsInRangeConverter<TObject> : BaseConverterOneWay<ICompara
 	/// <summary>The object that corresponds to False value.</summary>
 	public TObject? FalseObject { get; set; }
 
-	/// <summary>Converts an object that implemented IComparable to a <see cref="bool"/> based on the object being within a <see cref="Min"/> and <see cref="Max"/> range.</summary>
+	/// <summary>Converts an object that implemented IComparable to a <see cref="bool"/> based on the object being within a <see cref="MinValue"/> and <see cref="MaxValue"/> range.</summary>
 	/// <param name="value">The value to convert.</param>
 	/// <param name="culture">(Not Used)</param>
-	/// <returns>The object assigned to <see cref="TrueObject"/> if value is between <see cref="Min"/> and <see cref="Max"/> then <see cref="TrueObject"/> returns true, otherwise the value assigned to <see cref="FalseObject"/>.</returns>
+	/// <returns>The object assigned to <see cref="TrueObject"/> if value is between <see cref="MinValue"/> and <see cref="MaxValue"/> then <see cref="TrueObject"/> returns true, otherwise the value assigned to <see cref="FalseObject"/>.</returns>
 	public override object ConvertFrom(IComparable value, CultureInfo? culture)
 	{
 		ArgumentNullException.ThrowIfNull(value);
-		ArgumentNullException.ThrowIfNull((Min is null && Max is null) ? null : true, nameof(Min));
+		ArgumentNullException.ThrowIfNull((MinValue is null && MaxValue is null) ? null : true, nameof(MinValue));
 		if (!(TrueObject is null ^ FalseObject is not null))
 		{
 			throw new InvalidOperationException($"{nameof(TrueObject)} and {nameof(FalseObject)} should either be both defined or both omitted.");
 		}
 
-		if (Min is not null && Min.GetType() != value.GetType())
+		if (MinValue is not null && MinValue.GetType() != value.GetType())
 		{
-			throw new ArgumentOutOfRangeException(nameof(value), $" is expected to be of type {nameof(Min)}");
+			throw new ArgumentOutOfRangeException(nameof(value), $" is expected to be of type {nameof(MinValue)}");
 		}
 
-		if (Max is not null && Max.GetType() != value.GetType())
+		if (MaxValue is not null && MaxValue.GetType() != value.GetType())
 		{
-			throw new ArgumentOutOfRangeException(nameof(value), $" is expected to be of type {nameof(Max)}");
+			throw new ArgumentOutOfRangeException(nameof(value), $" is expected to be of type {nameof(MaxValue)}");
 		}
 
 		bool shouldReturnObjectResult = TrueObject is not null && FalseObject is not null;
 
-		if (Max is null)
+		if (MaxValue is null)
 		{
-			return EvaluateCondition(value.CompareTo(Min) >= 0, shouldReturnObjectResult);
+			return EvaluateCondition(value.CompareTo(MinValue) >= 0, shouldReturnObjectResult);
 		}
 
-		if (Min is null)
+		if (MinValue is null)
 		{
-			return EvaluateCondition(value.CompareTo(Max) <= 0, shouldReturnObjectResult);
+			return EvaluateCondition(value.CompareTo(MaxValue) <= 0, shouldReturnObjectResult);
 		}
 
-		return EvaluateCondition(value.CompareTo(Min) >= 0 && value.CompareTo(Max) <= 0, shouldReturnObjectResult);
+		return EvaluateCondition(value.CompareTo(MinValue) >= 0 && value.CompareTo(MaxValue) <= 0, shouldReturnObjectResult);
 	}
 
 	object EvaluateCondition(bool comparisonResult, bool shouldReturnObject) => (comparisonResult, shouldReturnObject) switch
