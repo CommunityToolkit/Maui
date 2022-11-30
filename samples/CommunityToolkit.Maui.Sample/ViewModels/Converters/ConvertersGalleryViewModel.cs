df4c8c5368ed1d8fa9e61e11b83e9e8d711c9961
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2022-11-30 12:33:25 -0800

    Update Description

--- samples/CommunityToolkit.Maui.Sample/ViewModels/Converters/ConvertersGalleryViewModel.cs
+++ samples/CommunityToolkit.Maui.Sample/ViewModels/Converters/ConvertersGalleryViewModel.cs
@@ -99,13 +99,13 @@ public class ConvertersGalleryViewModel : BaseGalleryViewModel
 				"A converter that allows the user to convert a LayoutState enum to a boolean value."),
 
 			SectionModel.Create<IsNotNullConverterViewModel>(nameof(IsNotNullConverter),
-				"A converter that allows users to convert an incoming object? to a bool."),
+				"A converter that allows users to convert an incoming `object?` to a bool."),
 
 			SectionModel.Create<IsNullConverterViewModel>(nameof(IsNullConverter),
-				"A converter that allows users to convert an incoming object? to a bool."),
+				"A converter that allows users to convert an incoming `object?` to a bool."),
 
 			SectionModel.Create<IsInRangeConverterViewModel>(nameof(IsInRangeConverter),
-				"A converter that allows users to validate an object is within a range? to a bool.")
+				"A converter that allows users to validate an object is within a range.")
 		})
 	{
 	}
