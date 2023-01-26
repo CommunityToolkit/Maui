8b6b188171f1d0255663f84da1c348ec6bfb7d8c
Shaun Lawrence <shaunrlawrence@gmail.com>
2023-01-26 21:03:56 +0000

    Sample to prove support for showing Popup in OnAppearing

---
+++ samples/CommunityToolkit.Maui.Sample/Pages/Views/Popup/ShowPopupInOnAppearingPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using CommunityToolkit.Maui.Sample.Models;
+using CommunityToolkit.Maui.Sample.ViewModels.Views;
+using CommunityToolkit.Maui.Views;
+
+namespace CommunityToolkit.Maui.Sample.Pages.Views;
+
+public partial class ShowPopupInOnAppearingPage : BasePage<ShowPopupInOnAppearingPageViewModel>
+{
+	readonly PopupSizeConstants popupSizeConstants;
+
+	public ShowPopupInOnAppearingPage(
+		PopupSizeConstants popupSizeConstants,
+		ShowPopupInOnAppearingPageViewModel showPopupInOnAppearingPageViewModel)
+		: base(showPopupInOnAppearingPageViewModel)
+	{
+		InitializeComponent();
+		this.popupSizeConstants = popupSizeConstants;
+	}
+
+	protected override void OnAppearing()
+	{
+		base.OnAppearing();
+
+		// Proves that we now support showing a popup before the platform is even ready.
+		this.ShowPopup(new SimplePopup(popupSizeConstants));
+	}
+}
