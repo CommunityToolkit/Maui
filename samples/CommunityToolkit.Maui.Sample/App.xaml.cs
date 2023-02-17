f3ef1a1f6fb5351b47538e5f695d6fe715a81a23
Gerald Versluis <gerald.versluis@microsoft.com>
2023-02-17 14:50:28 +0100

    Update App.xaml.cs

--- samples/CommunityToolkit.Maui.Sample/App.xaml.cs
+++ samples/CommunityToolkit.Maui.Sample/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Application = Microsoft.Maui.Controls.Application;
+﻿using CommunityToolkit.Maui.Views;
+using Application = Microsoft.Maui.Controls.Application;
 
 namespace CommunityToolkit.Maui.Sample;
 
@@ -8,6 +9,29 @@ public partial class App : Application
 	{
 		InitializeComponent();
 
-		MainPage = new AppShell();
+		var page = new ContentPage();
+		var stack = new VerticalStackLayout
+		{
+			new MediaElement
+			{
+				Source = MediaSource.FromUri("https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"),
+				ShouldAutoPlay = true,
+				WidthRequest = 400,
+				HeightRequest = 300
+			}
+		};
+
+		page.Content = stack;
+
+		var page1 = new ContentPage();
+		var button = new Button();
+		button.Text = "Click";
+		button.Clicked += async (s, a) =>
+		{
+			await Application.Current!.MainPage!.Navigation.PushAsync(page);
+		};
+
+		page1.Content = button;
+		MainPage = new NavigationPage(page1); //new AppShell();
 	}
 }
