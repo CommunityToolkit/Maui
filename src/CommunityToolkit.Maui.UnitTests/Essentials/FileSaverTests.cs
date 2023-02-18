b687648d7e10b59f0172af993bfaae05dca7dd51
Vladislav Antonyuk <vlad.antonyuk@gmail.com>
2023-02-18 19:30:10 +0200

    Add UTs

--- src/CommunityToolkit.Maui.UnitTests/Essentials/FileSaverTests.cs
+++ src/CommunityToolkit.Maui.UnitTests/Essentials/FileSaverTests.cs
@@ -22,4 +22,28 @@ public class FileSaverTests
 		await Assert.ThrowsAsync<NotImplementedException>(() => FileSaver.SaveAsync("file name", Stream.Null, CancellationToken.None));
 		await Assert.ThrowsAsync<NotImplementedException>(() => FileSaver.SaveAsync("initial path", "file name", Stream.Null, CancellationToken.None));
 	}
+
+	[Fact]
+	public async Task SaveSafeAsyncFailsOnNet()
+	{
+		FileSaver.SetDefault(new FileSaverImplementation());
+		var result = await FileSaver.SaveSafeAsync("fileName", Stream.Null, CancellationToken.None);
+		result.Should().NotBeNull();
+		result.Exception.Should().BeOfType<NotImplementedException>();
+		result.FilePath.Should().BeNull();
+		result.IsSuccessful.Should().BeFalse();
+		Assert.Throws<NotImplementedException>(result.EnsureSuccess);
+	}
+
+	[Fact]
+	public async Task SaveSafeAsyncWithInitialPathFailsOnNet()
+	{
+		FileSaver.SetDefault(new FileSaverImplementation());
+		var result = await FileSaver.SaveSafeAsync("initial path","fileName", Stream.Null, CancellationToken.None);
+		result.Should().NotBeNull();
+		result.Exception.Should().BeOfType<NotImplementedException>();
+		result.FilePath.Should().BeNull();
+		result.IsSuccessful.Should().BeFalse();
+		Assert.Throws<NotImplementedException>(result.EnsureSuccess);
+	}
 }
--- src/CommunityToolkit.Maui.UnitTests/Essentials/FolderPickerTests.cs
+++ src/CommunityToolkit.Maui.UnitTests/Essentials/FolderPickerTests.cs
@@ -23,4 +23,28 @@ public class FolderPickerTests
 		await Assert.ThrowsAsync<NotImplementedException>(() => FolderPicker.PickAsync(CancellationToken.None));
 		await Assert.ThrowsAsync<NotImplementedException>(() => FolderPicker.PickAsync("initial path", CancellationToken.None));
 	}
+
+	[Fact]
+	public async Task PickSafeAsyncFailsOnNet()
+	{
+		FolderPicker.SetDefault(new FolderPickerImplementation());
+		var result = await FolderPicker.PickSafeAsync(CancellationToken.None);
+		result.Should().NotBeNull();
+		result.Exception.Should().BeOfType<NotImplementedException>();
+		result.Folder.Should().BeNull();
+		result.IsSuccessful.Should().BeFalse();
+		Assert.Throws<NotImplementedException>(result.EnsureSuccess);
+	}
+
+	[Fact]
+	public async Task PickSafeAsyncWithInitialPathFailsOnNet()
+	{
+		FolderPicker.SetDefault(new FolderPickerImplementation());
+		var result = await FolderPicker.PickSafeAsync("initial path", CancellationToken.None);
+		result.Should().NotBeNull();
+		result.Exception.Should().BeOfType<NotImplementedException>();
+		result.Folder.Should().BeNull();
+		result.IsSuccessful.Should().BeFalse();
+		Assert.Throws<NotImplementedException>(result.EnsureSuccess);
+	}
 }
