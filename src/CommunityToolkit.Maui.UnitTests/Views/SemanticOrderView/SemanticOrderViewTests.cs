b8655112f78dd03004f5bc75a18427f3ad81e205
Shane Neuville <shneuvil@microsoft.com>
2023-02-21 12:40:51 -0600

    - add basic unit test

---
+++ src/CommunityToolkit.Maui.UnitTests/Views/SemanticOrderView/SemanticOrderViewTests.cs
@@ -0,0 +1,37 @@
+﻿using CommunityToolkit.Maui.Core;
+using CommunityToolkit.Maui.UnitTests.Mocks;
+using Xunit;
+
+namespace CommunityToolkit.Maui.UnitTests.Views;
+
+public class SemanticOrderViewTests : BaseHandlerTest
+{
+	const string resultWhenUserTapsOutsideOfSemanticOrderView = "User Tapped Outside of SemanticOrderView";
+	readonly ISemanticOrderView semanticOrderView = new MockSemanticOrderView();
+
+	public SemanticOrderViewTests()
+	{
+		Assert.IsAssignableFrom<ISemanticOrderView>(new MockSemanticOrderView());
+	}
+
+	[Fact]
+	public void OnViewOrderIsCalled()
+	{
+		var semanticOrderViewHandler = CreateViewHandler<MockSemanticOrderViewHandler>(semanticOrderView);
+		Assert.NotNull(semanticOrderView.Handler);
+		Assert.Equal(1, semanticOrderViewHandler.OnViewOrderCount);
+		var button = new Button();
+		((MockSemanticOrderView)semanticOrderView).ViewOrder = new[] { button };
+		Assert.Single(semanticOrderView.ViewOrder);
+		Assert.Equal(button, semanticOrderView.ViewOrder.First());
+		Assert.Equal(2, semanticOrderViewHandler.OnViewOrderCount);
+	}
+
+	class MockSemanticOrderView : Maui.Views.SemanticOrderView
+	{
+		public MockSemanticOrderView()
+		{
+
+		}
+	}
+}
