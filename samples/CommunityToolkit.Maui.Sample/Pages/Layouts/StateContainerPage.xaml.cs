0284f5d756c524ca9d0993c3ca09575375ac10e6
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2023-02-24 15:29:54 -0800

    Update Sample App

--- samples/CommunityToolkit.Maui.Sample/Pages/Layouts/StateContainerPage.xaml.cs
+++ samples/CommunityToolkit.Maui.Sample/Pages/Layouts/StateContainerPage.xaml.cs
@@ -1,4 +1,3 @@
-using CommunityToolkit.Maui.Animations;
 using CommunityToolkit.Maui.Layouts;
 using CommunityToolkit.Maui.Sample.ViewModels.Layouts;
 
@@ -9,15 +8,20 @@ public partial class StateContainerPage : BasePage<StateContainerViewModel>
 	public StateContainerPage(StateContainerViewModel viewModel) : base(viewModel)
 	{
 		InitializeComponent();
+		Padding = new Thickness(12, 0);
 	}
 
-	async void ChangeStateWithFadeAnimation(object? sender, EventArgs e) 
+	async void ChangeStateWithFadeAnimation(object? sender, EventArgs e)
 	{
 		await StateContainer.ChangeStateWithAnimation(GridWithAnimation, "ReplaceGrid", CancellationToken.None);
 	}
 
 	async void ChangeStateWithCustomAnimation(object? sender, EventArgs e)
 	{
-		await StateContainer.ChangeStateWithAnimation(GridWithAnimation, "ReplaceGrid", async (l, c)=> await new FadeAnimation(){Length = 1000, Opacity = 0}.Animate(l).WaitAsync(c), async (l, c) => await new FadeAnimation() { Length = 1000, Opacity = 1 }.Animate(l).WaitAsync(c), CancellationToken.None);
+		await StateContainer.ChangeStateWithAnimation(GridWithAnimation,
+														"ReplaceGrid",
+														(element, token) => element.ScaleTo(0, 100, Easing.SpringIn).WaitAsync(token),
+														(element, token) => element.ScaleTo(1, 250, Easing.SpringOut).WaitAsync(token),
+														CancellationToken.None);
 	}
 }
