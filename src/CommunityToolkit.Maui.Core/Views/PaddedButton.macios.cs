efae5851bb65a1210e8276ec3c2b9772f6faae4f
Vladislav Antonyuk <vlad.antonyuk@gmail.com>
2023-02-28 22:01:53 +0200

    Fix padding or title is not applied

--- src/CommunityToolkit.Maui.Core/Views/PaddedButton.macios.cs
+++ src/CommunityToolkit.Maui.Core/Views/PaddedButton.macios.cs
@@ -16,7 +16,6 @@ public sealed class PaddedButton : UIButton
 		TopPadding = topPadding;
 		RightPadding = rightPadding;
 		BottomPadding = bottomPadding;
-
 		SetPadding(leftPadding, topPadding, rightPadding, bottomPadding);
 	}
 
@@ -42,9 +41,8 @@ public sealed class PaddedButton : UIButton
 
 	void SetPadding(NFloat leftPadding, NFloat topPadding, NFloat rightPadding, NFloat bottomPadding)
 	{
-		if (OperatingSystem.IsIOSVersionAtLeast(15))
-		{
-			Configuration ??= UIButtonConfiguration.PlainButtonConfiguration;
+		if (OperatingSystem.IsIOSVersionAtLeast(15) && Configuration is not null)
+		{ 
 			Configuration.ContentInsets = new NSDirectionalEdgeInsets(topPadding, leftPadding, bottomPadding, rightPadding);
 		}
 		else
