2364741eb65245e3d9aa10612b877aeaff262c7b
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2023-03-09 17:45:53 -0800

    Update Formatting. Change `[UnsupportedOSPlatform("MacOS")]` -> `[UnsupportedOSPlatform("MacCatalyst")]`

--- src/CommunityToolkit.Maui.Core/Platform/KeyboardManager/KeyboardExtensions.macos.cs
+++ src/CommunityToolkit.Maui.Core/Platform/KeyboardManager/KeyboardExtensions.macos.cs
@@ -1,23 +1,13 @@
-using System;
-using UIKit;
+using System.Runtime.Versioning;
 
 namespace CommunityToolkit.Maui.Core.Platform;
 
-[UnsupportedOSPlatform("MacOS")]
+[UnsupportedOSPlatform("MacCatalyst")]
 public static partial class KeyboardExtensions
 {
-	static bool HideKeyboard(this UIView inputView)
-	{
-		throw new NotSupportedException("MacOS does not currently support opening the SoftKeyboard");
-	}
+    static bool HideKeyboard(this UIView inputView) => throw new NotSupportedException("MacOS does not currently support opening the SoftKeyboard");
 
-	static bool ShowKeyboard(this UIView inputView)
-	{
-		throw new NotSupportedException("MacOS does not currently support opening the SoftKeyboard");
-	}
+    static bool ShowKeyboard(this UIView inputView) => throw new NotSupportedException("MacOS does not currently support opening the SoftKeyboard");
 
-	static bool IsSoftKeyboardShowing(this UIView inputView)
-	{
-		throw new NotSupportedException("MacOS does not currently support opening the SoftKeyboard");
-	}
+    static bool IsSoftKeyboardShowing(this UIView inputView) => throw new NotSupportedException("MacOS does not currently support opening the SoftKeyboard");
 }
