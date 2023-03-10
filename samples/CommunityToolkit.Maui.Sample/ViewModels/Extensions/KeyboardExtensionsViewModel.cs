57421dc9880a769e4c5a7cb42873ff578e60cfe2
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2023-03-09 17:47:00 -0800

    Handle Exceptions. Update Formatting. Remove Unused Code

--- samples/CommunityToolkit.Maui.Sample/ViewModels/Extensions/KeyboardExtensionsViewModel.cs
+++ samples/CommunityToolkit.Maui.Sample/ViewModels/Extensions/KeyboardExtensionsViewModel.cs
@@ -1,62 +1,68 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using CommunityToolkit.Maui.Core.Platform;
+﻿using CommunityToolkit.Maui.Core.Platform;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
 namespace CommunityToolkit.Maui.Sample.ViewModels.Extensions;
+
 public partial class KeyboardExtensionsViewModel : BaseViewModel
 {
-
-	public KeyboardExtensionsViewModel()
-	{
-	}
+	[ObservableProperty]
+	string operationResult = string.Empty;
 
 	[RelayCommand]
 	void OnIsKeyboardShowing(ITextInput view)
 	{
 		if (view.IsSoftKeyboardShowing())
 		{
-			KeyboardShowingText = $"Soft Input Is Currently Showing";
+			OperationResult = $"Soft Input Is Currently Showing";
 		}
 		else
 		{
-			KeyboardShowingText = $"Soft Input Is Currently Hidden";
+			OperationResult = $"Soft Input Is Currently Hidden";
 		}
 	}
 
 	[RelayCommand]
-	async Task OnHideKeyboard(ITextInput view)
+	async Task OnHideKeyboard(ITextInput view, CancellationToken token)
 	{
-		if (await view.HideKeyboardAsync(CancellationToken.None))
+		try
 		{
-			OperationResult = "Hide Succeeded";
+			bool isSuccessful = await view.HideKeyboardAsync(token);
+
+			if (isSuccessful)
+			{
+				OperationResult = "Hide Succeeded";
+			}
+			else
+			{
+				OperationResult = "Hide Failed";
+			}
 		}
-		else
+		catch (Exception e)
 		{
-			OperationResult = "Hide Failed";
+			OperationResult = e.Message;
 		}
 	}
 
 	[RelayCommand]
-	async Task OnShowKeyboard(ITextInput view)
+	async Task OnShowKeyboard(ITextInput view, CancellationToken token)
 	{
-		if (await view.ShowKeyboardAsync(CancellationToken.None))
+		try
 		{
-			OperationResult = "Show Succeeded";
+			bool isSuccessful = await view.ShowKeyboardAsync(CancellationToken.None);
+
+			if (isSuccessful)
+			{
+				OperationResult = "Show Succeeded";
+			}
+			else
+			{
+				OperationResult = "Show Failed";
+			}
 		}
-		else
+		catch (Exception e)
 		{
-			OperationResult = "Show Failed";
+			OperationResult = e.Message;
 		}
 	}
-
-	[ObservableProperty]
-	string? keyboardShowingText;
-
-	[ObservableProperty]
-	string? operationResult;
 }
--- src/CommunityToolkit.Maui.Core/Platform/KeyboardManager/KeyboardExtensions.net.cs
+++ src/CommunityToolkit.Maui.Core/Platform/KeyboardManager/KeyboardExtensions.net.cs
@@ -1,23 +1,10 @@
-using System;
-using System.Diagnostics;
-using System.Reflection.Metadata;
-
 namespace CommunityToolkit.Maui.Core.Platform;
 
 public static partial class KeyboardExtensions
 {
-	static bool HideKeyboard(this object _)
-	{
-		throw new NotSupportedException();
-	}
+	static bool HideKeyboard(this object _) => throw new NotSupportedException();
 
-	static bool ShowKeyboard(this object _)
-	{
-		throw new NotSupportedException();
-	}
+	static bool ShowKeyboard(this object _) => throw new NotSupportedException();
 
-	static bool IsSoftKeyboardShowing(this object _)
-	{
-		throw new NotSupportedException();
-	}
+	static bool IsSoftKeyboardShowing(this object _) => throw new NotSupportedException();
 }
