63483a7e7d80b51a710222cde4da0d988dd92e07
Jay Cho <chojoong@gmail.com>
2023-03-13 17:32:21 +0900

    Add Tizen implementation

--- src/CommunityToolkit.Maui.Core/Platform/KeyboardExtensions/KeyboardExtensions.tizen.cs
+++ src/CommunityToolkit.Maui.Core/Platform/KeyboardExtensions/KeyboardExtensions.tizen.cs
@@ -1,12 +1,22 @@
-using System;
+using Tizen.NUI.BaseComponents;
 
 namespace CommunityToolkit.Maui.Core.Platform;
 
 public static partial class KeyboardExtensions
 {
-    static bool HideKeyboard(this object _) => throw new NotImplementedException();
+	static bool HideKeyboard(this View view) => SetKeyInputFocus(view, false);
 
-    static bool ShowKeyboard(this object _) => throw new NotImplementedException();
+	static bool ShowKeyboard(this View view) => SetKeyInputFocus(view, true);
 
-    static bool IsSoftKeyboardShowing(this object _) => throw new NotImplementedException();
+	static bool IsSoftKeyboardShowing(this View view)
+	{
+		return view.KeyInputFocus;
+	}
+
+	static bool SetKeyInputFocus(View view, bool isShow)
+	{
+		view.KeyInputFocus = isShow;
+
+		return view.KeyInputFocus;
+	}
 }
