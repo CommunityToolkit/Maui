f73e92f1553e929d847152a0019da91a80136a58
Vladislav Antonyuk <vlad.antonyuk@gmail.com>
2023-06-04 15:04:26 +0300

    1134 Add EventArgs sample

--- samples/CommunityToolkit.Maui.Sample/ViewModels/Behaviors/EventToCommandBehaviorViewModel.cs
+++ samples/CommunityToolkit.Maui.Sample/ViewModels/Behaviors/EventToCommandBehaviorViewModel.cs
@@ -11,7 +11,9 @@ public partial class EventToCommandBehaviorViewModel : BaseViewModel
 	public EventToCommandBehaviorViewModel()
 	{
 		IncrementCommand = new Command(() => ClickCount++);
+		IncrementCommand2 = new Command<EventArgs>((args) => ClickCount++);
 	}
 
 	public ICommand IncrementCommand { get; }
+	public ICommand IncrementCommand2 { get; }
 }
