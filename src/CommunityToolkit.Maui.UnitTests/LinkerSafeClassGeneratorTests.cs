63b6616feb2ac6a4aa22d15c68982fba8e6336ea
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2023-06-05 16:16:06 -0700

    Add LinkerSafeGenerator

---
+++ src/CommunityToolkit.Maui.UnitTests/LinkerSafeClassGeneratorTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace CommunityToolkit.Maui.UnitTests;
+
+public class LinkerSafeClassGeneratorTests : BaseTest
+{
+	[Fact]
+	public void EnsureLinkerSafeClassExists()
+	{
+		LinkerSafeClass.LinkerSafeMethod();
+	}
+}
+
