a649470a42ac259b453a2570af2e455e16b1cfe7
Gerald Versluis <gerald.versluis@microsoft.com>
2023-06-23 20:37:26 +0200

    Split AppThemeObject<T>, Resource and Color

---
+++ src/CommunityToolkit.Maui/AppThemeObjectOfT.shared.cs
@@ -0,0 +1,45 @@
+﻿namespace CommunityToolkit.Maui;
+
+/// <summary>
+/// Represents an object that is aware of the operating system theme.
+/// </summary>
+public class AppThemeObject<T>
+{
+	/// <summary>
+	/// The <see cref="object"/> that is used when the operating system uses light theme.
+	/// </summary>
+	public T? Light { get; set; }
+
+	/// <summary>
+	/// The <see cref="object"/> that is used when the operating system uses dark theme.
+	/// </summary>
+	public T? Dark { get; set; }
+
+	/// <summary>
+	/// The <see cref="object"/> that is used when the current theme is unspecified or
+	/// when a value is not provided for <see cref="Light"/> or <see cref="Dark"/>.
+	/// </summary>
+	public T? Default { get; set; }
+
+	internal BindingBase GetBinding()
+	{
+		var binding = new AppThemeBinding();
+
+		if (Light is not null)
+		{
+			binding.Light = Light;
+		}
+
+		if (Dark is not null)
+		{
+			binding.Dark = Dark;
+		}
+
+		if (Default is not null)
+		{
+			binding.Default = Default;
+		}
+
+		return binding;
+	}
+}
--- src/CommunityToolkit.Maui/AppThemeResource.shared.cs
+++ src/CommunityToolkit.Maui/AppThemeResource.shared.cs
@@ -3,43 +3,6 @@
 /// <summary>
 /// Represents a resource that is aware of the operating system theme.
 /// </summary>
-public class AppThemeResource
+public class AppThemeResource : AppThemeObject<object>
 {
-	/// <summary>
-	/// The <see cref="object"/> that is used when the operating system uses light theme.
-	/// </summary>
-	public object? Light { get; set; }
-
-	/// <summary>
-	/// The <see cref="object"/> that is used when the operating system uses dark theme.
-	/// </summary>
-	public object? Dark { get; set; }
-
-	/// <summary>
-	/// The <see cref="object"/> that is used when the current theme is unspecified or
-	/// when a value is not provided for <see cref="Light"/> or <see cref="Dark"/>.
-	/// </summary>
-	public object? Default { get; set; }
-
-	internal BindingBase GetBinding()
-	{
-		var binding = new AppThemeBinding();
-
-		if (Light is not null)
-		{
-			binding.Light = Light;
-		}
-
-		if (Dark is not null)
-		{
-			binding.Dark = Dark;
-		}
-
-		if (Default is not null)
-		{
-			binding.Default = Default;
-		}
-
-		return binding;
-	}
 }
