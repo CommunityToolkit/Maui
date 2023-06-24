ed99dee16fe3af1836e791222e5282ca6629914a
Shaun Lawrence <shaunrlawrence@gmail.com>
2023-06-24 16:55:37 +0100

    Added FlipVertical

---
+++ src/CommunityToolkit.Maui/Animations/FlipVerticalAnimation.shared.cs
@@ -0,0 +1,28 @@
+﻿namespace CommunityToolkit.Maui.Animations;
+
+/// <summary>
+/// Animation that will flip the supplied view vertically.
+/// </summary>
+public class FlipVerticalAnimation : RotateAnimation
+{
+	/// <summary>
+	/// Initializes a new instance of <see cref="FlipVerticalAnimation"/>.
+	/// </summary>
+	public FlipVerticalAnimation() : base(300)
+	{
+
+	}
+
+	/// <inheritdoc />
+	protected override double DefaultRotation { get; set; } = 90;
+
+	/// <inheritdoc />
+	public override async Task Animate(VisualElement view)
+	{
+		if (view != null)
+		{
+			await view.RotateXTo(Rotation, Length, Easing);
+			await view.RotateXTo(0, Length, Easing);
+		}
+	}
+}
