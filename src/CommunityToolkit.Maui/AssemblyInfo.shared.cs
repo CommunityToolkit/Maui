2fb9de9c27ac8491c2f251c4e017c864da9038ed
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2023-06-25 15:55:15 -0700

    Use `static` class for improved performance

--- src/CommunityToolkit.Maui/AssemblyInfo.shared.cs
+++ src/CommunityToolkit.Maui/AssemblyInfo.shared.cs
@@ -10,10 +10,9 @@
 
 [assembly: Microsoft.Maui.Controls.XmlnsPrefix(Constants.XamlNamespace, "toolkit")]
 
-class Constants
+static class Constants
 {
 	public const string XamlNamespace = "http://schemas.microsoft.com/dotnet/2022/maui/toolkit";
-
 	public const string CommunityToolkitNamespace = $"{nameof(CommunityToolkit)}.{nameof(CommunityToolkit.Maui)}";
 	public const string CommunityToolkitNamespacePrefix = $"{CommunityToolkitNamespace}.";
 }
