3443d8f8773e42d27a8c762863ca188b92d76684
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2023-07-06 10:52:12 -0700

    Use `Lazy<T>` to ensure thread safety

--- src/CommunityToolkit.Maui.Core/Essentials/Badge/Badge.shared.cs
+++ src/CommunityToolkit.Maui.Core/Essentials/Badge/Badge.shared.cs
@@ -3,7 +3,10 @@
 /// <inheritdoc cref="IBadge"/>
 public static class Badge
 {
-	static IBadge? defaultImplementation;
+	static Lazy<IBadge> defaultImplementationHolder = new(() => new BadgeImplementation());
+
+	/// <inheritdoc cref="IBadge" />
+	public static IBadge Default => defaultImplementationHolder.Value;
 
 	/// <inheritdoc cref="IBadge.SetCount" />
 	public static void SetCount(int count)
@@ -11,10 +14,6 @@ public static class Badge
 		Default.SetCount(count);
 	}
 
-	/// <inheritdoc cref="IBadge" />
-	public static IBadge Default =>
-		defaultImplementation ??= new BadgeImplementation();
-
 	internal static void SetDefault(IBadge implementation) =>
-		defaultImplementation = implementation;
+		defaultImplementationHolder = new(implementation);
 }
