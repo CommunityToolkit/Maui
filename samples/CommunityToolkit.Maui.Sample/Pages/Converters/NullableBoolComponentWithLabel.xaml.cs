cba1d20a4540fc8ee50c5082c36900c4c7485ac2
Bryce Lynch <techbryce@hotmail.com>
2023-07-26 20:15:18 +1000

    ADD - Added sample to IsStringNotNullOrEmptyConverterPage.xaml

---
+++ samples/CommunityToolkit.Maui.Sample/Pages/Converters/NullableBoolComponentWithLabel.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace CommunityToolkit.Maui.Sample.Pages.Converters;
+
+public partial class NullableBoolComponentWithLabel : ContentView
+{
+	public static readonly BindableProperty NullableIsVisibleProperty = BindableProperty.Create(
+		nameof(NullableIsVisible),
+		typeof(bool?),
+		typeof(NullableBoolComponentWithLabel));
+
+
+	public NullableBoolComponentWithLabel()
+	{
+		InitializeComponent();
+	}
+
+	public bool? NullableIsVisible
+	{
+		get => (bool?) GetValue(NullableIsVisibleProperty);
+		set => SetValue(NullableIsVisibleProperty, value);
+	}
+}
