78c8c81a545c79d5f1c9c403b0e2738dd5aaddfa
Pedro Jesus <pedrojesus.cefet@gmail.com>
2023-08-06 21:26:02 -0300

    Created BindablePropertySG

---
+++ src/CommunityToolkit.Maui.BindablePropertySG/Helpers/AttributeExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace CommunityToolkit.Maui.BindablePropertySG.Helpers;
+static class AttributeExtensions
+{
+	public static TypedConstant GetAttributeValueByName(this AttributeData attribute, string name)
+	{
+		var x = attribute.NamedArguments.SingleOrDefault(kvp => kvp.Key == name).Value;
+		return x;
+	}
+
+	public static string GetNamedArgumentsAttributeValueByNameAsString(this AttributeData attribute, string name, string placeholder = "null")
+	{
+		var data = attribute.NamedArguments.SingleOrDefault(kvp => kvp.Key == name).Value;
+
+		return data.Value is null ? placeholder : data.Value.ToString();
+	}
+
+	public static string GetConstructorArgumentsAttributeValueByNameAsString(this AttributeData attribute)
+	{
+		var data = attribute.ConstructorArguments[0];
+		
+		return data.Value is null ? throw new NullReferenceException() : data.Value.ToString();
+	}
+
+}
---
+++ src/CommunityToolkit.Maui.BindablePropertySG/Models/Records.cs
@@ -0,0 +1,22 @@
+﻿using CommunityToolkit.Maui.BindablePropertySG.Helpers;
+using Microsoft.CodeAnalysis;
+
+namespace CommunityToolkit.Maui.BindablePropertySG.Models;
+
+record BPInfo
+{
+	public string PropertyName { get; set; } = string.Empty;
+	public ITypeSymbol? ReturnType { get; set; }
+	public string DeclaringType { get; set; } = string.Empty;
+	public string DefaultValue { get; set; } = string.Empty;
+	public string DefaultBindingMode { get; set; } = string.Empty;
+	public string ValidateValueMethodName { get; set; } = string.Empty;
+	public string PropertyChangedMethodName { get; set; } = string.Empty;
+	public string PropertyChangingMethodName { get; set; } = string.Empty;
+	public string CoerceValueMethodName { get; set; } = string.Empty;
+	public string DefaultValueCreatorMethodName { get; set; } = string.Empty;
+}
+
+record SemanticValues(ClassInformation ClassInformation, EquatableArray<BPInfo> BPInfos);
+
+readonly record struct ClassInformation(string ClassName, string DeclaredAccessibility, string ContainingNamespace);
