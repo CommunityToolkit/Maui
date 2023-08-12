974794d726995f78848d5d507c6e066fe1f0eee7
Manuel Roat <roat.manuel@gmail.com>
2023-08-12 15:30:16 +0200

    fix: FolderPicker returns always internal paths on Android🐛
    FolderPicker always returned an internal path like '/storage/emulated/0/DCIM' even if an external path was selected, resulting in the app crashing when selecting a folder that only exists on sd card (like /storage/sdCard/External) or selecting the internal path instead of the external (like /storage/sdCard/DCIM resulted to be /storage/emulated/0/DCIM which is the internal one). Now the right path is returned.

--- src/CommunityToolkit.Maui.Core/Essentials/FolderPicker/FolderPickerImplementation.android.cs
+++ src/CommunityToolkit.Maui.Core/Essentials/FolderPicker/FolderPickerImplementation.android.cs
@@ -64,10 +64,25 @@ public sealed partial class FolderPickerImplementation : IFolderPicker
 		const string uriSchemeFolder = "content";
 		if (uri.Scheme is not null && uri.Scheme.Equals(uriSchemeFolder, StringComparison.OrdinalIgnoreCase))
 		{
-			var split = uri.Path?.Split(':') ?? throw new FolderPickerException("Unable to resolve path.");
-			return $"{Android.OS.Environment.ExternalStorageDirectory}/{split[^1]}";
+			// Example path would be /tree/primary:DCIM, or /tree/SDCare:DCIM
+			var path = uri.PathSegments?[1] ?? throw new FolderPickerException("Unable to resolve path.");
+
+			var split = path.Split(':');
+
+			// Primary is the device's internal storage, and anything else is an SD card or other external storage
+			if (split[0].Equals("primary", StringComparison.OrdinalIgnoreCase))
+			{
+				// Example for internal path /storage/emulated/0/DCIM
+				return $"{Android.OS.Environment.ExternalStorageDirectory?.Path}/{split[1]}";
+			}
+			else
+			{
+				// Example for external path /storage/1B0B-0B1C/DCIM
+				return $"/storage/{split[0]}/{split[1]}";
+			}
 		}
 
 		throw new FolderPickerException($"Unable to resolve absolute path or retrieve contents of URI '{uri}'.");
 	}
+
 }
