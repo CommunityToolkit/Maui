f37ab0b253c2c267fc59e647c4f820e421580a1e
cat0363 <kenji.nagano.0363@gmail.com>
2023-11-01 12:13:00 +0900

    Change the transparency of Popup overlay

--- src/CommunityToolkit.Maui.Core/Handlers/Popup/PopUpHandler.windows.cs
+++ src/CommunityToolkit.Maui.Core/Handlers/Popup/PopUpHandler.windows.cs
@@ -44,7 +44,7 @@ public partial class PopupHandler : ElementHandler<IPopup, Popup>
 		var uiSetting = new UISettings();
 		Windows.UI.Color backgroundColor = uiSetting.GetColorValue(UIColorType.Background);
 		var window = view.GetWindow();
-		window.AddOverlay(new PopupOverlay(window, IsColorDark(backgroundColor) ? Color.FromRgba(255, 255, 255, 102) : Color.FromRgba(0, 0, 0, 102)));
+		window.AddOverlay(new PopupOverlay(window, IsColorDark(backgroundColor) ? Color.FromRgba(0, 0, 0, 153) : Color.FromRgba(255, 255, 255, 153)));
 
 		view.OnOpened();
 	}
