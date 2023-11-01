2ec4be1b501dd11964dbb06d511e28188cc6b06f
Shaun Lawrence <shaunrlawrence@gmail.com>
2023-11-01 22:44:22 +0000

    PopupService (#1165)
    * Added initial PopupService implementation
    
    * Tweaks to allowing parameters to be passed to the view model behind a popup
    
    * Tidy up xml docs
    
    * Some unit tests
    
    * Only create a view model instance if the BindingContext hasn't been set
    
    * Remove the reliance on IQueryAttributable in favour of our own interface
    
    * A better way to find the current Page
    
    * Readonly dictionary and some safety checking around expected BindingContext types.
    
    * A different attempt at passing parameters without an explicit interface
    
    * Update src/CommunityToolkit.Maui/PopupService.cs
    
    Co-authored-by: Pedro Jesus <pedrojesus.cefet@gmail.com>
    
    * Now is a time for test
    
    * Remove unnecessary changes
    
    * Sample to perform a long running process
    
    * Provide ability to close popup from within popup view model
    
    * Prevent unnecessary instance being created
    
    * Refactor `CurrentPage`, Add Default Constructor, Refactor `ValidateBindingContext`
    
    * Update Unit Tests
    
    ---------
    
    Co-authored-by: Pedro Jesus <pedrojesus.cefet@gmail.com>
    Co-authored-by: Brandon Minnick <13558917+brminnick@users.noreply.github.com>

---
+++ samples/CommunityToolkit.Maui.Sample/Views/Popups/UpdatingPopup.xaml.cs
@@ -0,0 +1,20 @@
+﻿using CommunityToolkit.Maui.Sample.ViewModels.Views;
+using CommunityToolkit.Maui.Views;
+
+namespace CommunityToolkit.Maui.Sample.Views.Popups;
+
+public partial class UpdatingPopup : Popup
+{
+	public UpdatingPopup(UpdatingPopupViewModel updatingPopupViewModel)
+	{
+		InitializeComponent();
+		BindingContext = updatingPopupViewModel;
+
+		updatingPopupViewModel.Finished += OnUpdatingPopupViewModelFinished;
+	}
+
+	void OnUpdatingPopupViewModelFinished(object? sender, EventArgs e)
+	{
+		this.Close();
+	}
+}
