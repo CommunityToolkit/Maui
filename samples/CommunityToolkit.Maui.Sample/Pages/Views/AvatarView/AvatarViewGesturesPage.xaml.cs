d0f60b09e0a0cbed69cfa3aa384be502d2fc09bb
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2023-11-07 12:03:36 -0800

    Update AvatarViewGesturesPage

--- samples/CommunityToolkit.Maui.Sample/Pages/Views/AvatarView/AvatarViewGesturesPage.xaml.cs
+++ samples/CommunityToolkit.Maui.Sample/Pages/Views/AvatarView/AvatarViewGesturesPage.xaml.cs
@@ -7,21 +7,39 @@ public partial class AvatarViewGesturesPage : BasePage<AvatarViewGesturesViewMod
 {
 	public AvatarViewGesturesPage(AvatarViewGesturesViewModel avatarViewGesturesViewModel) : base(avatarViewGesturesViewModel) => InitializeComponent();
 
-	async void DragGestureRecognizer_DragStarting(object sender, DragStartingEventArgs e) => await ShowToastGestureMessage("AvatarView drag gesture recognizer, drag starting.");
+	async void DragGestureRecognizer_DragStarting(object sender, DragStartingEventArgs e)
+	{
+		var cts = new CancellationTokenSource(TimeSpan.FromSeconds(5));
+		await ShowToastGestureMessage("AvatarView drag gesture recognizer, drag starting.", cts.Token);
+	}
 
-	async void PanGestureRecognizer_PanUpdated(object sender, PanUpdatedEventArgs e) => await ShowToastGestureMessage("AvatarView pan gesture recognizer, pan updated.");
+	async void PanGestureRecognizer_PanUpdated(object sender, PanUpdatedEventArgs e)
+	{
+		var cts = new CancellationTokenSource(TimeSpan.FromSeconds(5));
+		await ShowToastGestureMessage("AvatarView pan gesture recognizer, pan updated.", cts.Token);
+	}
 
-	async void PinchGestureRecognizer_PinchUpdated(object sender, PinchGestureUpdatedEventArgs e) => await ShowToastGestureMessage("AvatarView pinch gesture recognizer, pinch updated.");
+	async void PinchGestureRecognizer_PinchUpdated(object sender, PinchGestureUpdatedEventArgs e)
+	{
+		var cts = new CancellationTokenSource(TimeSpan.FromSeconds(5));
+		await ShowToastGestureMessage("AvatarView pinch gesture recognizer, pinch updated.", cts.Token);
+	}
 
-	async void SwipeGestureRecognizer_Swiped(object sender, SwipedEventArgs e) => await ShowToastGestureMessage("AvatarView swipe gesture recognizer, swiped.");
+	async void SwipeGestureRecognizer_Swiped(object sender, SwipedEventArgs e)
+	{
+		var cts = new CancellationTokenSource(TimeSpan.FromSeconds(5));
+		await ShowToastGestureMessage("AvatarView swipe gesture recognizer, swiped.", cts.Token);
+	}
 
-	async void TapGestureRecognizer_Tapped(object sender, EventArgs e) => await ShowToastGestureMessage("AvatarView Tap Gesture Recognizer, tapped.");
+	async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+	{
+		var cts = new CancellationTokenSource(TimeSpan.FromSeconds(5));
+		await ShowToastGestureMessage("AvatarView Tap Gesture Recognizer, tapped.", cts.Token);
+	}
 
-	static async Task ShowToastGestureMessage(string message)
+	static Task ShowToastGestureMessage(string message, CancellationToken token)
 	{
 		Core.IToast toast = Toast.Make(message, Core.ToastDuration.Short);
-		
-		var cts = new CancellationTokenSource(TimeSpan.FromSeconds(5));
-		await toast.Show(cts.Token);
+		return toast.Show(token);
 	}
 }
