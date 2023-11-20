96aeba6ddb40a398981a7d18111810040a5e8cd1
Eduado Gomez <egomezr@outlook.com>
2023-11-20 19:29:07 +0100

    Created a coveter for hex, to Maui Color

---
+++ src/CommunityToolkit.Maui/Converters/HexToColorConverter.cs
@@ -0,0 +1,26 @@
+﻿namespace CommunityToolkit.Maui.Converters;
+public class HexToColorConverter : IValueConverter {
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+        if (value is string hexColor && hexColor.Length >= 6) // Checking for a minimum valid length
+        {
+            try {
+                if (hexColor[0] == '#') {
+                    hexColor = hexColor[1..]; // Remove '#' if present
+                }
+
+                Color color = Color.FromArgb(hexColor);
+                return color;
+            } catch (Exception e) {
+                // Log the error to the output window
+                Debug.WriteLine($"Error in converting hex color {value} \n Reason: {e.Message} ");
+            }
+        }
+
+        return null;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+        throw new NotImplementedException();
+    }
+}
