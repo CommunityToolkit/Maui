a643eef48d55b1b38c1585bea68dd03d3d21e94f
Shaun Lawrence <17139988+bijington@users.noreply.github.com>
2023-11-28 20:16:10 +0000

    Add sample to trigger an animation using the AnimateCommand

--- samples/CommunityToolkit.Maui.Sample/ViewModels/Behaviors/AnimationBehaviorViewModel.cs
+++ samples/CommunityToolkit.Maui.Sample/ViewModels/Behaviors/AnimationBehaviorViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows.Input;
 using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Maui.Markup;
 using CommunityToolkit.Mvvm.Input;
 
 namespace CommunityToolkit.Maui.Sample.ViewModels.Behaviors;
@@ -8,4 +9,12 @@ public partial class AnimationBehaviorViewModel : BaseViewModel
 {
 	[RelayCommand]
 	Task OnAnimation(CancellationToken token) => Snackbar.Make($"{nameof(AnimationCommand)} is triggered.").Show(token);
+
+	public ICommand? AnimateFromViewModelCommand { get; set; }
+
+	[RelayCommand]
+	void OnTriggerAnimation()
+	{
+		AnimateFromViewModelCommand?.Execute(CancellationToken.None);
+	}
 }
