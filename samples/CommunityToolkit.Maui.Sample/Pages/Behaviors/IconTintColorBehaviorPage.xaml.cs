45ca10c72e23c1c6af483414548c4faf97984341
Victor Hugo Garcia Hernandez <vhugogarcia@users.noreply.github.com>
2023-11-30 13:20:05 -0600

    Removes the transparent color set on Android when null (#1584)
    * Removed the transparent color that was being set on Android when the icon tint color is set to null.
    
    * Add `null` Color to IconTintColorBehavior in Sample App
    
    * Update Sample
    
    ---------
    
    Co-authored-by: Brandon Minnick <13558917+brminnick@users.noreply.github.com>

--- samples/CommunityToolkit.Maui.Sample/Pages/Behaviors/IconTintColorBehaviorPage.xaml.cs
+++ samples/CommunityToolkit.Maui.Sample/Pages/Behaviors/IconTintColorBehaviorPage.xaml.cs
@@ -9,22 +9,4 @@ public partial class IconTintColorBehaviorPage : BasePage<IconTintColorBehaviorV
 	{
 		InitializeComponent();
 	}
-
-	void ChangeSource_Clicked(System.Object sender, System.EventArgs e)
-	{
-		var imageSource = (ImageButton)sender;
-		var selectedImage = imageSource.Source as FileImageSource;
-
-		if (selectedImage is not null)
-		{
-			if (selectedImage.File == "dotnet_bot.png")
-			{
-				UpdateImage.Source = "shield.png";
-			}
-			else
-			{
-				UpdateImage.Source = "dotnet_bot.png";
-			}
-		}
-	}
 }
