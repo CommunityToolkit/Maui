eea645c9885bb61292a661ace00bd24e98c1cb49
Gerald Versluis <gerald.versluis@microsoft.com>
2024-01-24 10:44:15 +0100

    Update MauiMediaElement.macios.cs

--- src/CommunityToolkit.Maui.MediaElement/Views/MauiMediaElement.macios.cs
+++ src/CommunityToolkit.Maui.MediaElement/Views/MauiMediaElement.macios.cs
@@ -20,25 +20,26 @@ public class MauiMediaElement : UIView
 		ArgumentNullException.ThrowIfNull(playerViewController.View);
 		playerViewController.View.Frame = Bounds;
 
-#if IOS16_0_OR_GREATER || MACCATALYST16_1_OR_GREATER
 		// On iOS 16+ and macOS 13+ the AVPlayerViewController has to be added to a parent ViewController, otherwise the transport controls won't be displayed.
-		var viewController = parentViewController ?? WindowStateManager.Default.GetCurrentUIViewController();
-
-		// If we don't find the viewController, assume it's not Shell and still continue, the transport controls will still be displayed
-		if (viewController?.View is not null)
+		// If the MediaElement is contained in, for example, a CarouselView, parentViewController will be null, but transport controls are still diesplayed.
+		if (parentViewController is not null && OperatingSystem.IsIOSVersionAtLeast(16) || OperatingSystem.IsMacCatalystVersionAtLeast(16, 1))
 		{
-			// Zero out the safe area insets of the AVPlayerViewController
-			UIEdgeInsets insets = viewController.View.SafeAreaInsets;
-			playerViewController.AdditionalSafeAreaInsets =
-				new UIEdgeInsets(insets.Top * -1, insets.Left, insets.Bottom * -1, insets.Right);
+			var viewController = parentViewController ?? WindowStateManager.Default.GetCurrentUIViewController();
+
+			// If we don't find the viewController, assume it's not Shell and still continue, the transport controls will still be displayed
+			if (viewController?.View is not null)
+			{
+				// Zero out the safe area insets of the AVPlayerViewController
+				UIEdgeInsets insets = viewController.View.SafeAreaInsets;
+				playerViewController.AdditionalSafeAreaInsets =
+					new UIEdgeInsets(insets.Top * -1, insets.Left, insets.Bottom * -1, insets.Right);
 
 
-			// Add the View from the AVPlayerViewController to the parent ViewController
-			viewController.AddChildViewController(playerViewController);
-			viewController.View.AddSubview(playerViewController.View);
+				// Add the View from the AVPlayerViewController to the parent ViewController
+				viewController.AddChildViewController(playerViewController);
+				viewController.View.AddSubview(playerViewController.View);
+			}
 		}
-
-#endif
 
 		AddSubview(playerViewController.View);
 	}
