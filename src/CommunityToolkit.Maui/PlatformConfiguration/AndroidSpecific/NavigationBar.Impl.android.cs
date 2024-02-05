4c4e478222e7ce18f43dcb36260158ede5c296dd
Pedro Jesus <pedrojesus.cefet@gmail.com>
2024-02-04 23:08:07 -0300

    implemented NavigationBar feature for Android

---
+++ src/CommunityToolkit.Maui/PlatformConfiguration/AndroidSpecific/NavigationBar.Impl.android.cs
@@ -0,0 +1,86 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Views;
+using AndroidX.Core.View;
+using CommunityToolkit.Maui.Core.Extensions;
+using Microsoft.Maui.Controls.Platform;
+using Microsoft.Maui.Handlers;
+using Microsoft.Maui.Platform;
+
+namespace CommunityToolkit.Maui.PlatformConfiguration.AndroidSpecific;
+
+static partial class NavigationBar
+{
+	static bool? isSupported;
+
+	static bool IsSupported => isSupported ??= AndroidWindowExtensions.IsSupported(BuildVersionCodes.M);
+
+	internal static partial void RemapForControls()
+	{
+		PageHandler.Mapper.Add(ColorProperty.PropertyName, MapNavigationColorProperty);
+		PageHandler.Mapper.Add(StyleProperty.PropertyName, MapNavigationStyleProperty);
+	}
+
+	static void MapNavigationStyleProperty(IPageHandler handler, IContentView view)
+	{
+		if (view is not Page page)
+		{
+			return;
+		}
+
+		if (!page.IsSet(StyleProperty) || !IsSupported)
+		{
+			return;
+		}
+
+		var activity = (Activity?)handler.PlatformView.Context ?? throw new NullReferenceException("Activity is null");
+		var style = GetStyle(page);
+
+		switch (style)
+		{
+			case NavigationBarStyle.DarkContent:
+				SetSystemNavigationBarAppearance(activity, true);
+				break;
+
+			default:
+				SetSystemNavigationBarAppearance(activity, false);
+				break;
+		}
+
+		static void SetSystemNavigationBarAppearance(Activity activity, bool isLightSystemNavigationBars)
+		{
+			var window = GetCurrentWindow(activity);
+			var windowController = WindowCompat.GetInsetsController(window, window.DecorView);
+			windowController.AppearanceLightNavigationBars = isLightSystemNavigationBars;
+
+			static Android.Views.Window GetCurrentWindow(Activity activity)
+			{
+				var window = activity.Window ?? throw new InvalidOperationException($"{nameof(activity.Window)} cannot be null");
+				window.ClearFlags(WindowManagerFlags.TranslucentStatus);
+				window.AddFlags(WindowManagerFlags.DrawsSystemBarBackgrounds);
+				return window;
+			}
+		}
+	}
+
+	static void MapNavigationColorProperty(IPageHandler handler, IContentView view)
+	{
+		if (view is not Page page)
+		{
+			return;
+		}
+
+		if (!page.IsSet(ColorProperty) || !IsSupported)
+		{
+			return;
+		}
+
+		var activity = (Activity?)handler.PlatformView.Context ?? throw new NullReferenceException("Activity is null");
+		var window = activity.GetCurrentWindow();
+
+		var color = GetColor((Page)view).ToPlatform();
+
+		window.SetNavigationBarColor(color);
+	}
+
+}
---
+++ src/CommunityToolkit.Maui/PlatformConfiguration/AndroidSpecific/NavigationBar.Properties.cs
@@ -0,0 +1,74 @@
+﻿#if ANDROID
+using Android.App;
+using Android.Views;
+#endif
+using MAndroid = Microsoft.Maui.Controls.PlatformConfiguration.Android;
+using MauiElement = Microsoft.Maui.Controls.Page;
+
+namespace CommunityToolkit.Maui.PlatformConfiguration.AndroidSpecific;
+/// <summary>
+/// 
+/// </summary>
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+public static partial class NavigationBar
+{
+	public static readonly BindableProperty ColorProperty = BindableProperty.CreateAttached("Color", typeof(Color), typeof(NavigationBar), Colors.Transparent);
+
+	public static Color GetColor(BindableObject element)
+	{
+		return (Color)element.GetValue(ColorProperty);
+	}
+
+	public static void SetColor(BindableObject element, Color value)
+	{
+		element.SetValue(ColorProperty, value);
+	}
+
+	public static IPlatformElementConfiguration<MAndroid, MauiElement> SetColor(this IPlatformElementConfiguration<MAndroid, MauiElement> config, Color value)
+	{
+		SetColor(config.Element, value);
+		return config;
+	}
+
+	public static Color GetColor(this IPlatformElementConfiguration<MAndroid, MauiElement> config)
+	{
+		return GetColor(config.Element);
+	}
+
+	public static readonly BindableProperty StyleProperty = BindableProperty.CreateAttached("Style", typeof(NavigationBarStyle), typeof(NavigationBar), NavigationBarStyle.Default);
+
+	public static void SetStyle(BindableObject element, NavigationBarStyle value)
+	{
+		element.SetValue(StyleProperty, value);
+	}
+
+	public static NavigationBarStyle GetStyle(BindableObject element)
+	{
+		return (NavigationBarStyle)element.GetValue(StyleProperty);
+	}
+
+	public static IPlatformElementConfiguration<MAndroid, MauiElement> SetStyle(this IPlatformElementConfiguration<MAndroid, MauiElement> config, NavigationBarStyle value)
+	{
+		SetStyle(config.Element, value);
+		return config;
+	}
+
+	public static NavigationBarStyle GetStyle(this IPlatformElementConfiguration<MAndroid, MauiElement> config)
+	{
+		return GetStyle(config.Element);
+	}
+
+
+	internal static partial void RemapForControls();
+
+
+#if !ANDROID
+	internal static partial void RemapForControls()
+	{
+
+	}
+#endif
+}
+
+
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
