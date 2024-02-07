d08df9c7810ecc8df92e5a2bd07db773c1e16500
Victor H Garcia <ihugo@live.com>
2024-02-07 11:40:33 -0600

    Added support for full screen on Android

--- src/CommunityToolkit.Maui.MediaElement/Views/MauiMediaElement.android.cs
+++ src/CommunityToolkit.Maui.MediaElement/Views/MauiMediaElement.android.cs
@@ -4,6 +4,8 @@ using Android.Widget;
 using AndroidX.CoordinatorLayout.Widget;
 using Com.Google.Android.Exoplayer2.UI;
 using CommunityToolkit.Maui.Views;
+using AndroidX.Core.View;
+using Android.Content.Res;
 
 namespace CommunityToolkit.Maui.Core.Views;
 
@@ -13,6 +15,9 @@ namespace CommunityToolkit.Maui.Core.Views;
 public class MauiMediaElement : CoordinatorLayout
 {
 	readonly StyledPlayerView playerView;
+	readonly Context context;
+	int defaultSystemUiVisibility;
+	bool isSystemBarVisible;
 
 	/// <summary>
 	/// Initializes a new instance of the <see cref="MauiMediaElement"/> class.
@@ -22,18 +27,123 @@ public class MauiMediaElement : CoordinatorLayout
 	public MauiMediaElement(Context context, StyledPlayerView playerView) : base(context)
 	{
 		this.playerView = playerView;
+		this.context = context;
 
-		// Create a RelativeLayout for sizing the video
-		RelativeLayout relativeLayout = new(context)
+		playerView.FullscreenButtonClick += PlayerView_FullscreenButtonClick;
+
+		AddView(playerView);
+	}
+
+	/// <summary>
+	/// Allows the video to enter or exist a full screen mode
+	/// </summary>
+	/// <param name="sender"></param>
+	/// <param name="e"></param>
+	void PlayerView_FullscreenButtonClick(object? sender, StyledPlayerView.FullscreenButtonClickEventArgs e)
+	{
+		// Ensure there is a player view
+		if (this.playerView is null)
 		{
-			LayoutParameters = new CoordinatorLayout.LayoutParams(LayoutParams.MatchParent, LayoutParams.MatchParent)
-			{
-				Gravity = (int)GravityFlags.Center
-			}
-		};
+			return;
+		}
 
-		relativeLayout.AddView(playerView);
-		AddView(relativeLayout);
+		// Ensure current activity exists
+		var currentActivity = Platform.CurrentActivity;
+		if (currentActivity is null)
+		{
+			return;
+		}
+
+		// Ensure current window is available
+		var currentWindow = currentActivity.Window;
+		if (currentWindow is null)
+		{
+			return;
+		}
+
+		// Ensure current resources exist
+		var currentResources = currentActivity.Resources;
+		if (currentResources is null)
+		{
+			return;
+		}
+
+		// Ensure current resources configuration exist
+		if (currentResources.Configuration is null)
+		{
+			return;
+		}
+
+		var windowInsetsControllerCompat = WindowCompat.GetInsetsController(currentWindow, currentWindow.DecorView);
+
+		if (e.IsFullScreen)
+		{
+			// Force the landscape on the device
+			currentActivity.RequestedOrientation = Android.Content.PM.ScreenOrientation.Landscape;
+
+			// Hide the SystemBars and Status bar
+			if (OperatingSystem.IsAndroidVersionAtLeast(30))
+			{
+				currentWindow.SetDecorFitsSystemWindows(false);
+
+				var windowInsets = currentWindow.DecorView.RootWindowInsets;
+				if (windowInsets is not null)
+				{
+					isSystemBarVisible = windowInsets.IsVisible(WindowInsetsCompat.Type.NavigationBars()) || windowInsets.IsVisible(WindowInsetsCompat.Type.StatusBars());
+					
+					if (isSystemBarVisible)
+					{
+						windowInsetsControllerCompat.SystemBarsBehavior = WindowInsetsControllerCompat.BehaviorShowTransientBarsBySwipe;
+						currentWindow.InsetsController?.Hide(WindowInsets.Type.SystemBars());
+					}
+				}
+			}
+			else
+			{
+				defaultSystemUiVisibility = (int)currentWindow.DecorView.SystemUiVisibility;
+				int systemUiVisibility = defaultSystemUiVisibility | (int)SystemUiFlags.LayoutStable | (int)SystemUiFlags.LayoutHideNavigation | (int)SystemUiFlags.LayoutHideNavigation |
+					(int)SystemUiFlags.LayoutFullscreen | (int)SystemUiFlags.HideNavigation | (int)SystemUiFlags.Fullscreen | (int)SystemUiFlags.Immersive;
+				currentWindow.DecorView.SystemUiVisibility = (StatusBarVisibility)systemUiVisibility;
+			}
+
+			// Update the PlayerView
+			if (currentWindow.DecorView is FrameLayout layout)
+			{
+				RemoveView(playerView);
+				layout.AddView(playerView, new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MatchParent, ViewGroup.LayoutParams.MatchParent));
+			}
+		}
+		else
+		{
+			currentActivity.RequestedOrientation = Android.Content.PM.ScreenOrientation.Portrait;
+			
+			// Show again the SystemBars and Status bar
+			if (OperatingSystem.IsAndroidVersionAtLeast(30))
+			{
+				currentWindow.SetDecorFitsSystemWindows(true);
+
+				if (isSystemBarVisible)
+				{
+					var types = WindowInsetsCompat.Type.StatusBars() |
+								WindowInsetsCompat.Type.SystemBars() |
+								WindowInsetsCompat.Type.NavigationBars();
+					windowInsetsControllerCompat.Show(types);
+					windowInsetsControllerCompat.SystemBarsBehavior = WindowInsetsControllerCompat.BehaviorDefault;
+					currentWindow.InsetsController?.Show(WindowInsets.Type.SystemBars());
+				}
+			}
+			else
+			{
+				currentWindow.DecorView.SystemUiVisibility = (StatusBarVisibility)defaultSystemUiVisibility;
+			}
+
+			// Update the PlayerView
+			if (currentWindow.DecorView is FrameLayout layout)
+			{
+				layout.RemoveView(playerView);
+				AddView(playerView);
+			}
+		}
 	}
 
 	/// <summary>
