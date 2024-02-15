bfc1af2ad08c21b213e5df8f252eebbafd9b2a3b
Victor H Garcia <ihugo@live.com>
2024-02-15 16:34:33 -0600

    Replaced the full screen icon for SVG format. Removed unnecessary code.

--- src/CommunityToolkit.Maui.MediaElement/Extensions/PageExtensions.cs
+++ src/CommunityToolkit.Maui.MediaElement/Extensions/PageExtensions.cs
@@ -4,8 +4,6 @@
 // https://github.com/dotnet/maui/blob/main/src/Controls/src/Core/Platform/PageExtensions.cs
 static class PageExtensions
 {
-	internal static bool isFullScreen { get; set; } = false;
-
 	internal static Page GetCurrentPage(this Page currentPage)
 	{
 		if (currentPage.NavigationProxy.ModalStack.LastOrDefault() is Page modal)
