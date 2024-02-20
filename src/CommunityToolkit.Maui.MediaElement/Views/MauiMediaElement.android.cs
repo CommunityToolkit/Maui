3e3893f0a10bbc35d15b6b367f9eacb4e025acce
Victor H Garcia <ihugo@live.com>
2024-02-20 16:57:26 -0600

    Fixes the crash when the application is back from background

--- src/CommunityToolkit.Maui.MediaElement/Views/MauiMediaElement.android.cs
+++ src/CommunityToolkit.Maui.MediaElement/Views/MauiMediaElement.android.cs
@@ -21,6 +21,15 @@ public class MauiMediaElement : CoordinatorLayout
 	bool isSystemBarVisible;
 	bool isFullScreen;
 
+#pragma warning disable CS8618 // Non-nullable field is uninitialized. Consider declaring as nullable.
+#pragma warning disable IDE0060 // Remove unused parameter
+	public MauiMediaElement(nint ptr, JniHandleOwnership jni) : base(Platform.AppContext)
+	{
+		//Fixes no constructor found exception
+	}
+#pragma warning restore CS8618 // Non-nullable field is uninitialized. Consider declaring as nullable.
+#pragma warning restore IDE0060 // Remove unused parameter
+
 	/// <summary>
 	/// Initializes a new instance of the <see cref="MauiMediaElement"/> class.
 	/// </summary>
