7a7da29dd24f859b90010a618772eef9c2db9088
Victor H Garcia <ihugo@live.com>
2024-02-21 15:42:20 -0600

    Added launch mode on Android to SingleInstance

--- samples/CommunityToolkit.Maui.Sample/Platforms/Android/MainActivity.cs
+++ samples/CommunityToolkit.Maui.Sample/Platforms/Android/MainActivity.cs
@@ -4,7 +4,7 @@ using Android.OS;
 
 namespace CommunityToolkit.Maui.Sample;
 
-[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleInstance, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
 public class MainActivity : MauiAppCompatActivity
 {
 	protected override void OnPostCreate(Bundle? savedInstanceState)
