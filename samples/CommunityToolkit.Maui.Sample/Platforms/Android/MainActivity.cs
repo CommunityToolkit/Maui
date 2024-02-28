3bd90d28bea623130e6f64af838cbce5622f84b3
Victor H Garcia <ihugo@live.com>
2024-02-28 09:25:35 -0600

    Removed the try and catch from MainActivity
    Confirmed by PureWeen, we don't need to override the OnPostCreate event and just set the LaunchMode to SingleTask. I applied the changes and it worked as expected. https://github.com/dotnet/maui/issues/18692#issuecomment-1969033154

--- samples/CommunityToolkit.Maui.Sample/Platforms/Android/MainActivity.cs
+++ samples/CommunityToolkit.Maui.Sample/Platforms/Android/MainActivity.cs
@@ -4,19 +4,8 @@ using Android.OS;
 
 namespace CommunityToolkit.Maui.Sample;
 
-[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleInstance, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTask, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
 public class MainActivity : MauiAppCompatActivity
 {
-	protected override void OnPostCreate(Bundle? savedInstanceState)
-	{
-		try
-		{
-			base.OnPostCreate(savedInstanceState);
-		}
-		catch(InvalidOperationException ex)
-		{
-			// https://github.com/dotnet/maui/issues/18692
-			System.Diagnostics.Trace.WriteLine(ex.Message);
-		}
-	}
+	
 }
