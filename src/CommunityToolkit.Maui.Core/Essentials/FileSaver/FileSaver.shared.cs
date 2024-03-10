ee03335008ba6e51d5783c55410edbae514d2f26
Vladislav Antonyuk <vlad.antonyuk@gmail.com>
2024-03-10 13:13:09 +0200

    Add tests

--- src/CommunityToolkit.Maui.Core/Essentials/FileSaver/FileSaver.shared.cs
+++ src/CommunityToolkit.Maui.Core/Essentials/FileSaver/FileSaver.shared.cs
@@ -30,7 +30,7 @@ public static class FileSaver
 	public static Task<FileSaverResult> SaveAsync(string fileName, Stream stream, CancellationToken cancellationToken = default) =>
 		Default.SaveAsync(fileName, stream, cancellationToken);
 	
-	/// <inheritdoc cref="IFileSaver.SaveAsync(string, string, Stream, IProgress<double>, CancellationToken)"/>
+	/// <inheritdoc cref="IFileSaver.SaveAsync(string, string, Stream, IProgress{double}, CancellationToken)"/>
 	[SupportedOSPlatform("Android26.0")]
 	[SupportedOSPlatform("iOS14.0")]
 	[SupportedOSPlatform("MacCatalyst14.0")]
@@ -39,7 +39,7 @@ public static class FileSaver
 	public static Task<FileSaverResult> SaveAsync(string initialPath, string fileName, Stream stream, IProgress<double> progress, CancellationToken cancellationToken = default) =>
 		Default.SaveAsync(initialPath, fileName, stream, cancellationToken);
 
-	/// <inheritdoc cref="IFileSaver.SaveAsync(string, Stream, IProgress<double>, CancellationToken)"/>
+	/// <inheritdoc cref="IFileSaver.SaveAsync(string, Stream, IProgress{double}, CancellationToken)"/>
 	[SupportedOSPlatform("Android")]
 	[SupportedOSPlatform("iOS14.0")]
 	[SupportedOSPlatform("MacCatalyst14.0")]
