589b700f68474587a85a1673ddd26f0533505535
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2024-03-25 17:01:41 -0700

    Remove try/catch block

--- samples/CommunityToolkit.Maui.Sample/Pages/Base/BaseGalleryPage.cs
+++ samples/CommunityToolkit.Maui.Sample/Pages/Base/BaseGalleryPage.cs
@@ -14,20 +14,18 @@ public abstract class BaseGalleryPage<TViewModel> : BasePage<TViewModel> where T
 		Padding = 0;
 
 		Content = new CollectionView
-		{
-			SelectionMode = SelectionMode.Single,
-		}.ItemTemplate(new GalleryDataTemplate())
-		 .Bind(ItemsView.ItemsSourceProperty,
-					static (BaseGalleryViewModel vm) => vm.Items,
-					mode: BindingMode.OneTime)
-		 .Invoke(collectionView => collectionView.SelectionChanged += HandleSelectionChanged);
+			{
+				SelectionMode = SelectionMode.Single,
+			}.ItemTemplate(new GalleryDataTemplate())
+			.Bind(ItemsView.ItemsSourceProperty,
+				static (BaseGalleryViewModel vm) => vm.Items,
+				mode: BindingMode.OneTime)
+			.Invoke(collectionView => collectionView.SelectionChanged += HandleSelectionChanged);
 	}
 
 	async void HandleSelectionChanged(object? sender, SelectionChangedEventArgs e)
 	{
-		try
-		{
-			ArgumentNullException.ThrowIfNull(sender);
+		ArgumentNullException.ThrowIfNull(sender);
 
 		var collectionView = (CollectionView)sender;
 		collectionView.SelectedItem = null;
@@ -36,78 +34,77 @@ public abstract class BaseGalleryPage<TViewModel> : BasePage<TViewModel> where T
 		{
 			await Shell.Current.GoToAsync(AppShell.GetPageRoute(sectionModel.ViewModelType));
 		}
-		}
-		catch (Exception ex)
-		{
-			_ = ex;
-		}
-		
 	}
+}
 
-	class GalleryDataTemplate : DataTemplate
+class GalleryDataTemplate : DataTemplate
+{
+	public GalleryDataTemplate() : base(CreateDataTemplate)
 	{
-		public GalleryDataTemplate() : base(CreateDataTemplate)
-		{
 
-		}
-
-		enum Row { TopPadding, Content, BottomPadding }
-		enum Column { LeftPadding, Content, RightPadding }
-
-		static Grid CreateDataTemplate() => new()
-		{
-			RowDefinitions = Rows.Define(
-				(Row.TopPadding, 12),
-				(Row.Content, Star),
-				(Row.BottomPadding, 12)),
-
-			ColumnDefinitions = Columns.Define(
-				(Column.LeftPadding, 24),
-				(Column.Content, Star),
-				(Column.RightPadding, 24)),
-
-			Children =
-			{
-				new Card().Row(Row.Content).Column(Column.Content).DynamicResource(Border.StyleProperty, "BorderGalleryCard")
-			}
-		};
-
-		class Card : Border
-		{
-			public Card()
-			{
-				Content = new Grid
-				{
-					BackgroundColor = Colors.Transparent,
-
-					RowSpacing = 4,
-
-					RowDefinitions = Rows.Define(
-						(CardRow.Title, 24),
-						(CardRow.Description, Auto)),
-
-					ColumnDefinitions = Columns.Define(Star),
-
-					Children =
-					{
-						new Label()
-							.Row(CardRow.Title)
-							.Bind(Label.TextProperty,
-									static (SectionModel section) => section.Title,
-									mode: BindingMode.OneTime)
-							.DynamicResource(Label.StyleProperty, "LabelSectionTitle"),
-
-						new Label { MaxLines = 4, LineBreakMode = LineBreakMode.WordWrap }
-							.Row(CardRow.Description).TextStart().TextTop()
-							.Bind(Label.TextProperty,
-									static (SectionModel section) => section.Description,
-									mode: BindingMode.OneTime)
-							.DynamicResource(Label.StyleProperty, "LabelSectionText")
-					}
-				};
-			}
-		}
-
-		enum CardRow { Title, Description }
 	}
+
+	enum Row { TopPadding, Content, BottomPadding }
+	enum Column { LeftPadding, Content, RightPadding }
+
+	static Grid CreateDataTemplate() => new()
+	{
+		RowDefinitions = Rows.Define(
+			(Row.TopPadding, 12),
+			(Row.Content, Star),
+			(Row.BottomPadding, 12)),
+
+		ColumnDefinitions = Columns.Define(
+			(Column.LeftPadding, 24),
+			(Column.Content, Star),
+			(Column.RightPadding, 24)),
+
+		Children =
+		{
+			new Card().Row(Row.Content).Column(Column.Content).DynamicResource(Border.StyleProperty, "BorderGalleryCard")
+		}
+	};
+
+	class Card : Border
+	{
+		public Card()
+		{
+			Content = new Grid
+			{
+				BackgroundColor = Colors.Transparent,
+
+				RowSpacing = 4,
+
+				RowDefinitions = Rows.Define(
+					(CardRow.Title, 24),
+					(CardRow.Description, Auto)),
+
+				ColumnDefinitions = Columns.Define(Star),
+
+				Children =
+				{
+					new Label()
+						.Row(CardRow.Title)
+						.Bind(Label.TextProperty,
+							static (SectionModel section) => section.Title,
+							mode: BindingMode.OneTime)
+						.DynamicResource(Label.StyleProperty, "LabelSectionTitle"),
+
+					new Label
+						{
+							MaxLines = 4,
+							LineBreakMode = LineBreakMode.WordWrap
+						}
+						.Row(CardRow.Description).TextStart().TextTop()
+						.Bind(Label.TextProperty,
+							static (SectionModel section) => section.Description,
+							mode: BindingMode.OneTime)
+						.DynamicResource(Label.StyleProperty, "LabelSectionText")
+				}
+			};
+		}
+	}
+
+	enum CardRow { Title, Description }
+}
 }
