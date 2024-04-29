aed871a91a1f68acd7bcddbce244d452626e7765
Shaun Lawrence <17139988+bijington@users.noreply.github.com>
2024-04-29 20:02:43 +0100

    Remove broken cref in xml docs

--- samples/CommunityToolkit.Maui.Sample/Converters/CompareDoubleToColorConverter.cs
+++ samples/CommunityToolkit.Maui.Sample/Converters/CompareDoubleToColorConverter.cs
@@ -3,7 +3,7 @@ using CommunityToolkit.Maui.Converters;
 namespace CommunityToolkit.Maui.Sample.Converters;
 
 /// <summary>
-/// Compares a double value using the <see cref="CompareConverter.ComparingValue"/> 
+/// Compares a double value against the ComparingValue property
 /// and returns a <see cref="Color"/> based on the comparison.
 /// </summary>
 public sealed class CompareDoubleToColorConverter : CompareConverter<double, Color>
