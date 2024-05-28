fc698550e0408db86c6d7aec09a1f7f0c6685b90
Gerald Versluis <gerald.versluis@microsoft.com>
2024-05-28 10:26:33 +0200

    Add dummy Tizen CameraViewExtensions

---
+++ src/CommunityToolkit.Maui.CameraView/Extensions/CameraViewExtensions.tizen.cs
@@ -0,0 +1,11 @@
+﻿using CommunityToolkit.Maui.Core;
+using CommunityToolkit.Maui.Core.Primitives;
+
+namespace CommunityToolkit.Maui.Extensions;
+
+static class CameraViewExtensions
+{
+	public static int ToPlatform(this CameraFlashMode flashMode) => throw new NotSupportedException();
+
+	public static void UpdateAvailability(this IAvailability cameraView, Context context) => throw new NotSupportedException();
+}
