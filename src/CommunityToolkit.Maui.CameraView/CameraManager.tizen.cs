c07ea7e57872495ee65cc54dd1e9dbcd103a9164
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2024-05-28 18:01:30 -0700

    Update Tizen Method Declarations

--- src/CommunityToolkit.Maui.CameraView/CameraManager.tizen.cs
+++ src/CommunityToolkit.Maui.CameraView/CameraManager.tizen.cs
@@ -4,15 +4,17 @@ namespace CommunityToolkit.Maui.Core;
 
 partial class CameraManager
 {
-	private const string notSupportedMessage = "CameraView is not supported on Tizen.";
+	const string notSupportedMessage = "CameraView is not supported on Tizen.";
 	
+	public void Dispose() => throw new NotSupportedException(notSupportedMessage);
+
 	public partial void UpdateFlashMode(CameraFlashMode flashMode) => throw new NotSupportedException(notSupportedMessage);
 
 	public partial void UpdateZoom(float zoomLevel) => throw new NotSupportedException(notSupportedMessage);
 
 	public partial ValueTask UpdateCaptureResolution(Size resolution, CancellationToken token) => throw new NotSupportedException(notSupportedMessage);
 	
-	protected virtual partial ValueTask PlatformStartCameraPreview(CancellationToken token) => throw new NotSupportedException(notSupportedMessage);
+	protected virtual partial Task PlatformStartCameraPreview(CancellationToken token) => throw new NotSupportedException(notSupportedMessage);
 
 	protected virtual partial void PlatformStopCameraPreview() => throw new NotSupportedException(notSupportedMessage);
 
@@ -21,6 +23,4 @@ partial class CameraManager
 	protected virtual partial void PlatformDisconnect() => throw new NotSupportedException(notSupportedMessage);
 
 	protected virtual partial ValueTask PlatformTakePicture(CancellationToken token) => throw new NotSupportedException(notSupportedMessage);
-
-	public void Dispose() => throw new NotSupportedException(notSupportedMessage);
 }
