4a324482a7f0657a615f5b78586afacc0a0a899c
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2024-06-02 16:43:06 -0700

    Add Global Usings for Tizen

--- src/CommunityToolkit.Maui.Camera/GlobalUsings.shared.cs
+++ src/CommunityToolkit.Maui.Camera/GlobalUsings.shared.cs
@@ -7,4 +7,7 @@ global using NativePlatformView = global::Android.Views.View;
 #elif WINDOWS
 global using NativePlatformCameraPreviewView = global::Microsoft.UI.Xaml.FrameworkElement;
 global using NativePlatformView = global::Microsoft.UI.Xaml.FrameworkElement;
+#elif TIZEN
+global using NativePlatformCameraPreviewView = global::object;
+global using NativePlatformView = global::object;
 #endif
