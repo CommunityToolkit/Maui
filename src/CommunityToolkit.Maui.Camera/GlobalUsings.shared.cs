d2fc7795803a198761ad757809962f1ba2bc56b5
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2024-06-02 17:15:31 -0700

    Update GlobalUsings.shared.cs

--- src/CommunityToolkit.Maui.Camera/GlobalUsings.shared.cs
+++ src/CommunityToolkit.Maui.Camera/GlobalUsings.shared.cs
@@ -2,12 +2,12 @@
 global using NativePlatformCameraPreviewView = global::UIKit.UIView;
 global using NativePlatformView = global::UIKit.UIView;
 #elif ANDROID
-global using NativePlatformView = global::Android.Views.View;
 global using NativePlatformCameraPreviewView = global::AndroidX.Camera.View.PreviewView;
+global using NativePlatformView = global::Android.Views.View;
 #elif WINDOWS
 global using NativePlatformCameraPreviewView = global::Microsoft.UI.Xaml.FrameworkElement;
 global using NativePlatformView = global::Microsoft.UI.Xaml.FrameworkElement;
 #elif TIZEN
-global using NativePlatformCameraPreviewView = global::object;
-global using NativePlatformView = global::object;
+global using NativePlatformCameraPreviewView = object;
+global using NativePlatformView = object;
 #endif
