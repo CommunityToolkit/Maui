2be91a1185e39662cbdf6160c53985dd440c9a8e
Shaun Lawrence <17139988+bijington@users.noreply.github.com>
2024-06-04 21:17:45 +0100

    Some code tidy up

--- samples/CommunityToolkit.Maui.Sample/Pages/Behaviors/TouchBehaviorPage.xaml.cs
+++ samples/CommunityToolkit.Maui.Sample/Pages/Behaviors/TouchBehaviorPage.xaml.cs
@@ -4,18 +4,9 @@ namespace CommunityToolkit.Maui.Sample.Pages.Behaviors;
 
 public partial class TouchBehaviorPage : BasePage<TouchBehaviorViewModel>
 {
-	public ICommand JeffCommand { get; }
-	
 	public TouchBehaviorPage(TouchBehaviorViewModel viewModel)
 		: base(viewModel)
 	{
 		InitializeComponent();
-
-		JeffCommand = new Command(OnJeff);
-	}
-
-	void OnJeff()
-	{
-		Application.Current?.MainPage?.DisplayAlert("Tap", @"Jeff", "OK");
 	}
 }
