f6e336849fe15b1814e13255d8a21d2e5adfcdea
James Crutchley <ne0rmatrix@gmail.com>
2024-06-09 02:07:59 -0700

    Fix Media Element Windows CTD (#1921)
    * Updated fullScreenButton icon to fix a bug where App would crash to desktop.
    
    Updated the `Content` property of the `fullScreenButton` object from `fullScreenIcon` to a new `FontIcon` object. The `Glyph` property is now set to "\uE740" and the `FontFamily` property is set to "Segoe Fluent Icons". This change modifies the visual representation of the `fullScreenButton` in the UI.
    
    * Replace `fullScreenIcon` with direct `FontIcon` creation
    
    Removed the `fullScreenIcon` static readonly object from the `MauiMediaElement` class in the `MauiMediaElement.windows.cs` file. Now, a new `FontIcon` object is directly created in the `OnFullScreenButtonClick` method. This change simplifies the code and makes the `OnFullScreenButtonClick` method more self-contained.
    
    This removes reduntant static readonly object that was causing issues. The fontIcon is now generated dynamically to prevent CTD.
    
    * Update full screen button icons in MauiMediaElement
    
    Refactored the `MauiMediaElement` class in `MauiMediaElement.windows.cs` file. Removed the `exitFullScreenIcon` static readonly variable and instead created a new `FontIcon` directly where needed. Updated the `OnFullScreenButtonClick` method to set the `fullScreenButton.Content` to new `FontIcon` instances with different glyphs (`"\uE740"` and `"\uE73F"`), representing different icons in the "Segoe Fluent Icons" font family. These changes affect the icons used for the full screen button in the media player.
    
    This fixes a small UI issue where the incorrect icon was applied starting with full screen then reversion of Full screen status. So icons got reversed starting when selecting full screen. Fixed now. Removed the static readonly object as I was able to verify it does in fact have a bug with it.
    
    * Add Segoe Fluent Icons font for Windows platform
    
    Updated the `UseMauiCommunityToolkitMediaElement` method in the `AppBuilderExtensions` class to include a configuration for the "Segoe Fluent Icons" font from the "SegMDL2.ttf" file specifically for the Windows platform. Also, added a new `ItemGroup` in the `CommunityToolkit.Maui.MediaElement.csproj` file to pack the "SegMDL2.ttf" font file into the application when the `TargetFramework` contains '-windows'.
    
    * Revert adding font directly. Switched back to using existing system font.
    
    * 1. Revert changes
    2. remove `static readonly` from `FontIcons`
    3. Fix what font icon is applied while switching full screen status
    
    * Add `readonly` to `FontIcons`
    
    * remove uneeded changes from original files

--- src/CommunityToolkit.Maui.MediaElement/Views/MauiMediaElement.windows.cs
+++ src/CommunityToolkit.Maui.MediaElement/Views/MauiMediaElement.windows.cs
@@ -31,8 +31,9 @@ public class MauiMediaElement : Grid, IDisposable
 	readonly Grid buttonContainer;
 	readonly Button fullScreenButton;
 	readonly MediaPlayerElement mediaPlayerElement;
-	static readonly FontIcon fullScreenIcon = new() { Glyph = "\uE740", FontFamily = new FontFamily("Segoe Fluent Icons") };
-	static readonly FontIcon exitFullScreenIcon = new() { Glyph = "\uE73F", FontFamily = new FontFamily("Segoe Fluent Icons") };
+	// Cannot be static readonly because we need to be able to add icon to multiple instances of the button
+	readonly FontIcon fullScreenIcon = new() { Glyph = "\uE740", FontFamily = new FontFamily("Segoe Fluent Icons") };
+	readonly FontIcon exitFullScreenIcon = new() { Glyph = "\uE73F", FontFamily = new FontFamily("Segoe Fluent Icons") };
 	bool doesNavigationBarExistBeforeFullScreen;
 	bool isDisposed;
 
@@ -163,7 +164,7 @@ public class MauiMediaElement : Grid, IDisposable
 				popup.Child = null;
 				fullScreenGrid.Children.Clear();
 			}
-			fullScreenButton.Content = exitFullScreenIcon;
+			fullScreenButton.Content = fullScreenIcon;
 			Children.Add(mediaPlayerElement);
 			Children.Add(buttonContainer);
 
@@ -182,7 +183,7 @@ public class MauiMediaElement : Grid, IDisposable
 			mediaPlayerElement.Height = displayInfo.Height / displayInfo.Density;
 
 			Children.Clear();
-			fullScreenButton.Content = fullScreenIcon;
+			fullScreenButton.Content = exitFullScreenIcon;
 			fullScreenGrid.Children.Add(mediaPlayerElement);
 			fullScreenGrid.Children.Add(buttonContainer);
 
