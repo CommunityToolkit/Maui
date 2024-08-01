636cf464247e537175f287fc98eb6c59c34ec3d9
Víctor Hugo García Hernández <vhugogarcia@users.noreply.github.com>
2024-08-01 12:26:57 -0600

    Transparent status bar color android (#1630)
    * Fixes a issue that was preventing the status bar to set as transparent on Android only
    
    * Added sample to control the alpha value
    
    * Added initial opacity
    
    * Updated the Platform Color to store it on memory
    
    * Updated to use var to match the code styling
    
    * Added demo support for the StatusBar alpha control.
    
    * Removed unnecessary usings.
    
    * Updated the layout flags for Android to follow the platform guidelines properly set as transparent
    
    * Updated the If conditional for easy to read, and formatted.
    
    ---------
    
    Co-authored-by: Brandon Minnick <13558917+brminnick@users.noreply.github.com>
    Co-authored-by: Pedro Jesus <pedrojesus.cefet@gmail.com>

--- src/CommunityToolkit.Maui.Core/Platform/StatusBar/StatusBar.android.cs
+++ src/CommunityToolkit.Maui.Core/Platform/StatusBar/StatusBar.android.cs
@@ -5,63 +5,85 @@ using AndroidX.Core.View;
 using CommunityToolkit.Maui.Core.Extensions;
 using Microsoft.Maui.Platform;
 using Activity = Android.App.Activity;
+using PlatformColor = Android.Graphics.Color;
 
 namespace CommunityToolkit.Maui.Core.Platform;
 
 [SupportedOSPlatform("Android23.0")] // StatusBar is only supported on Android 23.0+
 static partial class StatusBar
 {
-	static readonly Lazy<bool> isSupportedHolder = new(() =>
-	{
-		if (OperatingSystem.IsAndroidVersionAtLeast((int)BuildVersionCodes.M))
-		{
-			return true;
-		}
+    static readonly Lazy<bool> isSupportedHolder = new(() =>
+    {
+        if (OperatingSystem.IsAndroidVersionAtLeast((int)BuildVersionCodes.M))
+        {
+            return true;
+        }
 
-		System.Diagnostics.Trace.WriteLine($"{nameof(StatusBar)} Color + Style functionality is not supported on this version of the Android operating system. Minimum supported Android API is {BuildVersionCodes.M}");
+        System.Diagnostics.Trace.WriteLine($"{nameof(StatusBar)} Color + Style functionality is not supported on this version of the Android operating system. Minimum supported Android API is {BuildVersionCodes.M}");
 
-		return false;
-	});
+        return false;
+    });
 
-	static Activity Activity => Microsoft.Maui.ApplicationModel.Platform.CurrentActivity ?? throw new InvalidOperationException("Android Activity can't be null.");
+    static Activity Activity => Microsoft.Maui.ApplicationModel.Platform.CurrentActivity ?? throw new InvalidOperationException("Android Activity can't be null.");
 
-	static bool IsSupported => isSupportedHolder.Value;
+    static bool IsSupported => isSupportedHolder.Value;
 
-	static void PlatformSetColor(Color color)
-	{
-		if (IsSupported)
-		{
-			Activity.Window?.SetStatusBarColor(color.ToPlatform());
-		}
-	}
+    static void PlatformSetColor(Color color)
+    {
+        if (!IsSupported)
+        {
+            return;
+        }
 
-	static void PlatformSetStyle(StatusBarStyle style)
-	{
-		if (!IsSupported)
-		{
-			return;
-		}
+        if (Activity.Window is not null)
+        {
+            var platformColor = color.ToPlatform();
+            Activity.Window.SetStatusBarColor(platformColor);
 
-		switch (style)
-		{
-			case StatusBarStyle.DarkContent:
-				SetStatusBarAppearance(Activity, true);
-				break;
+            bool isColorTransparent = platformColor == PlatformColor.Transparent;
+            if (isColorTransparent)
+            {
+                Activity.Window.ClearFlags(WindowManagerFlags.DrawsSystemBarBackgrounds);
+                Activity.Window.SetFlags(WindowManagerFlags.LayoutNoLimits, WindowManagerFlags.LayoutNoLimits);
+            }
+            else
+            {
+                Activity.Window.ClearFlags(WindowManagerFlags.LayoutNoLimits);
+                Activity.Window.SetFlags(WindowManagerFlags.DrawsSystemBarBackgrounds, WindowManagerFlags.DrawsSystemBarBackgrounds);
+            }
 
-			case StatusBarStyle.Default:
-			case StatusBarStyle.LightContent:
-				SetStatusBarAppearance(Activity, false);
-				break;
+            WindowCompat.SetDecorFitsSystemWindows(Activity.Window, !isColorTransparent);
+        }
 
-			default:
-				throw new NotSupportedException($"{nameof(StatusBarStyle)} {style} is not yet supported on Android");
-		}
-	}
+    }
 
-	static void SetStatusBarAppearance(Activity activity, bool isLightStatusBars)
-	{
-		var window = activity.GetCurrentWindow();
-		var windowController = WindowCompat.GetInsetsController(window, window.DecorView);
-		windowController.AppearanceLightStatusBars = isLightStatusBars;
-	}
+    static void PlatformSetStyle(StatusBarStyle style)
+    {
+        if (!IsSupported)
+        {
+            return;
+        }
+
+        switch (style)
+        {
+            case StatusBarStyle.DarkContent:
+                SetStatusBarAppearance(Activity, true);
+                break;
+
+            case StatusBarStyle.Default:
+            case StatusBarStyle.LightContent:
+                SetStatusBarAppearance(Activity, false);
+                break;
+
+            default:
+                throw new NotSupportedException($"{nameof(StatusBarStyle)} {style} is not yet supported on Android");
+        }
+    }
+
+    static void SetStatusBarAppearance(Activity activity, bool isLightStatusBars)
+    {
+        var window = activity.GetCurrentWindow();
+        var windowController = WindowCompat.GetInsetsController(window, window.DecorView);
+        windowController.AppearanceLightStatusBars = isLightStatusBars;
+    }
 }
