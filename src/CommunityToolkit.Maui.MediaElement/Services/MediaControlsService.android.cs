25d1a835f3d910e1decdc06d05bb0a92e9da5967
James Crutchley <ne0rmatrix@gmail.com>
2024-08-20 09:36:40 -0700

    Update nuget version and apply fixes for changes

--- src/CommunityToolkit.Maui.MediaElement/Services/MediaControlsService.android.cs
+++ src/CommunityToolkit.Maui.MediaElement/Services/MediaControlsService.android.cs
@@ -136,7 +136,7 @@ class MediaControlsService : Service
 		notification ??= new NotificationCompat.Builder(Platform.AppContext, "1");
 
 		notification.SetStyle(style);
-		notification.SetSmallIcon(_Microsoft.Android.Resource.Designer.Resource.Drawable.exo_styled_controls_audiotrack);
+		notification.SetSmallIcon(Resource.Drawable.exo_styled_controls_audiotrack);
 		notification.SetAutoCancel(false);
 		notification.SetVisibility(NotificationCompat.VisibilityPublic);
 		mediaSession.SetExtras(intent.Extras);
@@ -196,22 +196,22 @@ class MediaControlsService : Service
 		var pause = new Intent(this, typeof(MediaControlsService));
 		pause.SetAction("MediaAction.pause");
 		var pPause = PendingIntent.GetService(this, 1, pause, pendingIntentFlags);
-		actionPause ??= new NotificationCompat.Action.Builder(Resource.Drawable.exo_icon_pause, ACTION_PAUSE, pPause).Build();
+		actionPause ??= new NotificationCompat.Action.Builder(Resource.Drawable.media3_notification_pause, ACTION_PAUSE, pPause).Build();
 
 		var play = new Intent(this, typeof(MediaControlsService));
 		play.SetAction("MediaAction.play");
 		var pPlay = PendingIntent.GetService(this, 1, play, pendingIntentFlags);
-		actionPlay ??= new NotificationCompat.Action.Builder(Resource.Drawable.exo_icon_play, ACTION_PLAY, pPlay).Build();
+		actionPlay ??= new NotificationCompat.Action.Builder(Resource.Drawable.media3_icon_circular_play, ACTION_PLAY, pPlay).Build();
 
 		var previous = new Intent(this, typeof(MediaControlsService));
 		previous.SetAction("MediaAction.rewind");
 		var pPrevious = PendingIntent.GetService(this, 1, previous, pendingIntentFlags);
-		actionPrevious ??= new NotificationCompat.Action.Builder(Resource.Drawable.exo_icon_rewind, ACTION_REWIND, pPrevious).Build();
+		actionPrevious ??= new NotificationCompat.Action.Builder(Resource.Drawable.media3_notification_seek_back, ACTION_REWIND, pPrevious).Build();
 
 		var next = new Intent(this, typeof(MediaControlsService));
 		next.SetAction("MediaAction.fastForward");
 		var pNext = PendingIntent.GetService(this, 1, next, pendingIntentFlags);
-		actionNext ??= new NotificationCompat.Action.Builder(Resource.Drawable.exo_icon_fastforward, ACTION_FASTFORWARD, pNext).Build();
+		actionNext ??= new NotificationCompat.Action.Builder(Resource.Drawable.media3_notification_seek_forward, ACTION_FASTFORWARD, pNext).Build();
 
 		notification?.AddAction(actionPrevious);
 		notification?.AddAction(actionPause);
