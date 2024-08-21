9c19a7ad44e24927d3a64a4748fa0141fb2a0c54
Shaun Lawrence <17139988+bijington@users.noreply.github.com>
2024-08-21 22:02:09 +0100

    Reinstate BindableProperty definitions

--- src/CommunityToolkit.Maui/Converters/IsInRangeConverter.shared.cs
+++ src/CommunityToolkit.Maui/Converters/IsInRangeConverter.shared.cs
@@ -8,20 +8,56 @@ public sealed class IsInRangeConverter : IsInRangeConverter<IComparable, object>
 /// <summary>Converts the incoming value to a <see cref="bool"/> indicating whether or not the value is within a range.</summary>
 public abstract class IsInRangeConverter<TValue, TReturnObject> : BaseConverterOneWay<TValue, object> where TValue : IComparable
 {
+	/// <summary>
+	/// Bindable property for <see cref="FalseObject"/>
+	/// </summary>
+	public static readonly BindableProperty FalseObjectProperty = BindableProperty.Create(nameof(FalseObject), typeof(TReturnObject?), typeof(IsInRangeConverter<TValue, TReturnObject>));
+	
+	/// <summary>
+	/// Bindable property for <see cref="MaxValue"/>
+	/// </summary>
+	public static readonly BindableProperty MaxValueProperty = BindableProperty.Create(nameof(MaxValue), typeof(TValue), typeof(IsInRangeConverter<TValue, TReturnObject>));
+	
+	/// <summary>
+	/// Bindable property for <see cref="MinValue"/>
+	/// </summary>
+	public static readonly BindableProperty MinValueProperty = BindableProperty.Create(nameof(MinValue), typeof(TValue), typeof(IsInRangeConverter<TValue, TReturnObject>));
+
+	/// <summary>
+	/// Bindable property for <see cref="TrueObject"/>
+	/// </summary>
+	public static readonly BindableProperty TrueObjectProperty = BindableProperty.Create(nameof(TrueObject), typeof(TReturnObject?), typeof(IsInRangeConverter<TValue, TReturnObject>));
+	
 	/// <inheritdoc/>
 	public override object DefaultConvertReturnValue { get; set; } = new();
 
 	/// <summary>If supplied this value will be returned when the converter receives an input value that is <b>outside</b> of the <see cref="MinValue" /> and <see cref="MaxValue" />s.</summary>
-	public TReturnObject? FalseObject { get; set; }
+	public TReturnObject? FalseObject
+	{
+		get => (TReturnObject?)GetValue(FalseObjectProperty);
+		set => SetValue(FalseObjectProperty, value);
+	}
 
 	/// <summary>The upper bounds of the range to compare against when determining whether the input value to the convert is within range.</summary>
-	public TValue? MaxValue { get; set; }
+	public TValue? MaxValue
+	{
+		get => (TValue?)GetValue(MaxValueProperty);
+		set => SetValue(MaxValueProperty, value);
+	}
 
 	/// <summary>The lower bounds of the range to compare against when determining whether the input value to the convert is within range.</summary>
-	public TValue? MinValue { get; set; }
+	public TValue? MinValue
+	{
+		get => (TValue?)GetValue(MinValueProperty);
+		set => SetValue(MinValueProperty, value);
+	}
 
 	/// <summary>If supplied this value will be returned when the converter receives an input value that is <b>inside</b> (inclusive) of the <see cref="MinValue" /> and <see cref="MaxValue" />s.</summary>
-	public TReturnObject? TrueObject { get; set; }
+	public TReturnObject? TrueObject
+	{
+		get => (TReturnObject?)GetValue(TrueObjectProperty);
+		set => SetValue(TrueObjectProperty, value);
+	}
 
 	/// <summary>Converts an object that implemented IComparable to a <see cref="bool"/> based on the object being within a <see cref="MinValue"/> and <see cref="MaxValue"/> range.</summary>
 	/// <param name="value">The value to convert.</param>
