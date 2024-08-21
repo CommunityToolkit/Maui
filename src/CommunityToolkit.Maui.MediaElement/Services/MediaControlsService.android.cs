a4762f70a084adeba8cccac251409dff62a01c0d
James Crutchley <ne0rmatrix@gmail.com>
2024-08-21 16:38:35 -0700

    Fix Android App Exit On Back button pressed (#2145)
    Simplify OnDestroy method in MediaControlsService
    
    Removed logging and explicit termination calls from OnDestroy
    method in MediaControlsService.android.cs. This change aims to
    allow for a more graceful shutdown process and avoid abrupt
    termination of the application.

--- src/CommunityToolkit.Maui.MediaElement/Services/MediaControlsService.android.cs
+++ src/CommunityToolkit.Maui.MediaElement/Services/MediaControlsService.android.cs
@@ -220,10 +220,6 @@ class MediaControlsService : Service
 	public override void OnDestroy()
 	{
 		Platform.CurrentActivity?.StopService(new Intent(Platform.AppContext, typeof(MediaControlsService)));
-		System.Diagnostics.Trace.TraceInformation("MediaControlsService destroyed.");
-		Platform.CurrentActivity?.FinishAndRemoveTask();
-		System.Environment.Exit(0);
-		System.Diagnostics.Trace.TraceInformation("Application exiting.");
 		base.OnDestroy();
 	}
 	static void BroadcastUpdate(string receiver, string action)
