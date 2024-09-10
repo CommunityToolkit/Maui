856a987cca5e6a12e9eb2bb83ce31c516405a540
Shaun Lawrence <17139988+bijington@users.noreply.github.com>
2024-09-10 20:54:16 +0100

    Move options enum out to a separate file

--- src/CommunityToolkit.Maui.Core/Interfaces/IDrawingView.shared.cs
+++ src/CommunityToolkit.Maui.Core/Interfaces/IDrawingView.shared.cs
@@ -87,19 +87,3 @@ public interface IDrawingView : IView
 	/// <param name="lastDrawingLine">Last drawing line</param>
 	void OnDrawingLineCompleted(IDrawingLine lastDrawingLine);
 }
-
-/// <summary>
-/// Enumeration of the options available when generating an image stream using the DrawingView.
-/// </summary>
-public enum DrawingViewOutputOption
-{
-	/// <summary>
-	/// Outputs the area covered by the top-left to the bottom-right most points.
-	/// </summary>
-	Lines,
-	
-	/// <summary>
-	/// Outputs the full area displayed within the drawing view.
-	/// </summary>
-	FullCanvas
-}
