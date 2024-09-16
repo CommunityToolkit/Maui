9ea2053b33af010e152430fa618e050e876704ae
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2024-09-16 08:47:43 -0600

    Add Verifiers

---
+++ src/CommunityToolkit.Maui.Analyzers.UnitTests/Verifiers/CSharpAnalyzerVerifier`1+Test.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Testing;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Testing.Verifiers;
+
+namespace CommunityToolkit.Maui.Analyzers.UnitTests;
+public static partial class CSharpAnalyzerVerifier<TAnalyzer>
+	where TAnalyzer : DiagnosticAnalyzer, new()
+{
+	public class Test : CSharpAnalyzerTest<TAnalyzer, Microsoft.CodeAnalysis.Testing.DefaultVerifier>
+	{
+		public Test()
+		{
+			SolutionTransforms.Add((solution, projectId) =>
+			{
+				ArgumentNullException.ThrowIfNull(solution);
+
+				if (solution.GetProject(projectId) is not Project project)
+				{
+					throw new ArgumentException("Invalid ProjectId");
+				}
+
+				var compilationOptions = project.CompilationOptions ?? throw new InvalidOperationException($"{nameof(project.CompilationOptions)} cannot be null");
+				compilationOptions = compilationOptions.WithSpecificDiagnosticOptions(
+					compilationOptions.SpecificDiagnosticOptions.SetItems(CSharpVerifierHelper.NullableWarnings));
+				solution = solution.WithProjectCompilationOptions(projectId, compilationOptions);
+
+				return solution;
+			});
+		}
+	}
+}
---
+++ src/CommunityToolkit.Maui.Analyzers.UnitTests/Verifiers/CSharpCodeFixVerifier`2+Test.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp.Testing;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Testing;
+using Microsoft.CodeAnalysis.Testing.Verifiers;
+
+namespace CommunityToolkit.Maui.Analyzers.UnitTests;
+public static partial class CSharpCodeFixVerifier<TAnalyzer, TCodeFix>
+	where TAnalyzer : DiagnosticAnalyzer, new()
+	where TCodeFix : CodeFixProvider, new()
+{
+	public class Test : CSharpCodeFixTest<TAnalyzer, TCodeFix, DefaultVerifier>
+	{
+		public Test()
+		{
+			SolutionTransforms.Add((solution, projectId) =>
+			{
+				ArgumentNullException.ThrowIfNull(solution);
+
+				if (solution.GetProject(projectId) is not Project project)
+				{
+					throw new ArgumentException("Invalid ProjectId");
+				}
+
+				var compilationOptions = project.CompilationOptions ?? throw new InvalidOperationException($"{nameof(project.CompilationOptions)} cannot be null");
+				compilationOptions = compilationOptions.WithSpecificDiagnosticOptions(
+					compilationOptions.SpecificDiagnosticOptions.SetItems(CSharpVerifierHelper.NullableWarnings));
+				solution = solution.WithProjectCompilationOptions(projectId, compilationOptions);
+
+				return solution;
+			});
+		}
+	}
+}
