a55e4561e3e184b51b8f1d7f355a601ca9a06a6b
James Crutchley <ne0rmatrix@gmail.com>
2024-09-27 13:14:09 -0700

    Add custom transport controls and resource dictionary
    Introduced a new embedded resource, `ResourceDictionary.windows.xaml`, in the `CommunityToolkit.Maui.MediaElement.csproj` project file.
    
    Updated `MauiMediaElement.windows.cs`:
    - Imported new namespaces.
    - Added `CustomTransportControls` field and methods for loading resource dictionary and applying custom styles.
    - Updated constructor to load resource dictionary and set custom transport controls.
    - Added `LoadResourceDictionary`, `ApplyCustomStyle`, and `SetTransportControls` methods.
    - Updated `Dispose` method to handle custom transport controls.
    - Removed `OnMediaPlayerElementPointerMoved` method and related code.
    - Updated `OnFullScreenButtonClick` method to remove fullscreen button references.
    
    Updated `MediaManager.windows.cs`:
    - Disabled `SystemMediaTransportControls` in `CreatePlatformView`.
    
    Added `CustomTransportControls` class in `CommunityToolkit.Maui.Primitives`:
    - Imported necessary namespaces.
    - Defined `FullScreenButton` property.
    - Implemented `OnTemplateLoaded` event.
    - Overrode `OnApplyTemplate` method to add full-screen button.
    - Added `FullScreenButton_Click` event handler.
    - Introduced `isFullScreen` field to track full-screen state.

---
+++ src/CommunityToolkit.Maui.MediaElement/Primitives/CustomTransportControls.windows.cs
@@ -0,0 +1,65 @@
+﻿using CommunityToolkit.Maui.Core;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Button = Microsoft.UI.Xaml.Controls.Button;
+namespace CommunityToolkit.Maui.Primitives;
+
+/// <summary>
+/// A class that provides a way to create custom transport controls for a <see cref="IMediaElement"/>.
+/// </summary>
+public sealed class CustomTransportControls : MediaTransportControls
+{
+	/// <summary>
+	/// An event that triggers when the template is loaded.
+	/// </summary>
+	public event EventHandler<EventArgs>? OnTemplateLoaded;
+
+	/// <summary>
+	/// A button for Full screen controls.
+	/// </summary>
+	public AppBarButton? FullScreenButton { get; private set; }
+	bool isFullScreen = false;
+
+	/// <summary>
+	/// A <see cref="CustomTransportControls"/> for a <see cref="IMediaElement"/>.
+	/// </summary>
+	public CustomTransportControls()
+	{
+		this.DefaultStyleKey = typeof(CustomTransportControls);
+	}
+
+	/// <summary>
+	/// Add full screen button to the template.
+	/// </summary>
+	protected override void OnApplyTemplate()
+	{
+		base.OnApplyTemplate();
+
+		var temp = GetTemplateChild("FullWindowButton") as AppBarButton;
+		if(temp is not null)
+		{
+			FullScreenButton = temp;
+			FullScreenButton.Visibility = Microsoft.UI.Xaml.Visibility.Visible;
+			OnTemplateLoaded?.Invoke(this, EventArgs.Empty);
+			FullScreenButton.Click += FullScreenButton_Click;
+		}
+	}
+
+	void FullScreenButton_Click(object sender, RoutedEventArgs e)
+	{
+		if (FullScreenButton is null)
+		{
+			return;
+		}
+		if (isFullScreen)
+		{
+			FullScreenButton.Icon = new FontIcon { Glyph = "\uE740" };
+			isFullScreen = false;
+		}
+		else
+		{
+			FullScreenButton.Icon = new SymbolIcon(Symbol.BackToWindow);
+			isFullScreen = true;
+		}
+	}
+}
