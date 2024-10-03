ed06645844df6aa05216eaab44b984f65020be09
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2024-10-03 13:26:17 -0700

    Fix Crash on iOS when using MediaElement inside a CarouselView (again) (#2245)
    * Fix DataTemplate Support
    
    * Update MauiMediaElement.macios.cs
    
    * Re-add PageExtensions

---
+++ src/CommunityToolkit.Maui.MediaElement/Extensions/ElementExtensions.shared.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics.CodeAnalysis;
+namespace CommunityToolkit.Maui.Extensions;
+
+static class ElementExtensions
+{
+	public static bool TryFindParent<T>(this Element? child, [NotNullWhen(true)] out T? parent) where T : VisualElement
+	{
+		while (child is not null)
+		{
+			if (child.Parent is T element)
+			{
+				parent = element;
+				return true;
+			}
+
+			child = child.Parent;
+		}
+
+		parent = null;
+		return false;
+	}
+}
