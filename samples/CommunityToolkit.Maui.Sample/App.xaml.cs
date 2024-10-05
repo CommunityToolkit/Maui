6401190ec2e4af39b6aa777005df635b4d227c4a
James Crutchley <ne0rmatrix@gmail.com>
2024-10-05 10:34:56 -0700

    Add initial support for Multiple Windows on MacOS

--- samples/CommunityToolkit.Maui.Sample/App.xaml.cs
+++ samples/CommunityToolkit.Maui.Sample/App.xaml.cs
@@ -7,7 +7,24 @@ public partial class App : Application
 	public App()
 	{
 		InitializeComponent();
-
 		MainPage = new AppShell();
 	}
+
+#if MACCATALYST
+	protected override Window CreateWindow(IActivationState? activationState)
+    {
+        Window window = base.CreateWindow(activationState);
+        window.Destroying += (object? sender, EventArgs args) =>
+        {
+            if (Current?.Windows?.Count - 1 == 0) 
+			{
+				// Exit the app when the last window closes
+				// This ensures app closes when last window closes on Mac
+				Environment.Exit(0);
+			}
+        };
+        return window;
+    }
+
+#endif
 }
