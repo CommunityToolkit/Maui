b1c85dadc87e6ef22985011cfe249a15dc9c0e76
github@internetwideworld.com <github@internetwideworld.com>
2024-10-07 14:56:06 +0100

    Added Toast to XAML sample Fixed row height for pickers

--- samples/CommunityToolkit.Maui.Sample/Pages/Views/RatingView/RatingViewXamlPage.xaml.cs
+++ samples/CommunityToolkit.Maui.Sample/Pages/Views/RatingView/RatingViewXamlPage.xaml.cs
@@ -1,3 +1,4 @@
+using CommunityToolkit.Maui.Alerts;
 using CommunityToolkit.Maui.Core;
 using CommunityToolkit.Maui.Sample.ViewModels.Views;
 using CommunityToolkit.Maui.Views;
@@ -11,12 +12,12 @@ public partial class RatingViewXamlPage : BasePage<RatingViewXamlViewModel>
 		InitializeComponent();
 	}
 
-	void StepperMaximumRating_RatingChanged(object? sender, RatingChangedEventArgs e)
+	static async void StepperMaximumRating_RatingChanged(object? sender, RatingChangedEventArgs e)
 	{
-		// This is the weak event raised when the rating is changed.  The developer can then perform further actions (such as save to DB).
 		if (sender is RatingView ratingView)
 		{
-			_ = ratingView.Rating;
+			// This is the weak event raised when the rating is changed.  The developer can then perform further actions (such as save to DB).
+			await Toast.Make($"New Rating: {ratingView.Rating:F2}").Show(CancellationToken.None);
 		}
 	}
 
