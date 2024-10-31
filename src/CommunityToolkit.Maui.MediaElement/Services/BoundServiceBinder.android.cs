ceae6487e833fae56737d7164c2adca28fe405e2
James Crutchley <ne0rmatrix@gmail.com>
2024-10-31 15:46:50 -0700

    Make Bound Service immutable

--- src/CommunityToolkit.Maui.MediaElement/Services/BoundServiceBinder.android.cs
+++ src/CommunityToolkit.Maui.MediaElement/Services/BoundServiceBinder.android.cs
@@ -3,7 +3,7 @@ using CommunityToolkit.Maui.Media.Services;
 
 namespace CommunityToolkit.Maui.Services;
 
-class BoundServiceBinder(MediaControlsService mediaControlsService) : Binder
+sealed class BoundServiceBinder(MediaControlsService mediaControlsService) : Binder
 {
-	public MediaControlsService? Service { get; private set; } = mediaControlsService;
+	public MediaControlsService Service { get; } = mediaControlsService;
 }
