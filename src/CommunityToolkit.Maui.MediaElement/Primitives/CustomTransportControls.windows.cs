43e327c3fd8201e00154e62f899e6729d1fcf5c9
James Crutchley <ne0rmatrix@gmail.com>
2024-11-09 01:51:00 -0800

    Refactor CustomTransportControls class and clean up code
    - Cleaned up `using` directives by removing redundant `using CommunityToolkit.Maui.Core;` and reordering the remaining directives.
    - Changed `CustomTransportControls` class from public sealed to sealed partial.
    - Removed XML documentation comments for `CustomTransportControls` class and its members.
    - Changed `FullScreenButton` property from nullable `AppBarButton` with a private setter to non-nullable `AppBarButton` initialized with a new instance.
    - Removed null check for `FullScreenButton` in `FullScreenButton_Click` method as `FullScreenButton` is no longer nullable.

--- src/CommunityToolkit.Maui.MediaElement/Primitives/CustomTransportControls.windows.cs
+++ src/CommunityToolkit.Maui.MediaElement/Primitives/CustomTransportControls.windows.cs
@@ -1,36 +1,18 @@
-﻿using CommunityToolkit.Maui.Core;
-using Microsoft.UI.Xaml;
+﻿using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Button = Microsoft.UI.Xaml.Controls.Button;
 namespace CommunityToolkit.Maui.Primitives;
 
-/// <summary>
-/// A class that provides a way to create custom transport controls for a <see cref="IMediaElement"/>.
-/// </summary>
-public sealed class CustomTransportControls : MediaTransportControls
+sealed partial class CustomTransportControls : MediaTransportControls
 {
-	/// <summary>
-	/// An event that triggers when the template is loaded.
-	/// </summary>
 	public event EventHandler<EventArgs>? OnTemplateLoaded;
-
-	/// <summary>
-	/// A button for Full screen controls.
-	/// </summary>
-	public AppBarButton? FullScreenButton { get; private set; }
+	public AppBarButton FullScreenButton = new();
 	bool isFullScreen = false;
 
-	/// <summary>
-	/// A <see cref="CustomTransportControls"/> for a <see cref="IMediaElement"/>.
-	/// </summary>
 	public CustomTransportControls()
 	{
 		this.DefaultStyleKey = typeof(CustomTransportControls);
 	}
 
-	/// <summary>
-	/// Add full screen button to the template.
-	/// </summary>
 	protected override void OnApplyTemplate()
 	{
 		base.OnApplyTemplate();
@@ -47,10 +29,6 @@ public sealed class CustomTransportControls : MediaTransportControls
 
 	void FullScreenButton_Click(object sender, RoutedEventArgs e)
 	{
-		if (FullScreenButton is null)
-		{
-			return;
-		}
 		if (isFullScreen)
 		{
 			FullScreenButton.Icon = new FontIcon { Glyph = "\uE740" };
