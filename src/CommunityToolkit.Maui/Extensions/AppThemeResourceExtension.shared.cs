bd8d383954a1d518e75b5fddffb44a04fbfb2a93
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2024-11-12 15:11:32 -0800

    Enable IServiceProvider

--- src/CommunityToolkit.Maui/Extensions/AppThemeResourceExtension.shared.cs
+++ src/CommunityToolkit.Maui/Extensions/AppThemeResourceExtension.shared.cs
@@ -3,7 +3,7 @@
 /// <summary>
 /// A XAML markup extension that enables using <see cref="AppThemeColor"/> and <see cref="AppThemeObject"/> from XAML.
 /// </summary>
-[ContentProperty(nameof(Key)), AcceptEmptyServiceProvider]
+[ContentProperty(nameof(Key)), RequireService([typeof(IServiceProvider)])]
 public sealed class AppThemeResourceExtension : IMarkupExtension<BindingBase>
 {
 	/// <summary>
