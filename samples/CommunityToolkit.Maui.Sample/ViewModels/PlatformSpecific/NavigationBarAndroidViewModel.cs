7a71daeef49ea108416eff89f635ef42498352ec
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2024-12-17 16:23:36 -0800

    Use Partial Properties

--- samples/CommunityToolkit.Maui.Sample/ViewModels/PlatformSpecific/NavigationBarAndroidViewModel.cs
+++ samples/CommunityToolkit.Maui.Sample/ViewModels/PlatformSpecific/NavigationBarAndroidViewModel.cs
@@ -2,34 +2,49 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace CommunityToolkit.Maui.Sample.ViewModels.PlatformSpecific;
+
 public partial class NavigationBarAndroidViewModel : BaseViewModel
 {
-	[ObservableProperty, NotifyPropertyChangedFor(nameof(NavigationBarColor))]
-	int redSliderValue, greenSliderValue, blueSliderValue;
+    [ObservableProperty, NotifyPropertyChangedFor(nameof(NavigationBarColor))]
+    public partial int RedSliderValue { get; private set; }
 
-	[ObservableProperty, NotifyPropertyChangedFor(nameof(NavigationBarStyle))]
-	bool isLightContentChecked, isDarkContentChecked, isDefaultChecked = true;
+    [ObservableProperty, NotifyPropertyChangedFor(nameof(NavigationBarColor))]
+    public partial int GreenSliderValue { get; private set; }
 
-	public Color NavigationBarColor => Color.FromRgb(RedSliderValue, GreenSliderValue, BlueSliderValue);
+    [ObservableProperty, NotifyPropertyChangedFor(nameof(NavigationBarColor))]
+    public partial int BlueSliderValue { get; private set; }
 
-	public NavigationBarStyle NavigationBarStyle
-	{
-		get
-		{
-			if (IsDefaultChecked)
-			{
-				return NavigationBarStyle.Default;
-			}
-			if (IsLightContentChecked)
-			{
-				return NavigationBarStyle.LightContent;
-			}
-			if (IsDarkContentChecked)
-			{
-				return NavigationBarStyle.DarkContent;
-			}
+    [ObservableProperty, NotifyPropertyChangedFor(nameof(NavigationBarStyle))]
+    public partial bool IsLightContentChecked { get; private set; }
 
-			throw new NotSupportedException($"{nameof(NavigationBarStyle)} {NavigationBarStyle} is not supported.");
-		}
-	}
+    [ObservableProperty, NotifyPropertyChangedFor(nameof(NavigationBarStyle))]
+    public partial bool IsDarkContentChecked { get; private set; }
+
+    [ObservableProperty, NotifyPropertyChangedFor(nameof(NavigationBarStyle))]
+    public partial bool IsDefaultChecked { get; private set; } = true;
+
+    public Color NavigationBarColor => Color.FromRgb(RedSliderValue, GreenSliderValue, BlueSliderValue);
+
+    public NavigationBarStyle NavigationBarStyle
+    {
+        get
+        {
+            if (IsDefaultChecked)
+            {
+                return NavigationBarStyle.Default;
+            }
+
+            if (IsLightContentChecked)
+            {
+                return NavigationBarStyle.LightContent;
+            }
+
+            if (IsDarkContentChecked)
+            {
+                return NavigationBarStyle.DarkContent;
+            }
+
+            throw new NotSupportedException($"{nameof(NavigationBarStyle)} {NavigationBarStyle} is not supported.");
+        }
+    }
 }
