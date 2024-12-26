0630c5c58bcc0cad0f9eac3396b5be061e29a030
Pedro Jesus <pedrojesus.cefet@gmail.com>
2024-12-25 23:24:15 -0300

    create services for DialogFragment

---
+++ src/CommunityToolkit.Maui.Core/Services/DialogFragmentService.android.cs
@@ -0,0 +1,192 @@
+﻿using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using Android.Content;
+using Android.Views;
+using AndroidX.AppCompat.App;
+using DialogFragment = AndroidX.Fragment.App.DialogFragment;
+using Fragment = AndroidX.Fragment.App.Fragment;
+using FragmentManager = AndroidX.Fragment.App.FragmentManager;
+
+namespace CommunityToolkit.Maui.Core.Services;
+
+sealed class DialogFragmentService : IDialogFragmentService
+{
+	public void OnFragmentAttached(FragmentManager fm, Fragment f, Context context)
+	{
+		if (!IsDialogFragment(f, out var dialogFragment))
+		{
+			return;
+		}
+	}
+
+	public void OnFragmentCreated(FragmentManager fm, Fragment f, Bundle? savedInstanceState)
+	{
+		if (!IsDialogFragment(f, out var dialogFragment))
+		{
+			return;
+		}
+	}
+
+	public void OnFragmentDestroyed(FragmentManager fm, Fragment f)
+	{
+		if (!IsDialogFragment(f, out var dialogFragment))
+		{
+			return;
+		}
+	}
+
+	public void OnFragmentDetached(FragmentManager fm, Fragment f)
+	{
+		if (!IsDialogFragment(f, out var dialogFragment))
+		{
+			return;
+		}
+	}
+
+	public void OnFragmentPaused(FragmentManager fm, Fragment f)
+	{
+		if (!IsDialogFragment(f, out var dialogFragment))
+		{
+			return;
+		}
+	}
+
+	public void OnFragmentPreAttached(FragmentManager fm, Fragment f, Context context)
+	{
+		if (!IsDialogFragment(f, out var dialogFragment))
+		{
+			return;
+		}
+	}
+
+	public void OnFragmentPreCreated(FragmentManager fm, Fragment f, Bundle? savedInstanceState)
+	{
+		if (!IsDialogFragment(f, out var dialogFragment))
+		{
+			return;
+		}
+	}
+
+	public void OnFragmentResumed(FragmentManager fm, Fragment f)
+	{
+		if (!IsDialogFragment(f, out var dialogFragment))
+		{
+			return;
+		}
+	}
+
+	public void OnFragmentSaveInstanceState(FragmentManager fm, Fragment f, Bundle outState)
+	{
+		if (!IsDialogFragment(f, out var dialogFragment))
+		{
+			return;
+		}
+	}
+
+	public void OnFragmentStarted(FragmentManager fm, Fragment f)
+	{
+		if (!IsDialogFragment(f, out var dialogFragment) || Microsoft.Maui.ApplicationModel.Platform.CurrentActivity is not AppCompatActivity activity)
+		{
+			return;
+		}
+
+		HandleStatusBarColor(dialogFragment, activity);
+	}
+
+	static void HandleStatusBarColor(DialogFragment dialogFragment, AppCompatActivity activity)
+	{
+		if (activity.Window is null)
+		{
+			return;
+		}
+
+		var statusBarColor = activity.Window.StatusBarColor;
+		var platformColor = new Android.Graphics.Color(statusBarColor);
+		var dialog = dialogFragment.Dialog;
+
+		Debug.Assert(dialog is not null);
+		Debug.Assert(dialog.Window is not null);
+
+		var window = dialog.Window;
+
+		bool isColorTransparent = platformColor == Android.Graphics.Color.Transparent;
+
+		if (OperatingSystem.IsAndroidVersionAtLeast(30))
+		{
+			var windowInsetsController = window.InsetsController;
+			var appearance = activity.Window.InsetsController?.SystemBarsAppearance;
+
+			Debug.Assert(windowInsetsController is not null);
+
+			if (appearance.HasValue)
+			{
+				windowInsetsController.SetSystemBarsAppearance(appearance.Value, appearance.Value);
+			}
+			else
+			{
+				windowInsetsController.SetSystemBarsAppearance(
+						isColorTransparent ? 0 : (int)WindowInsetsControllerAppearance.LightStatusBars,
+						(int)WindowInsetsControllerAppearance.LightStatusBars);
+			}
+			window.SetStatusBarColor(platformColor);
+			if (!OperatingSystem.IsAndroidVersionAtLeast(35))
+			{
+				window.SetDecorFitsSystemWindows(!isColorTransparent);
+			}
+			else
+			{
+				AndroidX.Core.View.WindowCompat.SetDecorFitsSystemWindows(window, !isColorTransparent);
+			}
+		}
+		else
+		{
+			dialog.Window.SetStatusBarColor(platformColor);
+
+			if (isColorTransparent)
+			{
+				window.ClearFlags(WindowManagerFlags.DrawsSystemBarBackgrounds);
+				window.SetFlags(WindowManagerFlags.LayoutNoLimits, WindowManagerFlags.LayoutNoLimits);
+			}
+			else
+			{
+				window.ClearFlags(WindowManagerFlags.LayoutNoLimits);
+				window.SetFlags(WindowManagerFlags.DrawsSystemBarBackgrounds, WindowManagerFlags.DrawsSystemBarBackgrounds);
+			}
+		}
+	}
+
+	public void OnFragmentStopped(FragmentManager fm, Fragment f)
+	{
+		if (!IsDialogFragment(f, out var dialogFragment))
+		{
+			return;
+		}
+	}
+
+	public void OnFragmentViewCreated(FragmentManager fm, Fragment f, View v, Bundle? savedInstanceState)
+	{
+		if (!IsDialogFragment(f, out var dialogFragment))
+		{
+			return;
+		}
+	}
+
+	public void OnFragmentViewDestroyed(FragmentManager fm, Fragment f)
+	{
+		if (!IsDialogFragment(f, out var dialogFragment))
+		{
+			return;
+		}
+	}
+
+	static bool IsDialogFragment(Fragment fragment, [NotNullWhen(true)] out DialogFragment? dialogFragment)
+	{
+		dialogFragment = null;
+		if (fragment is DialogFragment dialog)
+		{
+			dialogFragment = dialog;
+			return true;
+		}
+		return false;
+	}
+}
