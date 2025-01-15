fbba90dd3a4ae715f8b0e3818520a1d1ad141f2f
Vladislav Antonyuk <vladislav.antonyuk@dataart.com>
2025-01-15 02:19:53 +0200

    register all popups

--- samples/CommunityToolkit.Maui.Sample/MauiProgram.cs
+++ samples/CommunityToolkit.Maui.Sample/MauiProgram.cs
@@ -266,10 +266,25 @@ public static class MauiProgram
 		services.AddTransientWithShellRoute<PopupSizingIssuesPage, PopupSizingIssuesViewModel>();
 
 		// Add Popups
+		services.AddPopup<ApplyToDerivedTypesPopup>();
+		services.AddPopup<ButtonPopup>();
 		services.AddPopup<CsharpBindingPopup, CsharpBindingPopupViewModel>();
+		services.AddPopup<DynamicStyleInheritancePopup>();
+		services.AddPopup<DynamicStylePopup>();
+		services.AddPopup<ExplicitStylePopup>();
+		services.AddPopup<ImplicitStylePopup>();
+		services.AddPopup<MultipleButtonPopup>();
+		services.AddPopup<NoOutsideTapDismissPopup>();
+		services.AddPopup<OpenedEventSimplePopup>();
+		services.AddPopup<RedBlueBoxPopup>();
+		services.AddPopup<ReturnResultPopup>();
+		services.AddPopup<SimplePopup>();
+		services.AddPopup<StyleClassPopup>();
+		services.AddPopup<StyleInheritancePopup>();
+		services.AddPopup<ToggleSizePopup>();
+		services.AddPopup<TransparentPopup>();
 		services.AddPopup<UpdatingPopup, UpdatingPopupViewModel>();
 		services.AddPopup<XamlBindingPopup, XamlBindingPopupViewModel>();
-		services.AddPopup<ReturnResultPopup>();
 
 		services.AddPopup<PopupContentView, PopupContentViewModel>();
 	}
