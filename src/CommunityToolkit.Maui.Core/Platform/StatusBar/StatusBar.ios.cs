75093d9a121d3f1d0493e06a9c16746e33f224d8
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2025-01-16 12:19:34 -0800

    Remove iOS 13 check (Toolkit only supports iOS 15+)

--- src/CommunityToolkit.Maui.Core/Platform/StatusBar/StatusBar.ios.cs
+++ src/CommunityToolkit.Maui.Core/Platform/StatusBar/StatusBar.ios.cs
@@ -44,51 +44,38 @@ static partial class StatusBar
 	static void PlatformSetColor(Color color)
 	{
 		var uiColor = color.ToPlatform();
-
-		if (OperatingSystem.IsIOSVersionAtLeast(13))
+		
+		var statusBarTag = new IntPtr(38482);
+		foreach (var window in UIApplication.SharedApplication.Windows)
 		{
-			var statusBarTag = new IntPtr(38482);
-			foreach (var window in UIApplication.SharedApplication.Windows)
+			var statusBar = window.ViewWithTag(statusBarTag);
+			var statusBarFrame = window.WindowScene?.StatusBarManager?.StatusBarFrame;
+			if (statusBarFrame is null)
 			{
-				var statusBar = window.ViewWithTag(statusBarTag);
-				var statusBarFrame = window.WindowScene?.StatusBarManager?.StatusBarFrame;
-				if (statusBarFrame is null)
-				{
-					continue;
-				}
-
-				// ReSharper disable once NullCoalescingConditionIsAlwaysNotNullAccordingToAPIContract
-				// window.ViewWithTag(tag) can return null
-				var statusBarSize = statusBar?.Frame.Size;
-				statusBar ??= new UIView(statusBarFrame.Value);
-				statusBar.Tag = statusBarTag;
-				statusBar.BackgroundColor = uiColor;
-				statusBar.TintColor = uiColor;
-				if (statusBarSize is null)
-				{
-					statusBar.Frame = new CGRect(statusBar.Frame.X, statusBar.Frame.Y, statusBar.Frame.Width, window.SafeAreaInsets.Top);
-				}
-
-				var statusBarSubViews = window.Subviews.Where(x => x.Tag == statusBarTag).ToList();
-				foreach (var statusBarSubView in statusBarSubViews)
-				{
-					statusBarSubView.RemoveFromSuperview();
-				}
-
-				window.AddSubview(statusBar);
-
-				TryUpdateStatusBarAppearance(window);
-			}
-		}
-		else
-		{
-			if (UIApplication.SharedApplication.ValueForKey(new NSString("statusBar")) is UIView statusBar
-				&& statusBar.RespondsToSelector(new ObjCRuntime.Selector("setBackgroundColor:")))
-			{
-				statusBar.BackgroundColor = uiColor;
+				continue;
 			}
 
-			TryUpdateStatusBarAppearance();
+			// ReSharper disable once NullCoalescingConditionIsAlwaysNotNullAccordingToAPIContract
+			// window.ViewWithTag(tag) can return null
+			var statusBarSize = statusBar?.Frame.Size;
+			statusBar ??= new UIView(statusBarFrame.Value);
+			statusBar.Tag = statusBarTag;
+			statusBar.BackgroundColor = uiColor;
+			statusBar.TintColor = uiColor;
+			if (statusBarSize is null)
+			{
+				statusBar.Frame = new CGRect(statusBar.Frame.X, statusBar.Frame.Y, statusBar.Frame.Width, window.SafeAreaInsets.Top);
+			}
+
+			var statusBarSubViews = window.Subviews.Where(x => x.Tag == statusBarTag).ToList();
+			foreach (var statusBarSubView in statusBarSubViews)
+			{
+				statusBarSubView.RemoveFromSuperview();
+			}
+
+			window.AddSubview(statusBar);
+
+			TryUpdateStatusBarAppearance(window);
 		}
 	}
 
