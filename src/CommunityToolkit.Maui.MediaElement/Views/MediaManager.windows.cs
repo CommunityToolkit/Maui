f7b6339c5d72f42b9266bf52d81404a30f698855
James Crutchley <ne0rmatrix@gmail.com>
2025-01-21 18:03:24 -0800

    Fix volume slider bug with Should Mute

--- src/CommunityToolkit.Maui.MediaElement/Views/MediaManager.windows.cs
+++ src/CommunityToolkit.Maui.MediaElement/Views/MediaManager.windows.cs
@@ -252,12 +252,11 @@ partial class MediaManager : IDisposable
 
 	protected virtual partial void PlatformUpdateShouldMute()
 	{
-		if (Player?.MediaPlayer is null)
+		if (Player is null)
 		{
 			return;
 		}
-
-		Player.MediaPlayer.IsMuted = MediaElement.ShouldMute;
+		Dispatcher.Dispatch(() => Player.MediaPlayer.IsMuted = MediaElement.ShouldMute);
 	}
 
 	protected virtual async partial ValueTask PlatformUpdateSource()
