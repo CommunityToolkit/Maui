e71032704014c15db214930293674cb66d3ebea7
Brandon Minnick <13558917+TheCodeTraveler@users.noreply.github.com>
2025-02-13 11:45:54 -0800

    Add VerifyIncludedShapePaths Unit Test

--- src/CommunityToolkit.Maui.UnitTests/Views/RatingView/RatingViewTests.cs
+++ src/CommunityToolkit.Maui.UnitTests/Views/RatingView/RatingViewTests.cs
@@ -12,6 +12,22 @@ namespace CommunityToolkit.Maui.UnitTests.Views;
 public class RatingViewTests : BaseHandlerTest
 {
 	const string customShape = "M 12 0C5.388 0 0 5.388 0 12s5.388 12 12 12 12-5.38 12-12c0-6.612-5.38-12-12-12z";
+
+	[Fact]
+	public void VerifyIncludedShapePaths()
+	{
+		const string expectedStarPath = "M9 11.3l3.71 2.7-1.42-4.36L15 7h-4.55L9 2.5 7.55 7H3l3.71 2.64L5.29 14z";
+		const string expectedHeartPath = "M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z";
+		const string expectedCirclePath = "M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2z";
+		const string expectedLikePath = "M1 21h4V9H1v12zm22-11c0-1.1-.9-2-2-2h-6.31l.95-4.57.03-.32c0-.41-.17-.79-.44-1.06L14.17 1 7.59 7.59C7.22 7.95 7 8.45 7 9v10c0 1.1.9 2 2 2h9c.83 0 1.54-.5 1.84-1.22l3.02-7.05c.09-.23.14-.47.14-.73v-1.91l-.01-.01L23 10z";
+		const string expectedDislikePath = "M15 3H6c-.83 0-1.54.5-1.84 1.22l-3.02 7.05c-.09.23-.14.47-.14.73v1.91l.01.01L1 14c0 1.1.9 2 2 2h6.31l-.95 4.57-.03.32c0 .41.17.79.44 1.06L9.83 23l6.59-6.59c.36-.36.58-.86.58-1.41V5c0-1.1-.9-2-2-2zm4 0v12h4V3h-4z";
+
+		RatingView.PathShapes.Star.Should().BeEquivalentTo(expectedStarPath);
+		RatingView.PathShapes.Heart.Should().BeEquivalentTo(expectedHeartPath);
+		RatingView.PathShapes.Circle.Should().BeEquivalentTo(expectedCirclePath);
+		RatingView.PathShapes.Like.Should().BeEquivalentTo(expectedLikePath);
+		RatingView.PathShapes.Dislike.Should().BeEquivalentTo(expectedDislikePath);
+	}
 	
 	[Fact]
 	public void Defaults_BindingContext()
--- src/CommunityToolkit.Maui/Views/RatingView/RatingView.PathShapes.shared.cs
+++ src/CommunityToolkit.Maui/Views/RatingView/RatingView.PathShapes.shared.cs
@@ -2,7 +2,7 @@ namespace CommunityToolkit.Maui.Views;
 
 public partial class RatingView
 {
-    static class PathShapes
+    internal static class PathShapes
     {
         public const string Star = "M9 11.3l3.71 2.7-1.42-4.36L15 7h-4.55L9 2.5 7.55 7H3l3.71 2.64L5.29 14z";
         public const string Heart = "M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z";
