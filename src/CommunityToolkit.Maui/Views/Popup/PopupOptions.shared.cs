ffdd87ce5033d63bc66f9df84e295c411f68dcb5
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2025-02-24 16:53:39 -0800

    Add `PopupOptionsDefaults`

--- src/CommunityToolkit.Maui/Views/Popup/PopupOptions.shared.cs
+++ src/CommunityToolkit.Maui/Views/Popup/PopupOptions.shared.cs
@@ -1,4 +1,6 @@
-﻿namespace CommunityToolkit.Maui;
+﻿using CommunityToolkit.Maui.Core;
+
+namespace CommunityToolkit.Maui;
 
 /// <summary>
 /// Popup options.
@@ -8,42 +10,42 @@ public partial class PopupOptions : BindableObject, IPopupOptions
 	/// <summary>
 	///  Backing BindableProperty for the <see cref="CanBeDismissedByTappingOutsideOfPopup"/> property.
 	/// </summary>
-	public static readonly BindableProperty CanBeDismissedByTappingOutsideOfPopupProperty = BindableProperty.Create(nameof(CanBeDismissedByTappingOutsideOfPopup), typeof(bool), typeof(PopupOptions), true);
+	public static readonly BindableProperty CanBeDismissedByTappingOutsideOfPopupProperty = BindableProperty.Create(nameof(CanBeDismissedByTappingOutsideOfPopup), typeof(bool), typeof(PopupOptions), PopupOptionsDefaults.CanBeDismissedByTappingOutsideOfPopup);
 
 	/// <summary>
 	///  Backing BindableProperty for the <see cref="OnTappingOutsideOfPopup"/> property.
 	/// </summary>
-	public static readonly BindableProperty OnTappingOutsideOfPopupProperty = BindableProperty.Create(nameof(OnTappingOutsideOfPopup), typeof(Action), typeof(PopupOptions), null);
+	public static readonly BindableProperty OnTappingOutsideOfPopupProperty = BindableProperty.Create(nameof(OnTappingOutsideOfPopup), typeof(Action), typeof(PopupOptions), PopupOptionsDefaults.OnTappingOutsideOfPopup);
 
 	/// <summary>
 	///  Backing BindableProperty for the <see cref="BackgroundColor"/> property.
 	/// </summary>
-	public static readonly BindableProperty BackgroundColorProperty = BindableProperty.Create(nameof(BackgroundColor), typeof(Color), typeof(PopupOptions), Color.FromRgba(0, 0, 0, 0.4));
+	public static readonly BindableProperty BackgroundColorProperty = BindableProperty.Create(nameof(BackgroundColor), typeof(Color), typeof(PopupOptions), PopupOptionsDefaults.BackgroundColor);
 
 	/// <summary>
 	///  Backing BindableProperty for the <see cref="Shape"/> property.
 	/// </summary>
-	public static readonly BindableProperty ShapeProperty = BindableProperty.Create(nameof(Shape), typeof(IShape), typeof(PopupOptions), null);
+	public static readonly BindableProperty ShapeProperty = BindableProperty.Create(nameof(Shape), typeof(IShape), typeof(PopupOptions), PopupOptionsDefaults.Shape);
 
 	/// <summary>
 	///  Backing BindableProperty for the <see cref="Margin"/> property.
 	/// </summary>
-	public static readonly BindableProperty MarginProperty = BindableProperty.Create(nameof(Margin), typeof(Thickness), typeof(PopupOptions), new Thickness(30));
+	public static readonly BindableProperty MarginProperty = BindableProperty.Create(nameof(Margin), typeof(Thickness), typeof(PopupOptions), PopupOptionsDefaults.Margin);
 
 	/// <summary>
 	///  Backing BindableProperty for the <see cref="Padding"/> property.
 	/// </summary>
-	public static readonly BindableProperty PaddingProperty = BindableProperty.Create(nameof(Padding), typeof(Thickness), typeof(PopupOptions), new Thickness(15));
+	public static readonly BindableProperty PaddingProperty = BindableProperty.Create(nameof(Padding), typeof(Thickness), typeof(PopupOptions), PopupOptionsDefaults.Padding);
 
 	/// <summary>
 	///  Backing BindableProperty for the <see cref="VerticalOptions"/> property.
 	/// </summary>
-	public static readonly BindableProperty VerticalOptionsProperty = BindableProperty.Create(nameof(VerticalOptions), typeof(LayoutOptions), typeof(PopupOptions), LayoutOptions.Center);
+	public static readonly BindableProperty VerticalOptionsProperty = BindableProperty.Create(nameof(VerticalOptions), typeof(LayoutOptions), typeof(PopupOptions), ConvertToLayoutOptions(PopupOptionsDefaults.VerticalOptions));
 
 	/// <summary>
 	///  Backing BindableProperty for the <see cref="HorizontalOptions"/> property.
 	/// </summary>
-	public static readonly BindableProperty HorizontalOptionsProperty = BindableProperty.Create(nameof(HorizontalOptions), typeof(LayoutOptions), typeof(PopupOptions), LayoutOptions.Center);
+	public static readonly BindableProperty HorizontalOptionsProperty = BindableProperty.Create(nameof(HorizontalOptions), typeof(LayoutOptions), typeof(PopupOptions), ConvertToLayoutOptions(PopupOptionsDefaults.HorizontalOptions));
 
 	/// <summary>
 	/// An empty instance of <see cref="IPopupOptions"/> containing default values.
@@ -111,4 +113,13 @@ public partial class PopupOptions : BindableObject, IPopupOptions
 		get => (LayoutOptions)GetValue(HorizontalOptionsProperty);
 		set => SetValue(HorizontalOptionsProperty, value);
 	}
+
+	static LayoutOptions ConvertToLayoutOptions(Microsoft.Maui.Primitives.LayoutAlignment layoutAlignment) => layoutAlignment switch
+	{
+		Microsoft.Maui.Primitives.LayoutAlignment.Start => LayoutOptions.Start,
+		Microsoft.Maui.Primitives.LayoutAlignment.Center => LayoutOptions.Center,
+		Microsoft.Maui.Primitives.LayoutAlignment.End => LayoutOptions.End,
+		Microsoft.Maui.Primitives.LayoutAlignment.Fill => LayoutOptions.Fill,
+		_ => throw new NotSupportedException($"{nameof(ConvertToLayoutOptions)} does not yet support {layoutAlignment}"),
+	};
 }
