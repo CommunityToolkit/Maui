573c774315f29db01cb5d4eadb342b9fd3cec58c
Shaun Lawrence <17139988+bijington@users.noreply.github.com>
2025-03-20 21:47:18 +0000

    Add some convert back tests

--- src/CommunityToolkit.Maui.UnitTests/Converters/BaseConverterTests.cs
+++ src/CommunityToolkit.Maui.UnitTests/Converters/BaseConverterTests.cs
@@ -30,6 +30,25 @@ public abstract class BaseConverterTests
 		Assert.Equal("Two", converter.Convert(inputValue, targetType, null, CultureInfo.CurrentCulture));
 	}
 	
+	[Theory]
+	[InlineData("4.123", typeof(Color))]
+	[InlineData("4", typeof(Color))]
+	public abstract void ConvertBack_WithMismatchedTargetType(object? inputValue, Type targetType);
+
+	[Theory]
+	[InlineData(4.123, typeof(string))]
+	[InlineData(true, typeof(string))]
+	public abstract void ConvertBack_WithInvalidValueType(object? inputValue, Type targetType);
+	
+	[Theory]
+	[InlineData("One", typeof(int))]
+	public void ConvertBack_ShouldCorrectlyReturnValueWithMatchingTargetType(object? inputValue, Type targetType)
+	{
+		ICommunityToolkitValueConverter converter = CreateConverter();
+
+		Assert.Equal(0, converter.ConvertBack(inputValue, targetType, null, CultureInfo.CurrentCulture));
+	}
+	
 	protected static BaseConverter<int, string> CreateConverter() => new MockConverter(["One", "Two", "Three"])
 	{
 		DefaultConvertReturnValue = "Three",
@@ -68,6 +87,24 @@ public class BaseConverterTestsWithExceptionsEnabled : BaseConverterTests
 		
 		exception.Message.Should().Be($"Value needs to be of type {converter.FromType} (Parameter 'value')");
 	}
+	
+	public override void ConvertBack_WithMismatchedTargetType(object? inputValue, Type targetType)
+	{
+		ICommunityToolkitValueConverter converter = CreateConverter();
+
+		var exception = Assert.Throws<ArgumentException>(() => converter.ConvertBack(inputValue, targetType, null, CultureInfo.CurrentCulture));
+		
+		exception.Message.Should().Be($"targetType needs to be assignable from {converter.FromType}. (Parameter 'targetType')");
+	}
+
+	public override void ConvertBack_WithInvalidValueType(object? inputValue, Type targetType)
+	{
+		ICommunityToolkitValueConverter converter = CreateConverter();
+
+		var exception = Assert.Throws<ArgumentException>(() => converter.ConvertBack(inputValue, targetType, null, CultureInfo.CurrentCulture));
+		
+		exception.Message.Should().Be($"Value needs to be of type {converter.ToType} (Parameter 'value')");
+	}
 }
 
 /// <summary>
@@ -91,5 +128,19 @@ public class BaseConverterTestsWithExceptionsSuppressed : BaseConverterTests
 		ICommunityToolkitValueConverter converter = CreateConverter();
 
 		converter.Convert(inputValue, targetType, null, CultureInfo.CurrentCulture).Should().Be(converter.DefaultConvertReturnValue);
+	}
+	
+	public override void ConvertBack_WithMismatchedTargetType(object? inputValue, Type targetType)
+	{
+		ICommunityToolkitValueConverter converter = CreateConverter();
+
+		converter.ConvertBack(inputValue, targetType, null, CultureInfo.CurrentCulture).Should().Be(converter.DefaultConvertBackReturnValue);
+	}
+	
+	public override void ConvertBack_WithInvalidValueType(object? inputValue, Type targetType)
+	{
+		ICommunityToolkitValueConverter converter = CreateConverter();
+
+		converter.ConvertBack(inputValue, targetType, null, CultureInfo.CurrentCulture).Should().Be(converter.DefaultConvertBackReturnValue);
 	}
 }
