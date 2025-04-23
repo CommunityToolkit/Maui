9183ee2efa7e0cbb7d313ab01b57569bf5f96c4c
Brandon Minnick <13558917+TheCodeTraveler@users.noreply.github.com>
2025-04-23 15:56:25 -0700

    Remove embedded videos

--- samples/CommunityToolkit.Maui.Sample/Pages/Views/MediaElement/MediaElementTextureViewPage.cs
+++ samples/CommunityToolkit.Maui.Sample/Pages/Views/MediaElement/MediaElementTextureViewPage.cs
@@ -1,3 +1,4 @@
+using CommunityToolkit.Maui.Sample.Constants;
 using CommunityToolkit.Maui.Sample.ViewModels.Views;
 using CommunityToolkit.Maui.Views;
 
@@ -5,17 +6,12 @@ namespace CommunityToolkit.Maui.Sample.Pages.Views;
 
 public partial class MediaElementTextureViewPage : BasePage<MediaElementTextureViewViewModel>
 {
-	readonly bool sHOW_LEFT_HALF_VIDEO = true;
-	readonly bool sHOW_RIGHT_HALF_VIDEO = true;
-
 	readonly List<MediaElement> mediaElements = [];
 	readonly List<Border> borders = [];
-	readonly List<AbsoluteLayout> absoluteLayouts = [];
+
 	public MediaElementTextureViewPage(MediaElementTextureViewViewModel viewModel) : base(viewModel)
 	{
 		AbsoluteLayout abs = [];
-		Content = abs;
-
 
 		for (int i = 0; i < 2; i++)
 		{
@@ -26,18 +22,18 @@ public partial class MediaElementTextureViewPage : BasePage<MediaElementTextureV
 			borders.Add(border);
 
 			AbsoluteLayout absolute = [];
-			absoluteLayouts.Add(absolute);
 			border.Content = absolute;
 
 			MediaElement mediaElement = new();
 			if (i == 0)
 			{
-				mediaElement.Source = MediaSource.FromResource("bunny.mp4");
+				mediaElement.Source = MediaSource.FromUri(StreamingVideoUrls.BuckBunny);
 			}
 			else
 			{
-				mediaElement.Source = MediaSource.FromResource("woods.mp4");
+				mediaElement.Source = MediaSource.FromUri(StreamingVideoUrls.ElephantsDream);
 			}
+
 			mediaElement.MetadataTitle = "Texture View";
 			mediaElement.MetadataArtist = "Community Toolkit";
 			mediaElement.MetadataArtworkUrl = "https://lh3.googleusercontent.com/pw/AP1GczNRrebWCJvfdIau1EbsyyYiwAfwHS0JXjbioXvHqEwYIIdCzuLodQCZmA57GADIo5iB3yMMx3t_vsefbfoHwSg0jfUjIXaI83xpiih6d-oT7qD_slR0VgNtfAwJhDBU09kS5V2T5ZML-WWZn8IrjD4J-g=w1792-h1024-s-no-gm";
@@ -48,35 +44,28 @@ public partial class MediaElementTextureViewPage : BasePage<MediaElementTextureV
 			absolute.Add(mediaElement);
 		}
 
+		abs.Add(borders[0]);
+		abs.Add(borders[1]);
 
-		//=============================
-		//ADD VIDEOS TO HIERARCHY
-		//=============================
-		if (sHOW_LEFT_HALF_VIDEO)
+		abs.SizeChanged += HandleSizeChanged;
+		Content = abs;
+	}
+
+	void HandleSizeChanged(object? sender, EventArgs e)
+	{
+		for (int i = 0; i < borders.Count; i++)
 		{
-			abs.Add(borders[0]);
-		}
-		if (sHOW_RIGHT_HALF_VIDEO)
-		{
-			abs.Add(borders[1]);
-		}
+			borders[i].HeightRequest = Content.Height;
+			borders[i].WidthRequest = Content.Width * 0.5;
 
+			mediaElements[i].HeightRequest = Content.Height;
+			mediaElements[i].WidthRequest = Content.Width;
 
-		Content.SizeChanged += delegate {
-			for (int i = 0; i < borders.Count; i++)
+			if (i == 1)
 			{
-				borders[i].HeightRequest = Content.Height;
-				borders[i].WidthRequest = Content.Width * 0.5;
-
-				mediaElements[i].HeightRequest = Content.Height;
-				mediaElements[i].WidthRequest = Content.Width;
-
-				if (i == 1)
-				{
-					borders[i].TranslationX = 0.5 * Content.Width;
-					mediaElements[i].TranslationX = -0.5 * Content.Width;
-				}
+				borders[i].TranslationX = 0.5 * Content.Width;
+				mediaElements[i].TranslationX = -0.5 * Content.Width;
 			}
-		};
+		}
 	}
 }
