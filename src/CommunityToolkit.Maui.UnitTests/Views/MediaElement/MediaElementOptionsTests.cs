8e739c31360495141afe71a08d7f85014018adda
James Crutchley <ne0rmatrix@gmail.com>
2025-04-25 12:10:47 -0700

    Add unit tests for MediaElement default view type
    Implemented unit tests for `AppBuilderExtensions_MediaElement` and `MediaElementOptions` to validate the setting and retrieval of the default Android view type for media elements. The tests cover various scenarios to ensure expected behavior.

---
+++ src/CommunityToolkit.Maui.UnitTests/Views/MediaElement/MediaElementOptionsTests.cs
@@ -0,0 +1,30 @@
+﻿using CommunityToolkit.Maui.Core;
+using FluentAssertions;
+using Xunit;
+
+namespace CommunityToolkit.Maui.UnitTests.Views;
+
+public class MediaElementOptionsTests
+{
+    [Fact]
+    public void DefaultAndroidViewType_ShouldBe_SurfaceView()
+    {
+        MediaElementOptions.DefaultAndroidViewType.Should().Be(AndroidViewType.SurfaceView);
+    }
+
+    [Fact]
+    public void SetDefaultAndroidViewType_ShouldUpdateDefaultAndroidViewType()
+    {
+        var mediaElementOptions = new MediaElementOptions(null!);
+        mediaElementOptions.SetDefaultAndroidViewType(AndroidViewType.TextureView);
+        MediaElementOptions.DefaultAndroidViewType.Should().Be(AndroidViewType.TextureView);
+    }
+
+    [Fact]
+    public void SetDefaultAndroidViewType_ShouldNotThrow_WhenCalledWithValidValue()
+    {
+        var mediaElementOptions = new MediaElementOptions(null!);
+        Action act = () => mediaElementOptions.SetDefaultAndroidViewType(AndroidViewType.SurfaceView);
+        act.Should().NotThrow();
+    }
+}
