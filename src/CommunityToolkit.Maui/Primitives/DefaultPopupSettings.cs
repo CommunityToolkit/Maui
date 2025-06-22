3900df1f8e26685e825971601ad48b2e82e4f914
Brandon Minnick <13558917+TheCodeTraveler@users.noreply.github.com>
2025-06-22 16:29:19 -0700

    Update XML docs

--- src/CommunityToolkit.Maui/Primitives/DefaultPopupSettings.cs
+++ src/CommunityToolkit.Maui/Primitives/DefaultPopupSettings.cs
@@ -27,32 +27,32 @@ public record DefaultPopupSettings
 	public Color BackgroundColor { get; init; } = PopupDefaults.BackgroundColor;
 
 	/// <summary>
-	/// Default Values for <see cref="Microsoft.Maui.Controls.View"/>
+	/// Default Values for <see cref="Popup"/>
 	/// </summary>
 	static class PopupDefaults
 	{
 		/// <summary>
-		/// Default value for <see cref="Microsoft.Maui.Controls.View.Margin"/> 
+		/// Default value for <see cref="Popup.Margin"/> 
 		/// </summary>
 		public static Thickness Margin { get; } = new(30);
 
 		/// <summary>
-		/// Default value for <see cref="Microsoft.Maui.Controls.Layout.Padding"/> 
+		/// Default value for <see cref="Popup.Padding"/> 
 		/// </summary>
 		public static Thickness Padding { get; } = new(15);
 
 		/// <summary>
-		/// Default value for <see cref="Microsoft.Maui.Controls.View.VerticalOptions"/> 
+		/// Default value for <see cref="Popup.VerticalOptions"/> 
 		/// </summary>
 		public static LayoutOptions VerticalOptions { get; } = LayoutOptions.Center;
 
 		/// <summary>
-		/// Default value for <see cref="Microsoft.Maui.Controls.View.HorizontalOptions"/> 
+		/// Default value for <see cref="Popup.HorizontalOptions"/> 
 		/// </summary>
 		public static LayoutOptions HorizontalOptions { get; } = LayoutOptions.Center;
 
 		/// <summary>
-		/// Default value for <see cref="VisualElement.BackgroundColor"/> BackgroundColor 
+		/// Default value for <see cref="Popup.BackgroundColor"/> BackgroundColor 
 		/// </summary>
 		public static Color BackgroundColor { get; } = Colors.White;
 
