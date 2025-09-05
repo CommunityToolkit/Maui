073a2bd4092ef302c7a78f2a171a26f12fcbf819
James Crutchley <ne0rmatrix@gmail.com>
2025-09-05 13:41:32 -0700

    Updated `FullScreenStateChangedEventArgs` to inherit from `EventArgs`

--- src/CommunityToolkit.Maui.MediaElement/Primitives/FullScreenStateChangedEventArgs.cs
+++ src/CommunityToolkit.Maui.MediaElement/Primitives/FullScreenStateChangedEventArgs.cs
@@ -3,26 +3,21 @@
 /// <summary>
 /// Event data for when the full screen state of the media element has changed.
 /// </summary>
-public sealed class FullScreenStateChangedEventArgs
+/// <remarks>
+/// Initializes a new instance of the <see cref="FullScreenStateChangedEventArgs"/> class.
+/// </remarks>
+/// <param name="previousState"></param>
+/// <param name="newState"></param>
+public sealed class FullScreenStateChangedEventArgs(MediaElementScreenState previousState, MediaElementScreenState newState) : EventArgs
 {
-	/// <summary>
-	/// Initializes a new instance of the <see cref="FullScreenStateChangedEventArgs"/> class.
-	/// </summary>
-	/// <param name="previousState"></param>
-	/// <param name="newState"></param>
-	public FullScreenStateChangedEventArgs(MediaElementScreenState previousState, MediaElementScreenState newState)
-	{
-		PreviousState = previousState;
-		NewState = newState;
-	}
 
 	/// <summary>
 	/// Gets the previous state that the <see cref="Core.IMediaElement"/> instance is transitioning from.
 	/// </summary>
-	public MediaElementScreenState PreviousState { get; }
+	public MediaElementScreenState PreviousState { get; } = previousState;
 
 	/// <summary>
 	/// Gets the new state that the <see cref="Core.IMediaElement"/> instance is transitioning to.
 	/// </summary>
-	public MediaElementScreenState NewState { get; }
+	public MediaElementScreenState NewState { get; } = newState;
 }
