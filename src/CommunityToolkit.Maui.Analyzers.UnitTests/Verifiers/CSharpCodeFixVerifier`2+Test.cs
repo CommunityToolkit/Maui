d438f9c91490b9e4a0e637ae1bb522b51e2d70b7
Brandon Minnick <13558917+TheCodeTraveler@users.noreply.github.com>
2025-10-07 10:09:06 -0700

    Update CSharpCodeFixVerifier`2+Test.cs

--- src/CommunityToolkit.Maui.Analyzers.UnitTests/Verifiers/CSharpCodeFixVerifier`2+Test.cs
+++ src/CommunityToolkit.Maui.Analyzers.UnitTests/Verifiers/CSharpCodeFixVerifier`2+Test.cs
@@ -13,8 +13,8 @@ public static partial class CSharpCodeFixVerifier<TAnalyzer, TCodeFix>
 	{
 		public Test(params ReadOnlySpan<Type> assembliesUnderTest)
 		{
-#if NET9_0
-			ReferenceAssemblies = Microsoft.CodeAnalysis.Testing.ReferenceAssemblies.Net.Net90;
+#if NET10_0
+			ReferenceAssemblies = Microsoft.CodeAnalysis.Testing.ReferenceAssemblies.Net.Net100;
 #else
 #error ReferenceAssemblies must be updated to current version of .NET
 #endif
