fbbcf5d6a390a4b3f358f7aa2c15263b61f999e9
Brandon Minnick <13558917+TheCodeTraveler@users.noreply.github.com>
2025-10-07 11:38:31 -0700

    Keep Compiler Error for `Microsoft.CodeAnalysis.Testing.ReferenceAssemblies.Net`

--- src/CommunityToolkit.Maui.Analyzers.UnitTests/Verifiers/CSharpAnalyzerVerifier`1+Test.cs
+++ src/CommunityToolkit.Maui.Analyzers.UnitTests/Verifiers/CSharpAnalyzerVerifier`1+Test.cs
@@ -10,7 +10,7 @@ public static partial class CSharpAnalyzerVerifier<TAnalyzer>
 	{
 		public Test(params ReadOnlySpan<Type> assembliesUnderTest)
 		{
-#if NET10_0
+#if NET9
 			ReferenceAssemblies = Microsoft.CodeAnalysis.Testing.ReferenceAssemblies.Net.Net90;
 #else
 #error ReferenceAssemblies must be updated to current version of .NET
--- src/CommunityToolkit.Maui.Analyzers.UnitTests/Verifiers/CSharpCodeFixVerifier`2+Test.cs
+++ src/CommunityToolkit.Maui.Analyzers.UnitTests/Verifiers/CSharpCodeFixVerifier`2+Test.cs
@@ -13,7 +13,7 @@ public static partial class CSharpCodeFixVerifier<TAnalyzer, TCodeFix>
 	{
 		public Test(params ReadOnlySpan<Type> assembliesUnderTest)
 		{
-#if NET10_0
+#if NET9
 			ReferenceAssemblies = Microsoft.CodeAnalysis.Testing.ReferenceAssemblies.Net.Net90;
 #else
 #error ReferenceAssemblies must be updated to current version of .NET
