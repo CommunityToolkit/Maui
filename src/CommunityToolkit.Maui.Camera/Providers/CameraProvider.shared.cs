a0674232628643f076b2c4a37ed9535f18bfd9e7
Brandon Minnick <13558917+TheCodeTraveler@users.noreply.github.com>
2025-10-13 09:45:22 -0700

    Add `ICameraProvider.AvailableCamerasChanged`

--- src/CommunityToolkit.Maui.Camera/Providers/CameraProvider.shared.cs
+++ src/CommunityToolkit.Maui.Camera/Providers/CameraProvider.shared.cs
@@ -5,9 +5,46 @@
 /// </summary>
 partial class CameraProvider : ICameraProvider
 {
+	readonly WeakEventManager availableCamerasChangedEventManager = new();
+	
+	public event EventHandler<IReadOnlyList<CameraInfo>?> AvailableCamerasChanged
+	{
+		add => availableCamerasChangedEventManager.AddEventHandler(value);
+		remove => availableCamerasChangedEventManager.RemoveEventHandler(value);
+	}
+
 	/// <inheritdoc/>
-	public IReadOnlyList<CameraInfo>? AvailableCameras { get; private set; }
+	public IReadOnlyList<CameraInfo>? AvailableCameras
+	{
+		get;
+		private set
+		{
+			if (!AreCameraInfoListsEqual(field, value))
+			{
+				field = value;
+				availableCamerasChangedEventManager.HandleEvent(this, value, nameof(AvailableCamerasChanged));	
+			}
+		}
+	}
 
 	/// <inheritdoc/>
 	public partial ValueTask RefreshAvailableCameras(CancellationToken token);
+
+	static bool AreCameraInfoListsEqual(in IReadOnlyList<CameraInfo>? cameraInfoList1, in IReadOnlyList<CameraInfo>? cameraInfoList2)
+	{
+		if (cameraInfoList1 is null && cameraInfoList2 is null)
+		{
+			return true;
+		}
+
+		if (cameraInfoList1 is null || cameraInfoList2 is null)
+		{
+			return false;
+		}
+
+		var cameraInfosInList1ButNotInList2 = cameraInfoList1.Except(cameraInfoList2).ToList();
+		var cameraInfosInList2ButNotInList1 = cameraInfoList2.Except(cameraInfoList1).ToList();
+
+		return cameraInfosInList1ButNotInList2.Count is 0 && cameraInfosInList2ButNotInList1.Count is 0;
+	}
 }
