219358437eb8f940f9534b3f7218aab4360a254f
Brandon Minnick <13558917+TheCodeTraveler@users.noreply.github.com>
2025-11-08 17:11:26 -0800

    Add `CommunityToolkit.Maui.SourceGenerators.Internal.UnitTests.csproj`

---
+++ src/CommunityToolkit.Maui.SourceGenerators.Internal.UnitTests/GlobalUsings.cs
@@ -0,0 +1,7 @@
+global using System;
+global using System.Collections.Immutable;
+global using System.Linq;
+global using System.Threading;
+global using System.Threading.Tasks;
+global using CommunityToolkit.Maui.SourceGenerators.Internal;
+global using CommunityToolkit.Maui.SourceGenerators.Internal.Helpers;
