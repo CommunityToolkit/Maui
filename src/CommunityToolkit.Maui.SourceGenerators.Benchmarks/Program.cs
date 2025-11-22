e2cc78afdf63ec8292ea1f6fd790c71cab62111c
Brandon Minnick <13558917+TheCodeTraveler@users.noreply.github.com>
2025-11-22 12:32:47 -0800

    Add Benchmarks

---
+++ src/CommunityToolkit.Maui.SourceGenerators.Benchmarks/Program.cs
@@ -0,0 +1,11 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace CommunityToolkit.Maui.SourceGenerators.Benchmarks;
+
+class Program
+{
+	public static void Main(string[] args)
+	{
+		BenchmarkRunner.Run(typeof(Program).Assembly);
+	}
+}
