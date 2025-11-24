fc89118dd778a745997c3834539c6042f0979fe5
Brandon Minnick <13558917+TheCodeTraveler@users.noreply.github.com>
2025-11-24 11:50:13 -0800

    `dotnet format`

--- src/CommunityToolkit.Maui.SourceGenerators.Internal/Helpers/AttributeExtensions.cs
+++ src/CommunityToolkit.Maui.SourceGenerators.Internal/Helpers/AttributeExtensions.cs
@@ -29,9 +29,9 @@ static class AttributeExtensions
 			return $"({data.Type}){members[(int)data.Value]}";
 		}
 
-		if(data.Type?.SpecialType is SpecialType.System_String)
+		if (data.Type?.SpecialType is SpecialType.System_String)
 		{
-			return data.Value is null ? $"\"{placeholder}\"": $"({data.Type})\"{data.Value}\"";
+			return data.Value is null ? $"\"{placeholder}\"" : $"({data.Type})\"{data.Value}\"";
 		}
 
 		if (data.Type?.SpecialType is SpecialType.System_Char)
