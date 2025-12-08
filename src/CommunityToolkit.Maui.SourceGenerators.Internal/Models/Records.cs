07d0087da7834b58d90f4ea9cf071475ce9bfa07
Brandon Minnick <13558917+TheCodeTraveler@users.noreply.github.com>
2025-12-08 10:56:14 -0800

    Use `"null"` instead of `string.IsNullOrEmpty()`

--- src/CommunityToolkit.Maui.SourceGenerators.Internal/Models/Records.cs
+++ src/CommunityToolkit.Maui.SourceGenerators.Internal/Models/Records.cs
@@ -6,7 +6,7 @@ namespace CommunityToolkit.Maui.SourceGenerators.Internal.Models;
 record BindablePropertyModel(string PropertyName, ITypeSymbol ReturnType, ITypeSymbol DeclaringType, string DefaultValue, string DefaultBindingMode, string ValidateValueMethodName, string PropertyChangedMethodName, string PropertyChangingMethodName, string CoerceValueMethodName, string DefaultValueCreatorMethodName, string NewKeywordText, bool IsReadOnlyBindableProperty, string? SetterAccessibility, bool HasInitializer)
 {
 	// When both a DefaultValueCreatorMethodName and an initializer are provided, we implement the DefaultValueCreator method and the ignore the partial Property initializer
-	public bool ShouldUsePropertyInitializer => HasInitializer && string.IsNullOrEmpty(DefaultValueCreatorMethodName);
+	public bool ShouldUsePropertyInitializer => HasInitializer && DefaultValueCreatorMethodName is "null";
 	public string BindablePropertyName => $"{PropertyName}Property";
 	public string BindablePropertyKeyName => $"{char.ToLower(PropertyName[0])}{PropertyName[1..]}PropertyKey";
 	public string EffectiveDefaultValueCreatorMethodName => ShouldUsePropertyInitializer ? $"__createDefault{PropertyName}" : DefaultValueCreatorMethodName;
