eec8a826bc2c93d19c9d8c65bb59eb24947bf00b
Brandon Minnick <13558917+TheCodeTraveler@users.noreply.github.com>
2025-12-20 11:26:14 -0400

    Add `FadeAnimationDefaults`

--- src/CommunityToolkit.Maui/Animations/FadeAnimation.shared.cs
+++ src/CommunityToolkit.Maui/Animations/FadeAnimation.shared.cs
@@ -1,16 +1,18 @@
-﻿namespace CommunityToolkit.Maui.Animations;
+﻿using CommunityToolkit.Maui.Core;
+
+namespace CommunityToolkit.Maui.Animations;
 
 /// <summary>
 /// Animation that will fade the supplied view to the specified <see cref="Opacity"/>
 /// and then back to its original <see cref="Opacity"/>.
 /// </summary>
-public partial class FadeAnimation() : BaseAnimation(300)
+public partial class FadeAnimation() : BaseAnimation(FadeAnimationDefaults.Length)
 {
 	/// <summary>
 	/// Gets or sets the opacity to fade to before returning to the elements current <see cref="VisualElement.Opacity"/>.
 	/// </summary>
 	[BindableProperty(DefaultBindingMode = BindingMode.TwoWay)]
-	public partial double Opacity { get; set; } = 0.3;
+	public partial double Opacity { get; set; } = FadeAnimationDefaults.Opacity;
 
 	/// <inheritdoc />
 	public override async Task Animate(VisualElement view, CancellationToken token = default)
