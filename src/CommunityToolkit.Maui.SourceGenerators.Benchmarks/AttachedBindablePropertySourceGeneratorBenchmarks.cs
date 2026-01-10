c7429d2674997d74992c5371240158f88a4ebe4e
Brandon Minnick <13558917+TheCodeTraveler@users.noreply.github.com>
2026-01-10 12:38:29 -0800

    Use `AttachedBindableProperty` for test names

--- src/CommunityToolkit.Maui.SourceGenerators.Benchmarks/AttachedBindablePropertySourceGeneratorBenchmarks.cs
+++ src/CommunityToolkit.Maui.SourceGenerators.Benchmarks/AttachedBindablePropertySourceGeneratorBenchmarks.cs
@@ -11,30 +11,30 @@ public class AttachedBindablePropertyAttributeSourceGeneratorBenchmarks
 	static readonly AttachedBindablePropertyAttributeSourceGenerator_ClassAttribute_IntegrationTests integrationTests = new();
 
 	[Benchmark]
-	public Task GenerateBindableProperty_SimpleExample_GeneratesCorrectCode()
+	public Task GenerateAttachedBindableProperty_SimpleExample_GeneratesCorrectCode()
 		=> commonUsageTests.GenerateAttachedBindableProperty_SimpleExample_GeneratesCorrectCode();
 
 	[Benchmark]
-	public Task GenerateBindableProperty_MultipleProperties_GeneratesCorrectCode()
+	public Task GenerateAttachedBindableProperty_MultipleProperties_GeneratesCorrectCode()
 		=> commonUsageTests.GenerateAttachedBindableProperty_MultipleProperties_GeneratesCorrectCode();
 
 	[Benchmark]
-	public Task GenerateBindableProperty_WithAllParameters_GeneratesCorrectCode()
+	public Task GenerateAttachedBindableProperty_WithAllParameters_GeneratesCorrectCode()
 		=> commonUsageTests.GenerateAttachedBindableProperty_WithAllParameters_GeneratesCorrectCode();
 
 	[Benchmark]
-	public Task GenerateBindableProperty_InternalClass_GeneratesCorrectCode()
+	public Task GenerateAttachedBindableProperty_InternalClass_GeneratesCorrectCode()
 		=> commonUsageTests.GenerateAttachedBindableProperty_InternalClass_GeneratesCorrectCode();
 
 	[Benchmark]
-	public Task GenerateBindableProperty_ComplexInheritanceScenario_GeneratesCorrectCode()
+	public Task GenerateAttachedBindableProperty_ComplexInheritanceScenario_GeneratesCorrectCode()
 		=> integrationTests.GenerateAttachedBindableProperty_ComplexInheritanceScenario_GeneratesCorrectCode();
 
 	[Benchmark]
-	public Task GenerateBindableProperty_NestedClass_GeneratesCorrectCode()
+	public Task GenerateAttachedBindableProperty_NestedClass_GeneratesCorrectCode()
 		=> integrationTests.GenerateAttachedBindableProperty_AttributeOnNestedInnerClass_GeneratesCorrectCode();
 
 	[Benchmark]
-	public Task GenerateBindableProperty_WithComplexDefaultValues_GeneratesCorrectCode()
+	public Task GenerateAttachedBindableProperty_WithComplexDefaultValues_GeneratesCorrectCode()
 		=> edgeCaseTests.GenerateAttachedBindableProperty_WithComplexDefaultValues_GeneratesCorrectCode();
 }
