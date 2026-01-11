714d08616e9cc09409fb45a43309301d5410c9a4
Brandon Minnick <13558917+TheCodeTraveler@users.noreply.github.com>
2026-01-10 16:24:22 -0800

    Add XML Comments for AccessModifier

--- src/CommunityToolkit.Maui.SourceGenerators.Internal/Generators/AttachedBindablePropertyAttributeSourceGenerator.cs
+++ src/CommunityToolkit.Maui.SourceGenerators.Internal/Generators/AttachedBindablePropertyAttributeSourceGenerator.cs
@@ -100,14 +100,22 @@ public class AttachedBindablePropertyAttributeSourceGenerator : IIncrementalGene
 		  #nullable enable
 		  namespace CommunityToolkit.Maui;
 
+		  ///<summary>Used by CommunityToolkit.Maui source generators to define access modifiers for generated code</summary>
 		  public enum AccessModifier
 		  {
+		  	///<summary>Generates <see langword="public"/> access modifier</summary>
 		  	Public = 0,
+		  	///<summary>Generates <see langword="internal"/> access modifier</summary>
 		  	Internal = 1,
+		  	///<summary>Generates <see langword="protected"/> <see langword="internal"/> access modifier</summary>
 		  	ProtectedInternal = 2,
+		  	///<summary>Generates <see langword="protected"/> access modifier</summary>
 		  	Protected = 3,
+		  	///<summary>Generates <see langword="private"/> <see langword="protected"/> access modifier</summary>
 		  	PrivateProtected = 4,
+		  	///<summary>Generates <see langword="private"/> access modifier</summary>
 		  	Private = 5,
+		  	///<summary>Generates no code</summary>
 		  	None = 6
 		  }
 		  """;
