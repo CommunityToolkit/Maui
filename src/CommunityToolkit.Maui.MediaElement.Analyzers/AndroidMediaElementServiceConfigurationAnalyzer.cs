651c5560af3fd42b5cad37156513869909e22c0a
Brandon Minnick <13558917+TheCodeTraveler@users.noreply.github.com>
2026-01-28 12:51:28 +0000

    Use Resources.resx

--- src/CommunityToolkit.Maui.MediaElement.Analyzers/AndroidMediaElementServiceConfigurationAnalyzer.cs
+++ src/CommunityToolkit.Maui.MediaElement.Analyzers/AndroidMediaElementServiceConfigurationAnalyzer.cs
@@ -13,13 +13,25 @@ namespace CommunityToolkit.Maui.MediaElement.Analyzers;
 [DiagnosticAnalyzer(LanguageNames.CSharp)]
 public class AndroidMediaElementServiceConfigurationAnalyzer : DiagnosticAnalyzer
 {
+	/// <summary>
+	/// Diagnostic ID for missing Android manifest configuration when service is enabled.
+	/// </summary>
+	public const string DiagnosticId = $"MCTME002";
+
+	const string category = "Usage";
+
 	const string mediaElementOptionsClassName = "MediaElementOptions";
 	const string isAndroidForegroundServiceEnabledProperty = "IsAndroidForegroundServiceEnabled";
 	const string setDefaultAndroidForegroundServiceEnabled = "SetDefaultAndroidForegroundServiceEnabled";
 	const string useMauiCommunityToolkitMediaElement = "UseMauiCommunityToolkitMediaElement";
 
-	public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics =>
-		[AndroidMediaElementServiceDiagnostics.MissingAndroidManifestConfigurationDescriptor];
+	static readonly LocalizableString title = new LocalizableResourceString(nameof(Resources.AndroidMediaElementServiceConfigurationTitle), Resources.ResourceManager, typeof(Resources));
+	static readonly LocalizableString messageFormat = new LocalizableResourceString(nameof(Resources.AndroidMediaElementServiceConfigurationFormat), Resources.ResourceManager, typeof(Resources));
+	static readonly LocalizableString description = new LocalizableResourceString(nameof(Resources.AndroidMediaElementServiceConfigurationErrorMessage), Resources.ResourceManager, typeof(Resources));
+
+	static readonly DiagnosticDescriptor rule = new(DiagnosticId, title, messageFormat, category, DiagnosticSeverity.Error, isEnabledByDefault: true, description: description);
+
+	public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => [rule];
 
 	public override void Initialize(AnalysisContext context)
 	{
@@ -64,7 +76,7 @@ public class AndroidMediaElementServiceConfigurationAnalyzer : DiagnosticAnalyze
 		if (hasPropertyInitializer)
 		{
 			var diagnostic = Diagnostic.Create(
-				AndroidMediaElementServiceDiagnostics.MissingAndroidManifestConfigurationDescriptor,
+				rule,
 				property.Locations[0]);
 
 			context.ReportDiagnostic(diagnostic);
@@ -100,7 +112,7 @@ public class AndroidMediaElementServiceConfigurationAnalyzer : DiagnosticAnalyze
 			if (firstArg.Kind() == SyntaxKind.TrueLiteralExpression)
 			{
 				var diagnostic = Diagnostic.Create(
-					AndroidMediaElementServiceDiagnostics.MissingAndroidManifestConfigurationDescriptor,
+					rule,
 					firstArg.GetLocation());
 
 				context.ReportDiagnostic(diagnostic);
