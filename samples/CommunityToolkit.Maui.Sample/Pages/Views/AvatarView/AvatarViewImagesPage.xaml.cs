792bbc5debbf1e60bc1f225518be4d5de0272b90
Pastajello <michal.sierpinski@pm.me>
2026-04-03 19:08:53 +0200

    add sample

--- samples/CommunityToolkit.Maui.Sample/Pages/Views/AvatarView/AvatarViewImagesPage.xaml.cs
+++ samples/CommunityToolkit.Maui.Sample/Pages/Views/AvatarView/AvatarViewImagesPage.xaml.cs
@@ -5,4 +5,9 @@ namespace CommunityToolkit.Maui.Sample.Pages.Views;
 public partial class AvatarViewImagesPage : BasePage<AvatarViewImagesViewModel>
 {
 	public AvatarViewImagesPage(AvatarViewImagesViewModel avatarViewImagesViewModel) : base(avatarViewImagesViewModel) => InitializeComponent();
+	
+	void Button_OnClicked(object? sender, EventArgs e)
+	{
+		buggyImage.ImageSource = ImageSource.FromFile("avatar_icon.png");
+	}
 }
