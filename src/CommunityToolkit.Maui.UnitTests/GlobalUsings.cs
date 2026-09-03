f33d370487623fbad3f09cf4fc68fe26982a38ec
Vladislav Antonyuk <vlad.antonyuk@gmail.com>
2026-09-03 21:20:52 +0300

    update packages

--- src/CommunityToolkit.Maui.UnitTests/GlobalUsings.cs
+++ src/CommunityToolkit.Maui.UnitTests/GlobalUsings.cs
@@ -1,3 +1,6 @@
 using Xunit;
+using Xunit.Sdk;
+using Xunit.v3;
 
-[assembly: CollectionBehavior(CollectionBehavior.CollectionPerAssembly, DisableTestParallelization = true, MaxParallelThreads = 1)]
+[assembly: CollectionBehavior(CollectionBehavior.CollectionPerAssembly)]
+[assembly: Parallelization(Mode = ParallelMode.None, MaxThreads = 1)]
